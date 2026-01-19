b159356f83e37e019dc1b51a2c06e22200ce52a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 20:30:06 +0000

    librest: update 0.10.2 bottle.

--- Formula/lib/librest.rb
+++ Formula/lib/librest.rb
@@ -13,12 +13,12 @@ class Librest < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "e76bb42c804ee253aae0f581c72d121155c22180f6ce3917b1a94e4276086a46"
-    sha256 cellar: :any, arm64_sequoia: "0a699a3e62f7035586853564492235d8778647d0cf348c2482708e17c4690c0e"
-    sha256 cellar: :any, arm64_sonoma:  "aac6f3ab8292924de0a5838b47151ce4cf1bcb083b71e49e3714507c90cb44dc"
-    sha256 cellar: :any, sonoma:        "53cca284053a914c5343fa07e98cee02aa01c5e02ca69974b0c71d7002dda40b"
-    sha256               arm64_linux:   "c4abc96ee37a45a227101d5345a9ecfc753860b6b77283fc20fc848b4757ab9f"
-    sha256               x86_64_linux:  "fed14fb30d8f8a379de83f08fd4ea0ef9ef4b6d194e8da469d9ba4acbda524f8"
+    sha256 cellar: :any, arm64_tahoe:   "4953ad6e88b6646bcf562d66193b617cbd2bba6c9dfecb37eed108f971be575b"
+    sha256 cellar: :any, arm64_sequoia: "22bd743bb1c8dd2453fd90278636395f2cafb6fe2adb734e67f1cfc2e7ac4333"
+    sha256 cellar: :any, arm64_sonoma:  "0a87dcb8ffcc161c718f36342ba14c14ed6f82bd4b4a54be4b0d54f0df3f1966"
+    sha256 cellar: :any, sonoma:        "f3019e4787530901ecbddda6775b47cedd8c44f1b7d7441e458675bc8ae31500"
+    sha256               arm64_linux:   "a2ed194719ac2d8318a309c63da0506d345c83691d1481ca4545579b874b40ef"
+    sha256               x86_64_linux:  "44ba6aff805854a906f060100e335f0d608618ceda53249ecd9c6b0d8b0df58d"
   end
 
   depends_on "gobject-introspection" => :build
