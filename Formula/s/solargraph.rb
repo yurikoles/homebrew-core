099ba91962ec29bbbc3d82987c803d53c99404cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 14:28:01 +0000

    solargraph 0.58.2

--- Formula/s/solargraph.rb
+++ Formula/s/solargraph.rb
@@ -3,8 +3,8 @@ class Solargraph < Formula
   homepage "https://solargraph.org"
   # Must be git, because solargraph.gemspec uses git ls-files
   url "https://github.com/castwide/solargraph.git",
-      tag:      "v0.58.0",
-      revision: "d08a25d516bc62d3bbc0c26934a3dd2091a26b8d"
+      tag:      "v0.58.2",
+      revision: "676da4c4cec040d735a9a54265c91dc91ec462bb"
   license "MIT"
 
   bottle do
