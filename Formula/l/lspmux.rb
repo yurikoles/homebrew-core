5477f4b47f9b3249c3c000f922cef45c5fabc88e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-19 20:04:33 +0000

    lspmux: add 0.3.0 bottle.

--- Formula/l/lspmux.rb
+++ Formula/l/lspmux.rb
@@ -6,6 +6,15 @@ class Lspmux < Formula
   license "EUPL-1.2"
   head "https://codeberg.org/p2502/lspmux.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5d37f6ac1c9841457e8533f39ca4b7090d6f7a0851722a8ed777677798b875f9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9462e1c0d75511447c498250b269c59ce55b226c2badc3aa4ab2724ba0aa4883"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44330c574fbf0a5c14eaa6b85d49a9510d856893c77fb10fe0e6015b1d3be4e1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c3304e7b57e6455f4a8deed2fc292bac9400d5ff0bbc84bd4a96f177b161f506"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e90116b031291bf519f529efac2ab4995b32408f4466121c4c0a756edd43a87c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c45d94f75046f86d12aa28761fd4d29735598fec4277662d2a1f8f3511319c1"
+  end
+
   depends_on "rust" => :build
   depends_on "rust-analyzer"
 
