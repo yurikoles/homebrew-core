02b35eaa7cebdc2e38dcb49e0350658262d088bc
Michael Cho <michael@michaelcho.dev>
2026-01-19 23:57:51 -0500

    proxygen: revision bump (glog 0.7.1)

--- Formula/p/proxygen.rb
+++ Formula/p/proxygen.rb
@@ -4,6 +4,7 @@ class Proxygen < Formula
   url "https://github.com/facebook/proxygen/releases/download/v2026.01.12.00/proxygen-v2026.01.12.00.tar.gz"
   sha256 "66364e2119618a98f5c3ad62765b53d8bc2c34a9e51e0e861345aa7a5e87414f"
   license "BSD-3-Clause"
+  revision 1
   head "https://github.com/facebook/proxygen.git", branch: "main"
 
   bottle do
