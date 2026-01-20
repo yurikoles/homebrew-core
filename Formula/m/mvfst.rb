eb52dbff6f2915979a9399420da02f585ca1d930
Michael Cho <michael@michaelcho.dev>
2026-01-19 23:57:51 -0500

    mvfst: revision bump (glog 0.7.1)

--- Formula/m/mvfst.rb
+++ Formula/m/mvfst.rb
@@ -4,6 +4,7 @@ class Mvfst < Formula
   url "https://github.com/facebook/mvfst/archive/refs/tags/v2026.01.12.00.tar.gz"
   sha256 "5c000c1ad7886a25d434224f85f9b5147acb9d0058497ec7666d843de129fd8a"
   license "MIT"
+  revision 1
   head "https://github.com/facebook/mvfst.git", branch: "main"
 
   bottle do
