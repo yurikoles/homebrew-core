aa40fea85b190e05fb02845bac22faf465968fce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-20 14:34:43 +0000

    terramate 0.15.5

--- Formula/t/terramate.rb
+++ Formula/t/terramate.rb
@@ -1,8 +1,8 @@
 class Terramate < Formula
   desc "Managing Terraform stacks with change detections and code generations"
   homepage "https://terramate.io/docs/"
-  url "https://github.com/terramate-io/terramate/archive/refs/tags/v0.15.4.tar.gz"
-  sha256 "870bc37bc197bf2771a605d3d3aec5894cd98d345db58446f2dc60f031273370"
+  url "https://github.com/terramate-io/terramate/archive/refs/tags/v0.15.5.tar.gz"
+  sha256 "7f1a1ec6313099aefd475776ad9f9167f50824dd882c4a27dfe4beefa13c7a52"
   license "MPL-2.0"
   head "https://github.com/terramate-io/terramate.git", branch: "main"
 
