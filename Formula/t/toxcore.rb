34d9a5fdd41f6d137f512fd10fb70d7e3c532604
Michael Cho <michael@michaelcho.dev>
2026-01-20 17:59:42 -0500

    toxcore 0.2.21

--- Formula/t/toxcore.rb
+++ Formula/t/toxcore.rb
@@ -3,10 +3,9 @@ class Toxcore < Formula
   homepage "https://tox.chat/"
   # This repo is a fork, but it is the source used by Debian, Fedora, and Arch,
   # and is the repo linked in the homepage.
-  url "https://github.com/TokTok/c-toxcore/releases/download/v0.2.20/c-toxcore-0.2.20.tar.gz"
-  sha256 "a9c89a8daea745d53e5d78e7aacb99c7b4792c4400a5a69c71238f45d6164f4c"
+  url "https://github.com/TokTok/c-toxcore/releases/download/v0.2.21/c-toxcore-v0.2.21.tar.xz"
+  sha256 "dde19a5eaa897f3b9ca6212e783a08753ad5c071deb14a73d84161853a067234"
   license "GPL-3.0-or-later"
-  revision 2
   head "https://github.com/TokTok/c-toxcore.git", branch: "master"
 
   bottle do
