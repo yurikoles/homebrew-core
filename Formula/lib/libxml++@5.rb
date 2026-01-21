d72c5ef286d42f50c448f77291f2fe40e328a21a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 03:21:46 +0000

    libxml++@5: update 5.6.0 bottle.

--- Formula/lib/libxml++@5.rb
+++ Formula/lib/libxml++@5.rb
@@ -11,12 +11,12 @@ class LibxmlxxAT5 < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "f700d5c85e52fa89adbd2fab8f8b73e071e52ab9aaef45121968209ce90a4f59"
-    sha256 cellar: :any, arm64_sequoia: "74ece1a21d1acc4aaf4f9895837de84aaf37855e309877213266aed5bfe7c2bb"
-    sha256 cellar: :any, arm64_sonoma:  "394658c92c750312e8f6ecaad34237bf6ab12a3897b810f2ad4e06349fd41f8a"
-    sha256 cellar: :any, sonoma:        "c04b51736c024ee77ebfbed0f850f05c9273dcfb9a90a1ae23eb426a00de23de"
-    sha256               arm64_linux:   "d300722e3348737fceeec7ffeb0722a1cb598bfe0c3e1f8253935e6509df5722"
-    sha256               x86_64_linux:  "70cf96991d0a83886ea06882bce125f42c209d2b932800eb6ac2359f6b92de5c"
+    sha256 cellar: :any, arm64_tahoe:   "d8f7af5b6ae22cf7f4ebc0cdcc6ff4c8da8948fed395c9ec78f70c945af17ef1"
+    sha256 cellar: :any, arm64_sequoia: "0de758278ded4db21000aef5e7e65ede12a06e98cb39bb031af13d6b1fc225f9"
+    sha256 cellar: :any, arm64_sonoma:  "21ed955b01f0d8bdd8108fe6b5ddffa0c90eb57e747bb0b2dc59c66ddd78f46e"
+    sha256 cellar: :any, sonoma:        "fda6da5a238721629336dc6136b3dd332a3ba9a1573ec3277709a03099b4a61e"
+    sha256               arm64_linux:   "b29e9a86607c16f976f10ed9c6674875e9c0261d465a4920191b68df1fb88da1"
+    sha256               x86_64_linux:  "555db96f572fb18e781cb8875bb73f3c595f2a36159dfd19350822d82e4a718a"
   end
 
   depends_on "meson" => :build
