2ab43661f99253bcee132f7f711069a4108b1be0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 15:31:59 +0000

    openskills: add 1.5.0 bottle.

--- Formula/o/openskills.rb
+++ Formula/o/openskills.rb
@@ -5,6 +5,10 @@ class Openskills < Formula
   sha256 "2aa0c31c2a09fad8c32705d519a5497aa671381189bde9d8f7911a7967a9d9bc"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "e2026c6d2ec9ba496ba0be75a24794d328cb23cfd8ede1922f0be3b1a81da9e8"
+  end
+
   depends_on "node"
 
   def install
