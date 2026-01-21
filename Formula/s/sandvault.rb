1ebf607dccefc3e268c2e31e9ef1e5a6742b46d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 08:17:15 +0000

    sandvault 1.1.2

--- Formula/s/sandvault.rb
+++ Formula/s/sandvault.rb
@@ -1,8 +1,8 @@
 class Sandvault < Formula
   desc "Run AI agents isolated in a sandboxed macOS user account"
   homepage "https://github.com/webcoyote/sandvault"
-  url "https://github.com/webcoyote/sandvault/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "3230d86f5d98eab85b220dd1a3892907bd8b00da3b792a75a32c0a077b138188"
+  url "https://github.com/webcoyote/sandvault/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "3c035dde8433495dfa36ef968b2c3c84f5aa5b4f546d80b933137eba7c496810"
   license "Apache-2.0"
 
   bottle do
