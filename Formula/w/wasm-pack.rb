351ae43585c29a9c955dcdf42029643c952e339d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 07:19:35 +0000

    wasm-pack: update 0.14.0 bottle.

--- Formula/w/wasm-pack.rb
+++ Formula/w/wasm-pack.rb
@@ -7,14 +7,12 @@ class WasmPack < Formula
   head "https://github.com/drager/wasm-pack.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "015d79bf605e1c7210d9662f663bd625d5ffb198b4b05f057ff96aeda01c4157"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "747c2699cacc93a426a98bab721cf4fcf70d04f041b9f985c5c28351823a3179"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5efb6426299ccfea3127cb7132c7583fd54d88072e623cb02f11532da201f243"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "c4721dbfd0281026391ea9d413a1e23c0bfecf803f370305a1c2e209234c1326"
-    sha256 cellar: :any_skip_relocation, sonoma:        "05ad5721098c7a62a35a817ca06af6a249d02d102e3739ce029809b6946824f4"
-    sha256 cellar: :any_skip_relocation, ventura:       "e42e4af8958a1593cbf06ed54a645512e1fd3f51e447b5c12882bd67f6fe0528"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a30ccc8550215b641f2203ce5375b53b5167f6368421243fd549fa68a4920a2a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ddf3a9d0f664911152c681c589f4751b92c4ac1c172b0f366b275a4dabfd5ff"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "31c12ac93a4e52a4b61b4bdb3e6fea208eaa93b9c30039babedc5957c0bc8d6a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f0ea3f4acbea89d2ef42c0b04336cbd739fe8abedc06199d708af7dfbe00bb7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2551c330819e973202af932f61610c8f2d78144081aefb839cc66cd11bb329b0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c25bada8ea52c2d71c8f919567fc95cad7e9238bda41458cdbb5c160014c3708"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a5f303937f07117619ec2adb850245769636b4829c2cda9d9f3c6089879be82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a46421d5f180137993d83ea1f60b902e0678971a562e18397aab8c57b8ec479c"
   end
 
   depends_on "cmake" => :build
