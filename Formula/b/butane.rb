6221acc6480c8d456d89c72c62a79dc2ae0bb803
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 21:50:37 +0000

    butane: update 0.26.0 bottle.

--- Formula/b/butane.rb
+++ Formula/b/butane.rb
@@ -12,12 +12,12 @@ class Butane < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "462e8be7b0aa75adecb7309207b6820cc18ca2991159e8bf3e79f8a14c5d4f93"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "462e8be7b0aa75adecb7309207b6820cc18ca2991159e8bf3e79f8a14c5d4f93"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "462e8be7b0aa75adecb7309207b6820cc18ca2991159e8bf3e79f8a14c5d4f93"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1c227b7f81ac0f40e7be5a2d263ddb1825a87c5ebbe552033abdd1cbcf6423e4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "acc15c74454f7db160bfb6801d1ddab0d043cff9152245aeaeb7e9a518c64deb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9791d51417e488f5aa4e00a60d95b46412f29f02e686ba4c7787ceee56af5aeb"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "41b5aa550fa53a4b56cd6f8ef44bcab381c5c60e1d2ec8396d3b10b24d78babe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41b5aa550fa53a4b56cd6f8ef44bcab381c5c60e1d2ec8396d3b10b24d78babe"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41b5aa550fa53a4b56cd6f8ef44bcab381c5c60e1d2ec8396d3b10b24d78babe"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0bed8a4d10dde908c0687e707f80e8a66484dd0475d1e00a7a9a7fef310fbd2e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "58ba8f3b92dcc0c8277bdc06befd4348fb6dec7825ca792afbc4314c594813c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4beb755a0353cd54e9865684e17f615792d7013b6605601e575040bbde39e04f"
   end
 
   depends_on "go" => :build
