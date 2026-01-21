3682c73b9d7d3deef07950718fb0878e30fe5e3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 13:54:16 +0000

    dstack: update 0.20.4 bottle.

--- Formula/d/dstack.rb
+++ Formula/d/dstack.rb
@@ -8,12 +8,12 @@ class Dstack < Formula
   license "MPL-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5ddc26b3630d1b9f55ccf0af0694a4be251e3926eeb9a0c9ffa3e0461c588f46"
-    sha256 cellar: :any,                 arm64_sequoia: "d07e2408cfd319af4e7b2dc8ad4260d311a27e421aa67de7913abda802790042"
-    sha256 cellar: :any,                 arm64_sonoma:  "2230d4e26564dfd008ccec07e99842413211f9ea0582af33bc9d07e17f323c88"
-    sha256 cellar: :any,                 sonoma:        "afb34e71fe10757f782a4a755da64db5764aac19692cfe45ff3401f96c07d253"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b6be96b6154b8457552985e1855ce72927ca1d92ffee41f828ca5fe64e4f89c9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7de5dd62b151fabcde1449fba02c0bba02b4a32c1a33e4a252fce6f1107dc85a"
+    sha256 cellar: :any,                 arm64_tahoe:   "e3cfb523162759066df26aa91299659c73568dc7bb64caaef696357d529d652e"
+    sha256 cellar: :any,                 arm64_sequoia: "53838eef0a757cfdd265b2dec0fa29de6c088f50bdc973976efee905e4c6007e"
+    sha256 cellar: :any,                 arm64_sonoma:  "db8cdd5846d49aaf0b9ac2ffc5b09d6ecf46150a25d41c3e9549d1477813d16b"
+    sha256 cellar: :any,                 sonoma:        "96274dea5dd244522aa27075f75feea45181e578aab9aba6133e8eb2bfc0ddb0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "702ef0572d2d49648e0b82800dfc3bed79751d234c40903fea25a2b44a7f2db3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7dccbc40586fa226926a5aa6b0a9b03393f05410134f8fd024355f5b2db5b475"
   end
 
   # `pkgconf` and `rust` are for bcrypt.
