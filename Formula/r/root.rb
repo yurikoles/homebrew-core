0b460cb12841192ec569de4d193c6b0e5a0a3dbd
Michael Cho <michael@michaelcho.dev>
2026-01-20 20:03:58 -0500

    root 6.38.00

--- Formula/r/root.rb
+++ Formula/r/root.rb
@@ -2,15 +2,17 @@ class Root < Formula
   desc "Analyzing petabytes of data, scientifically"
   homepage "https://root.cern"
   license "LGPL-2.1-or-later"
-  revision 1
   head "https://github.com/root-project/root.git", branch: "master"
 
   stable do
-    url "https://root.cern/download/root_v6.36.06.source.tar.gz"
-    sha256 "62f9d38d2f2ed3d46653529c98e8cbc9b8866776494eb40ba0c23e2f46b681c4"
+    url "https://root.cern/download/root_v6.38.00.source.tar.gz"
+    sha256 "a4429422c460f832cde514a580dd202b1d3c96e8919c24363c3d42f8cf5accdc"
 
-    # Backport part of https://github.com/root-project/root/commit/b2acf687d6b5f887b8f97f35d9b3b011adad5be4
-    patch :DATA
+    # Backport fix for RPATHs
+    patch do
+      url "https://github.com/root-project/root/commit/f646e520a5dc0b2e50b708c3d757bcf2abe618fd.patch?full_index=1"
+      sha256 "aa08b4dfe0274fd2b163d1b5eeb617864452337ff63d571264d17fe7f5d05f70"
+    end
   end
 
   livecheck do
@@ -48,8 +50,7 @@ class Root < Formula
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "lz4"
-  depends_on "mariadb-connector-c"
-  depends_on "nlohmann-json"
+  depends_on "nlohmann-json" => :no_linkage
   depends_on "numpy" # for tmva
   depends_on "openblas"
   depends_on "openssl@3"
@@ -87,9 +88,6 @@ class Root < Formula
   end
 
   def install
-    # Workaround for CMake 4 due to VDT, https://github.com/dpiparo/vdt/blob/master/CMakeLists.txt
-    ENV["CMAKE_POLICY_VERSION_MINIMUM"] = "3.5"
-
     # Skip modification of CLING_OSX_SYSROOT to the unversioned SDK path
     # Related: https://github.com/Homebrew/homebrew-core/issues/135714
     # Related: https://github.com/root-project/cling/issues/457
@@ -152,7 +150,6 @@ class Root < Formula
       -Dgnuinstall=ON
       -Dimt=ON
       -Dmathmore=ON
-      -Dmysql=ON
       -Docaml=OFF
       -Doracle=OFF
       -Dpgsql=OFF
@@ -236,53 +233,3 @@ class Root < Formula
     system python3, "-c", "import ROOT; ROOT.gSystem.LoadAllLibraries()"
   end
 end
-
-__END__
-diff --git a/bindings/pyroot/cppyy/CPyCppyy/CMakeLists.txt b/bindings/pyroot/cppyy/CPyCppyy/CMakeLists.txt
-index 911517294b..03406a9663 100644
---- a/bindings/pyroot/cppyy/CPyCppyy/CMakeLists.txt
-+++ b/bindings/pyroot/cppyy/CPyCppyy/CMakeLists.txt
-@@ -104,6 +104,11 @@ target_include_directories(${libname}
- 
- set_property(GLOBAL APPEND PROPERTY ROOT_EXPORTED_TARGETS ${libname})
- 
-+if(NOT MSVC)
-+  # Make sure that relative RUNPATH to main ROOT libraries is always correct.
-+  ROOT_APPEND_LIBDIR_TO_INSTALL_RPATH(${libname} ${CMAKE_INSTALL_PYTHONDIR})
-+endif()
-+
- # Install library
- install(TARGETS ${libname} EXPORT ${CMAKE_PROJECT_NAME}Exports
-                             RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT libraries
-diff --git a/bindings/pyroot/cppyy/cppyy-backend/CMakeLists.txt b/bindings/pyroot/cppyy/cppyy-backend/CMakeLists.txt
-index f296f3886d..25862bfa44 100644
---- a/bindings/pyroot/cppyy/cppyy-backend/CMakeLists.txt
-+++ b/bindings/pyroot/cppyy/cppyy-backend/CMakeLists.txt
-@@ -38,6 +38,11 @@ add_dependencies(${libname} move_headers)
- 
- set_property(GLOBAL APPEND PROPERTY ROOT_EXPORTED_TARGETS ${libname})
- 
-+if(NOT MSVC)
-+  # Make sure that relative RUNPATH to main ROOT libraries is always correct.
-+  ROOT_APPEND_LIBDIR_TO_INSTALL_RPATH(${libname} ${CMAKE_INSTALL_PYTHONDIR})
-+endif()
-+
- # Install library
- install(TARGETS ${libname} EXPORT ${CMAKE_PROJECT_NAME}Exports
-                             RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT libraries
-diff --git a/bindings/pyroot/pythonizations/CMakeLists.txt b/bindings/pyroot/pythonizations/CMakeLists.txt
-index e222e04c0e..30ef38edd9 100644
---- a/bindings/pyroot/pythonizations/CMakeLists.txt
-+++ b/bindings/pyroot/pythonizations/CMakeLists.txt
-@@ -210,6 +210,11 @@ endforeach()
- add_library(PyROOT INTERFACE)
- target_link_libraries(PyROOT INTERFACE cppyy_backend cppyy ROOTPythonizations)
- 
-+if(NOT MSVC)
-+  # Make sure that relative RUNPATH to main ROOT libraries is always correct.
-+  ROOT_APPEND_LIBDIR_TO_INSTALL_RPATH(${libname} ${CMAKE_INSTALL_PYTHONDIR})
-+endif()
-+
- # Install library
- install(TARGETS ${libname} EXPORT ${CMAKE_PROJECT_NAME}Exports
-                             RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR} COMPONENT libraries
