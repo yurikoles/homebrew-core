7f8392c33a92dfe68c1f580b49e9aeb967ba549e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-22 02:58:22 +0000

    mcp-server-chart 0.9.9

--- Formula/m/mcp-server-chart.rb
+++ Formula/m/mcp-server-chart.rb
@@ -1,8 +1,8 @@
 class McpServerChart < Formula
   desc "MCP with 25+ @antvis charts for visualization, generation, and analysis"
   homepage "https://github.com/antvis/mcp-server-chart"
-  url "https://registry.npmjs.org/@antv/mcp-server-chart/-/mcp-server-chart-0.9.7.tgz"
-  sha256 "bde0d30ffb84bf5593e301de57853d3b907e2f1c3d1778455ee4e5921a30d38d"
+  url "https://registry.npmjs.org/@antv/mcp-server-chart/-/mcp-server-chart-0.9.9.tgz"
+  sha256 "7c857085593404b2ff3092c1ded37b86d6304738de271c1a90391b60c81edbca"
   license "MIT"
 
   bottle do
