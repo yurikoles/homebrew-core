daa9a54f4dfdf964145798b0ea62d76f238c3c31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-22 22:52:02 +0000

    scala: update 3.8.1 bottle.

--- Formula/s/scala.rb
+++ Formula/s/scala.rb
@@ -11,7 +11,7 @@ class Scala < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8e29902c77acae967f07af084b1a036b46fbbdb2a22992fc1e94f1f9e763f9f1"
+    sha256 cellar: :any_skip_relocation, all: "c2e235b04ddbb1ea607c69c7bd071696ea2c4491350123b95a8acdd614fadbc5"
   end
 
   # JDK Compatibility: https://docs.scala-lang.org/overviews/jdk-compatibility/overview.html
