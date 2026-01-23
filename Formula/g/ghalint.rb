a2b81a8211aa4d348a64e703ce50d87a7e2aa242
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-23 03:47:54 +0000

    ghalint: update 1.5.5 bottle.

--- Formula/g/ghalint.rb
+++ Formula/g/ghalint.rb
@@ -7,13 +7,12 @@ class Ghalint < Formula
   head "https://github.com/suzuki-shunsuke/ghalint.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34563846ca0b2ecfa8f93b3731ef87cf701f3d3d9dec3b65d4a159b7a9cb1a15"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34563846ca0b2ecfa8f93b3731ef87cf701f3d3d9dec3b65d4a159b7a9cb1a15"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34563846ca0b2ecfa8f93b3731ef87cf701f3d3d9dec3b65d4a159b7a9cb1a15"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3698d68a7c3cc1d34dbeb90c16ec5fd64a6899b8a21ad812609ff071bba79efd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3bf871746002efddf8f508268403acc60384cb45a9bcaecbf698db0a821cf16d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "43910da32f7c8270e8c1f9a2026616f5d89fd86f1e70c331e98620fb7c20bbfd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d76a40b66cef3c662e3640e21831608994afd975421cdcade724f9c2a91cfdf0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d76a40b66cef3c662e3640e21831608994afd975421cdcade724f9c2a91cfdf0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d76a40b66cef3c662e3640e21831608994afd975421cdcade724f9c2a91cfdf0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "59af90a6c1a219d9c22e362f1c4247e6c86d467bc1fca2f1b0e6863bc40d8b87"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc504c542ae7a9a1760a766f97dec113404448ac12f60bb816fe01063928db19"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4279ba059a6609ea05324b5a7c05a8e7aab91f5b2a87a9c8b1f29aaf934b337"
   end
 
   depends_on "go" => :build
