938942cb576c10f0fa6a8405107db5c699a8b535
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-23 17:24:46 +0000

    nave 3.5.6
    nave: update license to BlueOak-1.0.0
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Formula/n/nave.rb
+++ Formula/n/nave.rb
@@ -1,9 +1,9 @@
 class Nave < Formula
   desc "Virtual environments for Node.js"
   homepage "https://github.com/isaacs/nave"
-  url "https://github.com/isaacs/nave/archive/refs/tags/v3.5.5.tar.gz"
-  sha256 "102b1fced7aad7746cbe9c1871984cea2560747f0369fb777857c1992dc09a7a"
-  license "ISC"
+  url "https://github.com/isaacs/nave/archive/refs/tags/v3.5.6.tar.gz"
+  sha256 "d5360cbfe3afda8622fa838183eec53503f8e3f4e08c224b5b1458159f30a99e"
+  license "BlueOak-1.0.0"
   head "https://github.com/isaacs/nave.git", branch: "main"
 
   bottle do
