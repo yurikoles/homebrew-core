02a58db5ed56d4226bc306f8d510252b3f3bef02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-24 20:56:34 +0000

    sandvault: update 1.1.9 bottle.

--- Formula/s/sandvault.rb
+++ Formula/s/sandvault.rb
@@ -6,10 +6,10 @@ class Sandvault < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "10d490df37f44650ebb58bc40bb90d88cc3eebaaff34bd2e75585f70d91b1bc1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "10d490df37f44650ebb58bc40bb90d88cc3eebaaff34bd2e75585f70d91b1bc1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "10d490df37f44650ebb58bc40bb90d88cc3eebaaff34bd2e75585f70d91b1bc1"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8dc5457514322df2dbec769fb33c4f616c8bb0dc32f5f1b78d9d262e718252fa"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "118accd0fa5ca49e0ad397914cbc697ec187529db4ff6d860a9a30cdeb8f9eb5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "118accd0fa5ca49e0ad397914cbc697ec187529db4ff6d860a9a30cdeb8f9eb5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "118accd0fa5ca49e0ad397914cbc697ec187529db4ff6d860a9a30cdeb8f9eb5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b7d3790b5fc130bc34218b523973ef44bc7af5bdde19f96daadd504f322bef95"
   end
 
   depends_on :macos
