635645dc8cd2696f56ad08677f3b084c5da1c956
Michael Cho <michael@michaelcho.dev>
2026-01-24 14:10:38 -0500

    gowall 0.2.3

--- Formula/g/gowall.rb
+++ Formula/g/gowall.rb
@@ -1,8 +1,8 @@
 class Gowall < Formula
   desc "Tool to convert a Wallpaper's color scheme / palette"
   homepage "https://achno.github.io/gowall-docs/"
-  url "https://github.com/Achno/gowall/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "31992b7895211310301ca169bcc98305a1971221aa5d718033be3a45512ca9a4"
+  url "https://github.com/Achno/gowall/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "ff5289250cd1bfe7adef728c85c4c97aed906330e9bd79760be540eb49343d51"
   license "MIT"
   head "https://github.com/Achno/gowall.git", branch: "main"
 
@@ -17,11 +17,31 @@ class Gowall < Formula
   end
 
   depends_on "go" => :build
+  depends_on "mupdf"
+
+  resource "go-fitz" do
+    url "https://github.com/gen2brain/go-fitz/archive/refs/tags/v1.24.15.tar.gz"
+    sha256 "086b656bbb00c314083b7097b1d295f98034f4d75ffddf4fc706a5f1c3c5cf6b"
+  end
 
   def install
-    ENV["CGO_ENABLED"] = "1" if OS.linux? && Hardware::CPU.arm?
+    # Work around https://github.com/gen2brain/go-fitz/issues/143
+    (buildpath/"go-fitz").install resource("go-fitz")
+    (buildpath/"go.work").write <<~GOMOD
+      go #{Formula["go"].version.major_minor}
+      use .
+      replace github.com/gen2brain/go-fitz => ./go-fitz
+    GOMOD
+    inreplace "go-fitz/fitz_cgo.go", "C.int(len(buf))", "C.size_t(len(buf))"
 
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    ENV["CGO_ENABLED"] = "1" # for go-fitz
+    # Workaround to avoid patchelf corruption when cgo is required
+    if OS.linux? && Hardware::CPU.arm64?
+      ENV["GO_EXTLINK_ENABLED"] = "1"
+      ENV.append "GOFLAGS", "-buildmode=pie"
+    end
+
+    system "go", "build", *std_go_args(ldflags: "-s -w", tags: "extlib")
 
     generate_completions_from_executable(bin/"gowall", shell_parameter_format: :cobra)
   end
