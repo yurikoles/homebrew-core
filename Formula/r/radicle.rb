47f9399904a8063b6e702d572e18174d089f2130
flexagoon <code@fxgn.dev>
2026-01-24 01:56:14 +0100

    radicle 1.6.1 (new formula)

---
+++ Formula/r/radicle.rb
@@ -0,0 +1,38 @@
+class Radicle < Formula
+  desc "Sovereign code forge built on Git"
+  homepage "https://radicle.xyz"
+  url "https://files.radicle.xyz/releases/latest/heartwood-1.6.1.tar.gz"
+  sha256 "a4806357baf162263002a24fc24660dfed7a43d566a24fc5428b0948d67e2011"
+  license all_of: ["MIT", "Apache-2.0"]
+
+  depends_on "asciidoctor" => :build
+  depends_on "rust" => :build
+
+  depends_on "openssh"
+
+  uses_from_macos "zlib"
+
+  def install
+    ENV["RADICLE_VERSION"] = version.to_s
+
+    %w[radicle-cli radicle-node radicle-remote-helper].each do |bin|
+      system "cargo", "install", *std_cargo_args(path: "crates/#{bin}")
+    end
+
+    generate_completions_from_executable(bin/"rad", "completion")
+
+    system "asciidoctor", "-b", "manpage", "-d", "manpage", "*.1.adoc"
+    man1.install Dir["*.1"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rad version")
+    assert_match version.to_s, shell_output("#{bin}/radicle-node --version")
+
+    assert_match "Your Radicle DID is", pipe_output("#{bin}/rad auth --alias homebrew --stdin", "homebrew", 0)
+    assert_match "\"repos\": 0", shell_output("#{bin}/rad stats")
+    system bin/"rad", "ls"
+
+    assert_match "a passphrase is required", shell_output(bin/"radicle-node", 1)
+  end
+end
