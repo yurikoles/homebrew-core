6eb832f2d39cb1e159bea6a6393d5bc2dd316f72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-24 03:24:55 +0000

    apt: update 3.1.14 bottle.

--- Formula/a/apt.rb
+++ Formula/a/apt.rb
@@ -12,8 +12,8 @@ class Apt < Formula
   end
 
   bottle do
-    sha256 arm64_linux:  "7c90bbc28ba0370a200bc06fb57ab205f574e28375032821e001e67fcb9ab4a1"
-    sha256 x86_64_linux: "c960f838b61d94bdd36e4673386b9e3f37df86135f2e873a28d332c2fe2ca949"
+    sha256 arm64_linux:  "450c8b619042bc6dfc049b8c40cdcf868bad793e094000896e4ecddfb3c0b82e"
+    sha256 x86_64_linux: "f6b17555f4b4885490a04c65f5da5e683212addcb734e69640562f5a7b322b8b"
   end
 
   keg_only "it conflicts with system apt"
