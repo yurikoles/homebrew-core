37b23241e304353e4a2685bf88d554dad6e59390
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-25 09:40:09 +0000

    mactop: update 2.0.8 bottle.

--- Formula/m/mactop.rb
+++ Formula/m/mactop.rb
@@ -7,9 +7,9 @@ class Mactop < Formula
   head "https://github.com/metaspartan/mactop.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a8454ed378295f2f23f8b9d5e0b0f749ee49d71509e4c557b4c4c5333ca35848"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "681be53c386e7b669ec3ce9cbe6218a5fdbb2d8779944c3b1cfd5f2e4dfc5661"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8392d710c0cd3d7c4bad670ea6ee339efcb247912b2c7ed0d3d03befa5773b9b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb6c6366b353cb024ce857e424b876f96ace01ae9ac6ef85fd4a2a60590c83ec"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e9dd079197419f4d25f14979d70edb36b787e65fe629c134d3538c740797544"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7dcb74e026feed7d9fb33a21abfeeac62744da3bd4d1949bd0205d0d5eba9d7"
   end
 
   depends_on "go" => :build
