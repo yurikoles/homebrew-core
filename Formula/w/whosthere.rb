adcd1e2c0f937147e383dcf0bc764e7f34f2ce29
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2026-01-25 17:25:40 +0100

    whosthere 0.4.0 (new formula)
    - adds `whosthere` as new formula to `homebrew-core`
    - setup the linker flags to use only the commit sha on head builds
    - added additional test case which expects an error on a non-existing
    network interface

---
+++ Formula/w/whosthere.rb
@@ -0,0 +1,31 @@
+class Whosthere < Formula
+  desc "LAN discovery tool with a modern TUI written in Go"
+  homepage "https://github.com/ramonvermeulen/whosthere"
+  url "https://github.com/ramonvermeulen/whosthere/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "5133bb75c0e8fd7296910012f1a7cf576ecf438771593a074e40e7eee4330d38"
+  license "Apache-2.0"
+  head "https://github.com/ramonvermeulen/whosthere.git", branch: "main"
+
+  livecheck do
+    url :stable
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.versionStr=#{version}
+      -X main.dateStr=#{Time.now.utc.iso8601}
+    ]
+
+    ldflags << "-X main.commitStr=#{Utils.git_short_head}" if build.head?
+    system "go", "build", *std_go_args(ldflags: ldflags)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/whosthere --version")
+    output = shell_output("#{bin}/whosthere --interface non_existing 2>&1", 1)
+    assert_match "no such network interface", output
+  end
+end
