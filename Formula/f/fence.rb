d30bf5a4cb596a1c2e8468e4c973f23c445f5151
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 23:43:05 +0000

    fence: update 0.1.16 bottle.

--- Formula/f/fence.rb
+++ Formula/f/fence.rb
@@ -7,12 +7,12 @@ class Fence < Formula
   head "https://github.com/Use-Tusk/fence.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3fc27831a6f1e552585cd2450a504c4f8e78dd3924c9e768a7289c2c9aff4c17"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0c263c89a109ae466aa6d43762f178ec47bce77206dc717e97c38e12eca31a69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "975a5bfb24ba5368778dc3478a6e13122d6b094d13f5aa7a5174055f9ddb4d31"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fc4331fbd9ad4b677fda1734b4a66d0bfd0539c9ecd9276f67d292be97d29066"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc4331fbd9ad4b677fda1734b4a66d0bfd0539c9ecd9276f67d292be97d29066"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fc4331fbd9ad4b677fda1734b4a66d0bfd0539c9ecd9276f67d292be97d29066"
+    sha256 cellar: :any_skip_relocation, sonoma:        "439a9f7d18d734dbe892936ebabcc15da0f5ba65bcece368fbdf4fa1f0ef73f2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7d8381e129eb0fcce47a01cc2b7149661c2b530f557a145f466b0540be7d7cc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f52a74f4630db8353d38bfbe705a7ab21de91f7cdb5d72a89d13a37aee4a25e"
   end
 
   depends_on "go" => :build
