27aad0f01baad394bd668e3ed97020fc751621fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 06:16:58 +0000

    vampire: add 5.0.1 bottle.

--- Formula/v/vampire.rb
+++ Formula/v/vampire.rb
@@ -5,6 +5,15 @@ class Vampire < Formula
   sha256 "3d991c914e9f400641d8b2e4362065c218c0ecb08079b96b0da1714aa6842520"
   license "BSD-3-Clause"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f7ddec00fee445a0e1bfd5caca53a28e4a30e41fcb0cdfe4e9c5497bfc78f70d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0cd98ea75f19465e88f05a2da929d1758c71989744ed1035d58413ca5a0a16d0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bedd8ba34e0e8e1f5752b14cd7c812787f3e3cec5f2b2b7a74d6e17cced04fba"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e6b03fb0f7a8a6461626d8548a762fe7ae49d5c42792a47758cf2ccf45e7e333"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "41ade77fd28eef439c3133270c8f8f99ee598266d5b83243ebbe79f99b62632d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c71ff5f94b3d9cfb3a346e2c0c85c3b16ec51a378f8b201fccd45657d4ee104"
+  end
+
   depends_on "cmake" => :build
 
   on_macos do
