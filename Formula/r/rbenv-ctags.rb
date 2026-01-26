5b06cfbe581dcdfaab47d7bf3a9ce4349a59ca34
Michael Cho <michael@michaelcho.dev>
2026-01-26 14:53:07 -0500

    rbenv-ctags: migrate to maintained `universal-ctags`

--- Formula/r/rbenv-ctags.rb
+++ Formula/r/rbenv-ctags.rb
@@ -14,8 +14,8 @@ class RbenvCtags < Formula
     sha256 cellar: :any_skip_relocation, all: "93ff69e4515aae2a296578472b395c3bc8721fc711327c6f2a9126111910700e"
   end
 
-  depends_on "ctags"
   depends_on "rbenv"
+  depends_on "universal-ctags"
 
   def install
     prefix.install Dir["*"]
