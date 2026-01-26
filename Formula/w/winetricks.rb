612b82a92e917bcdd041fe3a6422e38cabfa811f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 03:26:41 +0000

    winetricks: update 20260125 bottle.

--- Formula/w/winetricks.rb
+++ Formula/w/winetricks.rb
@@ -12,7 +12,7 @@ class Winetricks < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "8206662a0e6dc4617ffb4b6c6c3688dd39e019291e95e91eec0e0c2a49b4fcf1"
+    sha256 cellar: :any_skip_relocation, all: "ae2359e4cc84f0b29407d3e4d8824c14ea950f107016b24bc2edf4c01b54485c"
   end
 
   depends_on "cabextract"
