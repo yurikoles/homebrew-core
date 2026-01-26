6fd960a5ad37d7e0128c701eb9aca9bd9b709e7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-26 11:18:45 +0000

    fence: add 0.1.15 bottle.

--- Formula/f/fence.rb
+++ Formula/f/fence.rb
@@ -6,6 +6,15 @@ class Fence < Formula
   license "Apache-2.0"
   head "https://github.com/Use-Tusk/fence.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "677b9543389ace25a731684860474e6190967a2bc30c77543bca8d92e66a574e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3fc27831a6f1e552585cd2450a504c4f8e78dd3924c9e768a7289c2c9aff4c17"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0c263c89a109ae466aa6d43762f178ec47bce77206dc717e97c38e12eca31a69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "975a5bfb24ba5368778dc3478a6e13122d6b094d13f5aa7a5174055f9ddb4d31"
+  end
+
   depends_on "go" => :build
 
   on_linux do
