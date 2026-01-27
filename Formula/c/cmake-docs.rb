804930bb546599f5506c0e9ff4eb96b298736104
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 19:56:25 +0000

    cmake-docs 4.2.3

--- Formula/c/cmake-docs.rb
+++ Formula/c/cmake-docs.rb
@@ -1,10 +1,10 @@
 class CmakeDocs < Formula
   desc "Documentation for CMake"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v4.2.2/cmake-4.2.2.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-4.2.2.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-4.2.2.tar.gz"
-  sha256 "bbda94dd31636e89eb1cc18f8355f6b01d9193d7676549fba282057e8b730f58"
+  url "https://github.com/Kitware/CMake/releases/download/v4.2.3/cmake-4.2.3.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-4.2.3.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-4.2.3.tar.gz"
+  sha256 "7efaccde8c5a6b2968bad6ce0fe60e19b6e10701a12fce948c2bf79bac8a11e9"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
