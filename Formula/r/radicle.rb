63c8c445e915ed0487bee8e2f586c5d5e120f1c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 13:26:33 +0000

    radicle: add 1.6.1 bottle.

--- Formula/r/radicle.rb
+++ Formula/r/radicle.rb
@@ -5,6 +5,15 @@ class Radicle < Formula
   sha256 "a4806357baf162263002a24fc24660dfed7a43d566a24fc5428b0948d67e2011"
   license all_of: ["MIT", "Apache-2.0"]
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4e11422998700c5249fcc8f64455ff641759a1cd54c48e8e8f1247e380ff36db"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8cd7a08c94ba66d98f47921a52f8ffe7c07906043ad58b9c5a0efb871565f2e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e5a94672e5c8b0c9929b278b32b740066f9af2cfa2538dfb930fd79816a6428c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c65df15d763aca75824479d0e904e121614a583007cff87c47d41eb3f7969e5d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4edae9386a5a06eda3155f6bc4e82b2b6c780761a17824d4b2e317f35a06ab7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "96457b9b2e4acd952d08749b0f2937e77630d3789e810a179494d1da3d223866"
+  end
+
   depends_on "asciidoctor" => :build
   depends_on "rust" => :build
 
