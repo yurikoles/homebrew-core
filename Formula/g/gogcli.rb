84f663d955d361bb21c50b5f8c51a63319ebbdfe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 13:19:41 +0000

    gogcli: add 0.9.0 bottle.

--- Formula/g/gogcli.rb
+++ Formula/g/gogcli.rb
@@ -6,6 +6,15 @@ class Gogcli < Formula
   license "MIT"
   head "https://github.com/steipete/gogcli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e847f2f0cae94088a37f6dcfc0d490f2dea13df027508fcdcbdb746f6baade2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd59d35fe9580d181c63b587cec89b85cb72fd7d28a232d7022395d0d3946223"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ee455a27c6fb17e792a10df2ecf0bc2928e381542b97a8b1f17de0fa31f772f5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b6738abf97848ee8fb4232621d00f81e4836e3b5fb8a3e7cfd28e4527ca071e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2982fbffd775f33e0dd136af63d5eac4a139dd1e53df6c6ed06ec23ea159d954"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "65c5dadce663da106a4755f0f1f683121ca607508bd95a5764a3ce1fdc519057"
+  end
+
   depends_on "go" => :build
 
   def install
