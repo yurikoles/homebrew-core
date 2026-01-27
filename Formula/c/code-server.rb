465eaf8c701f8933eea2d2399233dd0a4059b4da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 04:06:36 +0000

    code-server: update 4.108.2 bottle.

--- Formula/c/code-server.rb
+++ Formula/c/code-server.rb
@@ -6,12 +6,12 @@ class CodeServer < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c816f5a0538d4b153652c8ad2dbc64ed0c940ccdea0d2324b6c3ae502e640fd7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "63f0f2fe5b282b8adabf1332623e4db56e977ea59962c18a60e34650a37af1f8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d34a561dc2c5f6af232a4a41e5fb19ec44746e0231dd5d9ce698cc5d8474c557"
-    sha256 cellar: :any_skip_relocation, sonoma:        "417efd2ce3c593f2bcf4dddf71c407fb61ee0b6aff7a398c1b579d9b2402c6d8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "95c29a0defac05254d2ab606317b6674fc48eca299641c7b7b83a77eb0592e8d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59229dd3c3b56c21f4f7a8cb19f933dd2993b157280d2e14f2678b31e6b23184"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a3f0ddd6eb6c7fc6e35ef4b30a7a317f500a19b7e56d704cde86286111b6e7ea"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "476a01757451165df7af02da5b6fb9fdd913e3756fcbadb1bb8ad50a44031b20"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "91f8fb98c626f25e30e71f58f984d43693d266c1ac34310f1f9deea064d22651"
+    sha256 cellar: :any_skip_relocation, sonoma:        "e0c6dbb00567b9dc10ff44fb43a243111470b63b6eced2ce13b6b4c8c0581fcb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c32cfedb24e730cc73a279e8089bfd9b5bed49285b7ddb313ac631732eb9a25"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed63830bdd367a40a2d077d820c073078b4839383371ae749bad43487f718e21"
   end
 
   depends_on "pkgconf" => :build
