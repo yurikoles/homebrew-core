d97eebe43f35af35448848154b88ba2eb7edffba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 20:08:05 +0000

    libks 2.0.10

--- Formula/lib/libks.rb
+++ Formula/lib/libks.rb
@@ -1,8 +1,8 @@
 class Libks < Formula
   desc "Foundational support for signalwire C products"
   homepage "https://github.com/signalwire/libks"
-  url "https://github.com/signalwire/libks/archive/refs/tags/v2.0.9.tar.gz"
-  sha256 "c3a352b49a0452a710e80ad9c19a318c4d946a33b5ff6094617f3d4cb9ca02e2"
+  url "https://github.com/signalwire/libks/archive/refs/tags/v2.0.10.tar.gz"
+  sha256 "cd0d8504870c2e0e1306e55fd27dede976ab9f3a919487bc10b526576d24d568"
   license all_of: [
     "MIT",
     "BSD-3-Clause", # src/ks_hash.c
