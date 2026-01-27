6e7cca8224b9e242ce2da147df78c596a8514812
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 03:56:20 +0000

    kagent: update 0.7.12 bottle.

--- Formula/k/kagent.rb
+++ Formula/k/kagent.rb
@@ -7,12 +7,12 @@ class Kagent < Formula
   head "https://github.com/kagent-dev/kagent.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "850ffb6a7b7e8f5b14235ca5e8ac9ab41746bf02be66106c660120975617402c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "850ffb6a7b7e8f5b14235ca5e8ac9ab41746bf02be66106c660120975617402c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "850ffb6a7b7e8f5b14235ca5e8ac9ab41746bf02be66106c660120975617402c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "db47822d0560cb54ec68b2c478c0e0152625aa40854cc87079e6961420d4b53e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e15125fd468585c860710e59a5473d1efa4acb3d37be058d09a49f0770523277"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdb319a0a8e97c1bf7fb5140970d9db0eb947b120aab45d65e6f7aa9eacdc325"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8b20f88f2e271f64e63bb34e01ac165673512f1315bdb75419b0d5e7436b9397"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b20f88f2e271f64e63bb34e01ac165673512f1315bdb75419b0d5e7436b9397"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8b20f88f2e271f64e63bb34e01ac165673512f1315bdb75419b0d5e7436b9397"
+    sha256 cellar: :any_skip_relocation, sonoma:        "772e2ec5435a5efa0bd61397ee7843d8863f9dd0c8eef52356b8ec26efe61001"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b7a227a368b80def976108c0f0e74b49d9dfa57d5ef715ce2d1e215d98204a10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ffb33763af2b586cb9112d4122e598f26d29b1d2a722b936a910de802de6fd4"
   end
 
   depends_on "go" => :build
