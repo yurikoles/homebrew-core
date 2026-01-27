02141e0600c35d18442948c02d32d435e0cabd0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 00:06:35 +0000

    nx: update 22.4.2 bottle.

--- Formula/n/nx.rb
+++ Formula/n/nx.rb
@@ -7,12 +7,12 @@ class Nx < Formula
   version_scheme 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "653c59d4f376a649f55d7f3265e34a54f8ec2fbbcbf8dbfffbacecfc31419147"
-    sha256 cellar: :any,                 arm64_sequoia: "59da0c7aceb47d7858697a463ef805d50490454f8de0d43e05e4e741093c0d41"
-    sha256 cellar: :any,                 arm64_sonoma:  "59da0c7aceb47d7858697a463ef805d50490454f8de0d43e05e4e741093c0d41"
-    sha256 cellar: :any,                 sonoma:        "55c0f24371570558d2d062071f37e212b6c03b1bfb426fe698e944051cf83380"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c008df187202518e0c192990fdd63f26e615c1eafb644378bb66a8e4ea4f010"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "039fb3587b4de25be8d9d844525389bf6dbf73581e3e50731cd85ffc9654cb3a"
+    sha256 cellar: :any,                 arm64_tahoe:   "cf534671f15a39c9e33ec4e79382fdea5623fd2997dd65da0a0ff91acc7b8476"
+    sha256 cellar: :any,                 arm64_sequoia: "e4dc26d8d8f5b603e9a2e514d40f75de4ad4714416ef6aa7a85f469b3e408a2f"
+    sha256 cellar: :any,                 arm64_sonoma:  "e4dc26d8d8f5b603e9a2e514d40f75de4ad4714416ef6aa7a85f469b3e408a2f"
+    sha256 cellar: :any,                 sonoma:        "c8697099e697f08d1a99a0b84ba9e28c0d442a56b8acecaf21a9b939657658c0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "37097333ed8ffd449ba2286768f36322cdfb8f9fe437432a9090ebdc372ddf67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2f18fabd964fc3743a606685084e113e45beed2187eac0370d7702ebe29838e"
   end
 
   depends_on "node"
