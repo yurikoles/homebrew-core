c1ba57e623e91d6cd1493873767000d9ee105a11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-28 19:55:32 +0000

    babl 0.1.122

--- Formula/b/babl.rb
+++ Formula/b/babl.rb
@@ -1,8 +1,8 @@
 class Babl < Formula
   desc "Dynamic, any-to-any, pixel format translation library"
   homepage "https://www.gegl.org/babl/"
-  url "https://download.gimp.org/pub/babl/0.1/babl-0.1.120.tar.xz"
-  sha256 "f476ad15201fb4ed0c90c174c524b1e4271ccd69a377242d6a69fcdf87ceacc2"
+  url "https://download.gimp.org/pub/babl/0.1/babl-0.1.122.tar.xz"
+  sha256 "6851f705cda38f2df08a4ba8618279ce30d0a46f957fe6aa325b7b7de297bed2"
   license "LGPL-3.0-or-later"
   # Use GitHub instead of GNOME's git. The latter is unreliable.
   head "https://github.com/GNOME/babl.git", branch: "master"
