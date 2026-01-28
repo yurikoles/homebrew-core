375d27df515cf3257a9be9c3b1a56a7f81884d59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-28 17:16:17 +0000

    hierarchy-builder 1.10.2

--- Formula/h/hierarchy-builder.rb
+++ Formula/h/hierarchy-builder.rb
@@ -1,10 +1,9 @@
 class HierarchyBuilder < Formula
   desc "High level commands to declare a hierarchy based on packed classes"
   homepage "https://github.com/math-comp/hierarchy-builder"
-  url "https://github.com/math-comp/hierarchy-builder/releases/download/v1.10.1/hierarchy-builder-1.10.1.tar.gz"
-  sha256 "8fa555024ffee5892b3bf516db2ade1378d9902479fa6109c8f047bf760cdfa4"
+  url "https://github.com/math-comp/hierarchy-builder/releases/download/v1.10.2/hierarchy-builder-1.10.2.tar.gz"
+  sha256 "bdd7dccc5248e7500e02fd1745fd17faa41920f43a32ca2de2b2139212ee53b5"
   license "MIT"
-  revision 2
 
   livecheck do
     url :stable
