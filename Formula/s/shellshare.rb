3234d339c485f8cdd35a92bcba22f217a1b94d6d
botantony <antonsm21@gmail.com>
2026-01-28 23:20:10 +0100

    shellshare 1.2.0

--- Formula/s/shellshare.rb
+++ Formula/s/shellshare.rb
@@ -3,11 +3,9 @@ class Shellshare < Formula
 
   desc "Live Terminal Broadcast"
   homepage "https://github.com/vitorbaptista/shellshare"
-  url "https://github.com/vitorbaptista/shellshare/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "0a102c863f60402ab48908563edde38450add0ae02454360fa94065824a61907"
+  url "https://github.com/vitorbaptista/shellshare/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "bc56d21bcef73aa2c40cd689c84cf8a0c8d2682e76b1f48e8f7ff53b7325f72e"
   license "Apache-2.0"
-
-  no_autobump! because: :requires_manual_review
 
   bottle do
     rebuild 1
