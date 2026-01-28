9479c0992bbb9aa9a58460b97caea6838f183279
Michael Cho <michael@michaelcho.dev>
2026-01-28 18:43:20 -0500

    cp2k 2026.1

--- Formula/c/cp2k.rb
+++ Formula/c/cp2k.rb
@@ -1,8 +1,8 @@
 class Cp2k < Formula
   desc "Quantum chemistry and solid state physics software package"
   homepage "https://www.cp2k.org/"
-  url "https://github.com/cp2k/cp2k/releases/download/v2025.1/cp2k-2025.1.tar.bz2"
-  sha256 "65c8ad5488897b0f995919b9fa77f2aba4b61677ba1e3c19bb093d5c08a8ce1d"
+  url "https://github.com/cp2k/cp2k/releases/download/v2026.1/cp2k-2026.1.tar.bz2"
+  sha256 "4364c74bcffaa474bc234e11686b09550e4d06932acf2147a341e4f7679dd88e"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -24,6 +24,7 @@ class Cp2k < Formula
   depends_on "fypp" => :build
   depends_on "pkgconf" => :build
 
+  depends_on "dbcsr"
   depends_on "fftw"
   depends_on "gcc" # for gfortran
   depends_on "libint"
@@ -34,67 +35,48 @@ class Cp2k < Formula
 
   uses_from_macos "python" => :build
 
-  fails_with :clang do
-    cause "needs OpenMP support for C/C++ and Fortran"
+  on_macos do
+    depends_on "libomp"
   end
 
   def install
-    # TODO: Add workaround to link to LLVM OpenMP (libomp) with gfortran after migrating OpenBLAS
-    omp_args = []
-    # if OS.mac?
-    #   omp_args << "-DOpenMP_Fortran_LIB_NAMES=omp"
-    #   omp_args << "-DOpenMP_omp_LIBRARY=#{Formula["libomp"].opt_lib}/libomp.dylib"
-    # end
-
-    # TODO: Remove dbcsr build along with corresponding CMAKE_PREFIX_PATH
-    # and add -DCP2K_BUILD_DBCSR=ON once `cp2k` build supports this option.
-    system "cmake", "-S", "exts/dbcsr", "-B", "build_psmp/dbcsr",
-                    "-DBUILD_SHARED_LIBS=ON",
-                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
-                    "-DWITH_EXAMPLES=OFF",
-                    *omp_args, *std_cmake_args(install_prefix: libexec)
-    system "cmake", "--build", "build_psmp/dbcsr"
-    system "cmake", "--install", "build_psmp/dbcsr"
-    # Need to build another copy for non-MPI variant.
-    system "cmake", "-S", "exts/dbcsr", "-B", "build_ssmp/dbcsr",
-                    "-DUSE_MPI=OFF",
-                    "-DWITH_EXAMPLES=OFF",
-                    *omp_args, *std_cmake_args(install_prefix: buildpath/"dbcsr")
-    system "cmake", "--build", "build_ssmp/dbcsr"
-    system "cmake", "--install", "build_ssmp/dbcsr"
+    # Avoid over-optimizing fortran code as we don't have a shim for gfortran
+    optflags = ENV["HOMEBREW_OPTFLAGS"].to_s.split.join(";")
+    inreplace "cmake/CompilerConfiguration.cmake", "-march=native;-mtune=native", optflags
 
     # Avoid trying to access /proc/self/statm on macOS
     ENV.append "FFLAGS", "-D__NO_STATM_ACCESS" if OS.mac?
 
-    cp2k_cmake_args = %W[
-      -DCMAKE_INSTALL_RPATH=#{rpath};#{rpath(target: libexec/"lib")}
+    args = %W[
+      -DBUILD_SHARED_LIBS=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
       -DCP2K_BLAS_VENDOR=OpenBLAS
+      -DCP2K_USE_FFTW3=ON
       -DCP2K_USE_LIBINT2=ON
       -DCP2K_USE_LIBXC=ON
-    ] + omp_args + std_cmake_args
+      -DCP2K_USE_MPI=ON
+      -DCP2K_USE_MPI_F08=ON
+    ]
+    if OS.mac?
+      args += %W[
+        -DOpenMP_Fortran_LIB_NAMES=omp
+        -DOpenMP_omp_LIBRARY=#{Formula["libomp"].opt_lib}/libomp.dylib
+      ]
+    end
 
-    system "cmake", "-S", ".", "-B", "build_psmp/cp2k",
-                    "-DCMAKE_PREFIX_PATH=#{libexec}",
-                    *cp2k_cmake_args
-    system "cmake", "--build", "build_psmp/cp2k"
-    system "cmake", "--install", "build_psmp/cp2k"
-
-    # Only build the main executable for non-MPI variant as libs conflict.
-    # Can consider shipping MPI and non-MPI variants as separate formulae
-    # or removing one variant depending on usage.
-    system "cmake", "-S", ".", "-B", "build_ssmp/cp2k",
-                    "-DBUILD_SHARED_LIBS=OFF",
-                    "-DCMAKE_PREFIX_PATH=#{buildpath}/dbcsr;#{libexec}",
-                    "-DCP2K_USE_MPI=OFF",
-                    *cp2k_cmake_args
-    system "cmake", "--build", "build_ssmp/cp2k", "--target", "cp2k-bin"
-    bin.install Dir["build_ssmp/cp2k/bin/*.ssmp"]
-
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
     (pkgshare/"tests").install "tests/Fist/water.inp"
   end
 
   test do
-    system bin/"cp2k.ssmp", pkgshare/"tests/water.inp"
-    system "mpirun", bin/"cp2k.psmp", pkgshare/"tests/water.inp"
+    if OS.mac?
+      require "utils/linkage"
+      libgomp = Formula["gcc"].opt_lib/"gcc/current/libgomp.dylib"
+      refute Utils.binary_linked_to_library?(lib/"libcp2k.dylib", libgomp), "Unwanted linkage to libgomp!"
+    end
+
+    system Formula["open-mpi"].bin/"mpirun", bin/"cp2k.psmp", pkgshare/"tests/water.inp"
   end
 end
