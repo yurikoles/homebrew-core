d89e1b88233517ceccaecb4260d691d30c6dc6f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-29 05:02:27 +0000

    cargo-features-manager: add 0.11.1 bottle.

--- Formula/c/cargo-features-manager.rb
+++ Formula/c/cargo-features-manager.rb
@@ -6,6 +6,15 @@ class CargoFeaturesManager < Formula
   license "MIT"
   head "https://github.com/ToBinio/cargo-features-manager.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "94ce47b67a56b1faa492c358ac28983b61ddbadb4ad0d6dd43271ff2a47c2c66"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e1846ea47e4dd663b7cc0265376036d919fcabe84af45c232a62a81a5391664"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9827f2046ce1e5020c57e36fc760718b1f0c7f4e2719d8eed672dd454b0fdb37"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6b329a740b87964ff97724159cc0ba7c3a3fea0decdfd493bedf7f02a7c9c64c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d8b51411bdcdd331629eda6404f0e3d0befce1402940636168e5bddeccf0fc03"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40e2a29fa648d6c5c128e572134933dee66a4353709ff799757b81bf875b2a07"
+  end
+
   depends_on "rust" => :build
   depends_on "rustup" => :test
 
