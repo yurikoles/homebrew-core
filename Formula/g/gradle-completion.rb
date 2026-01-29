a3809b0c888077ecf20dbc327a75cef98b896fa8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-29 17:19:26 +0000

    gradle-completion 9.3.1

--- Formula/g/gradle-completion.rb
+++ Formula/g/gradle-completion.rb
@@ -1,8 +1,8 @@
 class GradleCompletion < Formula
   desc "Bash and Zsh completion for Gradle"
   homepage "https://gradle.org/"
-  url "https://github.com/gradle/gradle-completion/archive/refs/tags/v9.3.0.tar.gz"
-  sha256 "07e6fc54a215abcd9c1a9a28e45607c666f20d72d6d68aac69a90e66c59f55e5"
+  url "https://github.com/gradle/gradle-completion/archive/refs/tags/v9.3.1.tar.gz"
+  sha256 "ad1578915f793074fedd180fc2fb87390af9ee1a8ebf63d09e0af4a5fa4f1c4e"
   license "MIT"
   head "https://github.com/gradle/gradle-completion.git", branch: "master"
 
