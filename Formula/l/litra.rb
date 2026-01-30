cfbc27408b81ff26cb74e65eca502f75bcdccc6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 04:05:16 +0000

    litra: add 3.1.0 bottle.

--- Formula/l/litra.rb
+++ Formula/l/litra.rb
@@ -6,6 +6,15 @@ class Litra < Formula
   license "MIT"
   head "https://github.com/timrogers/litra-rs.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7c6f2b88475cb118a19e3668b15ce928bf2e4fa31c2124409ff04af1a0eafc67"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cac5f7dae722ea35cd485cad20d67675f3e52ad5028cdc45c77b0ee625b47970"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "369766ce4d20c5be2d25f770a716a1f65fb1f82d40a9afc80b57e29bfef269d4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f56c0417c7b63273a7886469dc99cf2c2fcccab270dca2cba4b2b5b384dacabf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "80bacf3f67b49fc6a1b78aa63475e917cd2a0ca2d5a258ea196b348314cdea3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8115333877484cbfa51207c607d8b2fa66aacd5de27d4a37f7a652870308f60b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
