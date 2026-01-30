b709cad9d7cf03d334b6998e8981b9dd3a2f6348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 11:23:51 +0000

    mysqltuner 2.8.32

--- Formula/m/mysqltuner.rb
+++ Formula/m/mysqltuner.rb
@@ -1,8 +1,8 @@
 class Mysqltuner < Formula
   desc "Increase performance and stability of a MySQL installation"
   homepage "https://mysqltuner.com/"
-  url "https://github.com/jmrenouard/MySQLTuner-perl/archive/refs/tags/v2.8.29.tar.gz"
-  sha256 "5de9b127abe7455c942b2790fe0558a5c415c07582121ed59c9761bf26b0c9c3"
+  url "https://github.com/jmrenouard/MySQLTuner-perl/archive/refs/tags/v2.8.32.tar.gz"
+  sha256 "85028fa25c7a9309af76bd50ea49f4b88f42b033eaca59c564edd70f08b18b7b"
   license "GPL-3.0-or-later"
   head "https://github.com/jmrenouard/MySQLTuner-perl.git", branch: "master"
 
