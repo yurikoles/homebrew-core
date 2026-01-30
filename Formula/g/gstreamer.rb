4dff079a4d3b2a20a078b7400b26714744e42cc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-30 04:11:40 +0000

    gstreamer: update 1.28.0 bottle.

--- Formula/g/gstreamer.rb
+++ Formula/g/gstreamer.rb
@@ -24,12 +24,12 @@ class Gstreamer < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "a6501828af9ce32b2089e0793b106cde74fd6fbe1c855e6842a9a74748e84b90"
-    sha256 arm64_sequoia: "2daeef4894b2e1456f3446a326dd26008c3e387b8a1baa5f51a3489770825cbd"
-    sha256 arm64_sonoma:  "80779d46e04f90591cc18d18c653dd43acdd372896bd1b3ee4dde2be819b6ab1"
-    sha256 sonoma:        "f200824778c0dec095c2f8bab0ba62763aab85bb5cb0e4ec40e0a02bb191895c"
-    sha256 arm64_linux:   "cc45e957d12e8100e6876e98f32e77c96a0d1526efb841d18547d07c9b78aa43"
-    sha256 x86_64_linux:  "a0675faa09c52b3668004244682cf13432cfa56cd9c9880eb9047a8cdd77291c"
+    sha256 arm64_tahoe:   "9bdbf1ff97edf21a691506fce5550a40ad69a788f10fd1eac091e20982156613"
+    sha256 arm64_sequoia: "9c7e4e152050a631b6db14ab416d8296012adb6c0bae95f6874b6cacf8aaccdb"
+    sha256 arm64_sonoma:  "8dfbbc216de1016b8c7873816e68ef8d3d1342cd9dbb446af1ae203ed0b88d98"
+    sha256 sonoma:        "f7d5ae01fba98a32bbba80334ec1fcb62e90d9b177f7fe3e77d826af79fc7811"
+    sha256 arm64_linux:   "37a0fb559ec175ff363122dda2ee2a7e321e0e2b63b50e0718302adfd738ac5b"
+    sha256 x86_64_linux:  "49c1be5bf635413254b9eb523676d4d724f8e7991453eeeb215951780b40ed0a"
   end
 
   head do
