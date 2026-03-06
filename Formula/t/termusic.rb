e258ecc0440035171b0192579e7b2ee5e4737463
Porkepix <porkepix@gmail.com>
2026-03-06 12:24:00 +0100

    termusic: add features, adjust dependencies
    Also add a head stanza

--- Formula/t/termusic.rb
+++ Formula/t/termusic.rb
@@ -4,6 +4,7 @@ class Termusic < Formula
   url "https://github.com/tramhao/termusic/archive/refs/tags/v0.12.1.tar.gz"
   sha256 "686f66856d755f2d2056a9548f074b11ba9568ac8075fafd8903e332bf166227"
   license all_of: ["MIT", "GPL-3.0-or-later"]
+  head "https://github.com/tramhao/termusic.git", branch: "master"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c2f883e043839db16ac4f8de8add8b972de0e008463f714e7a19c89cb76be45"
@@ -17,16 +18,20 @@ class Termusic < Formula
   depends_on "pkgconf" => :build
   depends_on "protobuf" => :build
   depends_on "rust" => :build
-  depends_on "sound-touch" => :build
   depends_on "openssl@3"
+  depends_on "opus"
+
+  uses_from_macos "llvm" => :build # for libclang
 
   on_linux do
     depends_on "alsa-lib"
+    depends_on "libgccjit"
   end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "tui", features: "cover-viuer-iterm")
-    system "cargo", "install", *std_cargo_args(path: "server")
+    server_features = %w[rusty-libopus rusty-simd rusty-soundtouch]
+    system "cargo", "install", *std_cargo_args(path: "tui")
+    system "cargo", "install", *std_cargo_args(path: "server", features: server_features)
   end
 
   test do
