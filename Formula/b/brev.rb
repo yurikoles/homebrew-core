c476a98cc452e94efcf98f574367f2a39165140a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 23:04:21 +0000

    brev 0.6.319

--- Formula/b/brev.rb
+++ Formula/b/brev.rb
@@ -1,8 +1,8 @@
 class Brev < Formula
   desc "CLI tool for managing workspaces provided by brev.dev"
   homepage "https://developer.nvidia.com/brev"
-  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.318.tar.gz"
-  sha256 "da3c6673da7d84df545c28efa5dee728e6c78769f568a040daefa67e3dcda6ed"
+  url "https://github.com/brevdev/brev-cli/archive/refs/tags/v0.6.319.tar.gz"
+  sha256 "50c8d176aca85ee4dcd33e29133040a5c4b58404351b0d6a65e4da3cc91cb46e"
   license "MIT"
   head "https://github.com/brevdev/brev-cli.git", branch: "main"
 
