6bb88c42b0bf16291c9aa0b2c924e819e1154ad6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 15:30:51 +0000

    zsh-completions 0.36.0

--- Formula/z/zsh-completions.rb
+++ Formula/z/zsh-completions.rb
@@ -1,8 +1,8 @@
 class ZshCompletions < Formula
   desc "Additional completion definitions for zsh"
   homepage "https://github.com/zsh-users/zsh-completions"
-  url "https://github.com/zsh-users/zsh-completions/archive/refs/tags/0.35.0.tar.gz"
-  sha256 "811bb4213622720872e08d6e0857f1dd7bc12ff7aa2099a170b76301a53f4fbe"
+  url "https://github.com/zsh-users/zsh-completions/archive/refs/tags/0.36.0.tar.gz"
+  sha256 "5aa68be2999a7be2eb56de8e4acff8f3bba4a66b9acbb233752536857408fb2e"
   # The main/default license is the same as Zsh (MIT-Modern-Variant); however,
   # the majority of completions are BSD-3-Clause. The remainder is mostly MIT.
   # A few completions use other licenses and are specifically noted below.
@@ -25,6 +25,7 @@ class ZshCompletions < Formula
 
   def install
     inreplace "src/_ghc", "/usr/local", HOMEBREW_PREFIX
+    inreplace "src/_gio", "/usr/local/share", HOMEBREW_PREFIX/"share"
     # We install this into `pkgshare` to avoid conflicts
     # with completions installed by other formulae. See:
     #   https://github.com/Homebrew/homebrew-core/pull/126586
