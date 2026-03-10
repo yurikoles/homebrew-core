bf91258426b7b54145032f33e826b2b4f9ee2b99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 04:15:14 +0000

    lazygit: update 0.60.0 bottle.

--- Formula/l/lazygit.rb
+++ Formula/l/lazygit.rb
@@ -12,12 +12,12 @@ class Lazygit < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2843225fba69f1a2eef078bd7e45f251d9d09768d812d6a2d38ae786e6eb7442"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2843225fba69f1a2eef078bd7e45f251d9d09768d812d6a2d38ae786e6eb7442"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2843225fba69f1a2eef078bd7e45f251d9d09768d812d6a2d38ae786e6eb7442"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fc62102ce042affe16e1700cbdafe45c6344f932d8a45a63e5d7f4ded9c24696"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8acccd38a2aea178be53d30b86981c0ef3cce6875e8e617c860abcb44fd4c5f0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c33ae2332b07f1547508f4e196542727d2c9e5c7d42bf41723c5390f0c6f9f73"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c728240925aac5e240bbfd48135870e20809d8a5757f43ad4ac90ff3631f90c5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c728240925aac5e240bbfd48135870e20809d8a5757f43ad4ac90ff3631f90c5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c728240925aac5e240bbfd48135870e20809d8a5757f43ad4ac90ff3631f90c5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "275ff8591a04c6383cf79c18b63ca45423a0c8511d741768ab9c7adad16421c5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e1814cf229f4ab63a7fac063369dbf7636505fbb1efe2232ae0c70958b201040"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5de188c828e69621d3a851a581ba6703dd0d1ce4293080f7587699488b832dcc"
   end
 
   depends_on "go" => :build
