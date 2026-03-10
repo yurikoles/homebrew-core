df39754e34c536abdb59a4144507c95d344f5736
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 16:30:51 +0000

    nono: add 0.15.0 bottle.

--- Formula/n/nono.rb
+++ Formula/n/nono.rb
@@ -5,6 +5,15 @@ class Nono < Formula
   sha256 "5f48d630a4a61f96297166d9711af1aa128e16b02452370bd3196c54bcafa12a"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "006a1cfcfcc073ce8850019e2d1f55b402768e68b0112dd155fb5551b99f7922"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7010340b6ef99a0559d57575542d672a33589fa8f1ccba4b999ca49c12ec4544"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8d7e1ee5561562a5d6704c8558eec8e9eac2cdbfaa34ded43f537a37443d91e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f729825f7dc985371baa87a2dd13c6fd95899516b1f53e0694b2d6eacdc75076"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ce60d8208fd05816c2be4feaf40c5b42a5523c27565152cb90cfd79d90f75ded"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d27a5060290739e0271ad9cabad16f3de8913b171d29b3dca5a930bfd5572313"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
