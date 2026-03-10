bd387cd427705a5ae0cbe7438ba9c2ecc28e2b31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 03:50:45 +0000

    killswitch: update 0.8.2 bottle.

--- Formula/k/killswitch.rb
+++ Formula/k/killswitch.rb
@@ -9,10 +9,10 @@ class Killswitch < Formula
   no_autobump! because: :bumped_by_upstream
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f7a1663a4d840a24e873605173d5b3b268f1e658f348e11fdc65e59da8db1feb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e0ec09f4b48481274648ca33d56be42d804d1a24d4f607e1d12d9f228496807"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4366691141c65359723d291c0d2f0912d3738a90f3bfb650a8f16ad19b60577e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "cdb2b9555122b312ebc31e7d79d9a5cd5d3afb98d3584d81f80327fc8bb62b29"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "59f19ffd34d74717e5e4c98a54ff3aea8aea6d8cc930210c1bfa81d06c1ca8ba"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "99c93051e4bedae26eefccc504b3664e747bcfb3e05e0a6d9f29d1d1e9223d86"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de4be0598f404fffa4e2d7f842782fa449f0aed07c94ff41af89dc4221515765"
+    sha256 cellar: :any_skip_relocation, sonoma:        "50f57f490df220d489d64d747cb124c72997ac18d6d0c1d7f250b3a3fb7373cb"
   end
 
   depends_on "rust" => :build
