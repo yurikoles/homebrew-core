33eed4187abfb65a634673dac52e4d2adbdded53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 08:09:56 +0000

    ferron 2.6.0

--- Formula/f/ferron.rb
+++ Formula/f/ferron.rb
@@ -1,8 +1,8 @@
 class Ferron < Formula
   desc "Fast, memory-safe web server written in Rust"
   homepage "https://www.ferronweb.org/"
-  url "https://github.com/ferronweb/ferron/archive/refs/tags/2.5.5.tar.gz"
-  sha256 "e4bfd513d87ce32043c9f28cfb74b58c8b610f610bba79c59a0ff57370e0578c"
+  url "https://github.com/ferronweb/ferron/archive/refs/tags/2.6.0.tar.gz"
+  sha256 "50e874e4cefa8ac3601b171ffd694c13bef87306ef341b00973de180a3b1355b"
   license "MIT"
   head "https://github.com/ferronweb/ferron.git", branch: "develop-2.x"
 
@@ -25,114 +25,10 @@ class Ferron < Formula
     port = free_port
 
     (testpath/"ferron.yaml").write "global: {\"port\":#{port}}"
-    expected_output = <<~HTML.chomp
-      <!doctype html>
-             <html lang="en">
-                 <head>
-                     <meta charset="UTF-8" />
-                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                     <title>404 Not Found</title>
-                     <style>html,
-      body {
-          margin: 0;
-          padding: 0;
-          font-family:
-              system-ui,
-              -apple-system,
-              BlinkMacSystemFont,
-              "Segoe UI",
-              Roboto,
-              Oxygen,
-              Ubuntu,
-              Cantarell,
-              "Open Sans",
-              "Helvetica Neue",
-              sans-serif;
-          background-color: #ffffff;
-          color: #0f172a;
-      }
-
-      body {
-          padding: 1em;
-          -webkit-box-sizing: border-box;
-          -moz-box-sizing: border-box;
-          box-sizing: border-box;
-          width: 100%;
-          max-width: 1280px;
-          margin: 0 auto;
-      }
-
-      header {
-          text-align: center;
-      }
-
-      h1 {
-          font-size: 2.5em;
-      }
-
-      a {
-          color: #f47825;
-      }
-
-      @media screen and (max-width: 512px) {
-          h1 {
-              font-size: 2em;
-          }
-      }
-
-      @media screen and (prefers-color-scheme: dark) {
-          html,
-          body {
-              background-color: #14181f;
-              color: #f2f2f2;
-          }
-      }
-      </style>
-      <style>html {
-          height: 100%;
-      }
-
-      body {
-          display: table;
-          -webkit-box-sizing: border-box;
-          -moz-box-sizing: border-box;
-          box-sizing: border-box;
-          width: 100%;
-          height: 100%;
-      }
-
-      .error-container {
-          display: table-cell;
-          vertical-align: middle;
-          text-align: center;
-      }
-
-      .error-code {
-          display: block;
-          font-size: 4em;
-      }
-
-      .error-message {
-          display: block;
-      }
-      </style>
-                 </head>
-                 <body>
-                     <main class="error-container">
-            <h1>
-                <span class="error-code">404</span>
-                <span class="error-message">Not Found</span>
-            </h1>
-            <p class="error-description">The requested resource wasn't found. Double-check the URL if entered manually.</p>
-        </main>
-                 </body>
-             </html>
-    HTML
-
     begin
       pid = spawn bin/"ferron", "-c", testpath/"ferron.yaml"
       sleep 3
-      assert_match expected_output, shell_output("curl -s 127.0.0.1:#{port}")
+      assert_match "The requested resource wasn't found", shell_output("curl -s 127.0.0.1:#{port}")
     ensure
       Process.kill("TERM", pid)
       Process.wait(pid)
