d77f62ada3a8ee4dd3fd08ca287f440fa8bed900
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 18:31:19 +0000

    rtk: update 0.28.1 bottle.

--- Formula/r/rtk.rb
+++ Formula/r/rtk.rb
@@ -7,12 +7,12 @@ class Rtk < Formula
   head "https://github.com/rtk-ai/rtk.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29798f6cb6f84c140ca27525b5d7328013dd0d7efaeeb001989af3c38e3ae065"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0db6bb43631dde7a7abc17cde0c4bbeb67a8467645567c39aebd51415aa436a4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "69282f79a5bca22da634783322b9e94ebe324c63ade4277e68bb71bbb016b05c"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b005e0c29ac51038667102517db748cbe71594b37983531da70fb21681cd6d21"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4f4bddb5eb3cb31f8c2d42ca0dcbd33585cfc407e8cdb6ab9f90502c351b815"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "07b4a982c9838a2d5ae89ab7e19852ccd6207c500eeeff6c017135703cffcbfe"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "978eec15b5e031579b3773a6b738b36bd64e07273e4bbf2f70b51711edd71646"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb52ee438bf8bf8c35a7e696a484540404cf5b4f45d24a5a7d2e78b6999be90c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "903b8a43877a9d3cc275f2c2cbec30aca2297cf6510f77b00adbdd0313313a50"
+    sha256 cellar: :any_skip_relocation, sonoma:        "aee3563bb368fd297bf1c22e4a30fb10afdba8e71aa5b753da95497c6b2d6e76"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c206df245f241ad904dd1fa360bd5a818b2c284da7d0756f80fada30eb7bbcab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e0989d737dc7bd1f39ac58dee9f046444448f6f86f9d59aa76275d4f2b9efa2"
   end
 
   depends_on "rust" => :build
