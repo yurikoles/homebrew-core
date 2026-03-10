4e54685ebdb1e4fe6999d5457c2b46af735a9bb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 23:08:12 +0000

    container-compose 0.10.0

--- Formula/c/container-compose.rb
+++ Formula/c/container-compose.rb
@@ -1,8 +1,8 @@
 class ContainerCompose < Formula
   desc "Manage Apple Container with Docker Compose files"
   homepage "https://github.com/mcrich23/container-compose"
-  url "https://github.com/Mcrich23/container-compose/archive/refs/tags/0.9.0.tar.gz"
-  sha256 "7ffe905fea325b01f6090e95fd97f9165a69080e12b928a55f927b8adaa8949d"
+  url "https://github.com/Mcrich23/container-compose/archive/refs/tags/0.10.0.tar.gz"
+  sha256 "dbdabf599237cadc4840630a84591ed2c339feea9f9efdbba7e3422e6f58289b"
   license "MIT"
   head "https://github.com/mcrich23/container-compose.git", branch: "main"
 
