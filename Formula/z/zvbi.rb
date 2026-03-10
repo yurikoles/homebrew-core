c0b221948c258e7fbe83100814a3c9e0ea948c47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 18:20:43 +0000

    zvbi: add 0.2.44 bottle.

--- Formula/z/zvbi.rb
+++ Formula/z/zvbi.rb
@@ -6,6 +6,15 @@ class Zvbi < Formula
   license "GPL-2.0-or-later"
   head "https://github.com/zapping-vbi/zvbi.git", branch: "main"
 
+  bottle do
+    sha256 arm64_tahoe:   "f7803981991063b4265a50a8b870311edc8afafe9362c7334f768f8b6322b27f"
+    sha256 arm64_sequoia: "a8be8e1c10d76737f9b54ecfb5871b2e0598f76dabaae15b41f8e0c2cd6393b5"
+    sha256 arm64_sonoma:  "67ede4426aa1d02ed9563d16ec686d245ed8eec54efba20ed24af2470f1c2078"
+    sha256 sonoma:        "337e382234bbce4a697bfb11013c522d303ff922f953dfff0d03817c6a933168"
+    sha256 arm64_linux:   "a1d79ee36f8a7b4611c73d5bbe9e2d7bd9572bd4f3763a51fc894c32d4c4b37a"
+    sha256 x86_64_linux:  "f5ee604a80ba6c9d1d36d125953b4ca446d7d250cd5fe0797af9dae285e642a4"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
