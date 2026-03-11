d1b9ea71773f64d24816ffd7d5464419ff2b9719
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 20:06:58 +0000

    coinutils 2.11.13

--- Formula/c/coinutils.rb
+++ Formula/c/coinutils.rb
@@ -1,10 +1,9 @@
 class Coinutils < Formula
   desc "COIN-OR utilities"
   homepage "https://github.com/coin-or/CoinUtils"
-  url "https://github.com/coin-or/CoinUtils/archive/refs/tags/releases/2.11.12.tar.gz"
-  sha256 "eef1785d78639b228ae2de26b334129fe6a7d399c4ac6f8fc5bb9054ba00de64"
+  url "https://github.com/coin-or/CoinUtils/archive/refs/tags/releases/2.11.13.tar.gz"
+  sha256 "ddfea48e10209215748bc9f90a8c04abbb912b662c1aefaf280018d0a181ef79"
   license "EPL-2.0"
-  revision 1
   head "https://github.com/coin-or/CoinUtils.git", branch: "master"
 
   livecheck do
