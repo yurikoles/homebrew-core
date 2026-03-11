a6619ae8cd5b613cb4210890a008714d14409e45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 14:54:35 +0000

    mq 0.5.18

--- Formula/m/mq.rb
+++ Formula/m/mq.rb
@@ -1,8 +1,8 @@
 class Mq < Formula
   desc "Jq-like command-line tool for markdown processing"
   homepage "https://mqlang.org/"
-  url "https://github.com/harehare/mq/archive/refs/tags/v0.5.17.tar.gz"
-  sha256 "ecbf2ec04c50441cb75b5fa18a5b477b6740efdf3f2eb5307c1896f453509c3d"
+  url "https://github.com/harehare/mq/archive/refs/tags/v0.5.18.tar.gz"
+  sha256 "0299adbe1b919c81d28d58d9ace4f8e9c5b69cbdd9349c7d77e859e621af7dc7"
   license "MIT"
   head "https://github.com/harehare/mq.git", branch: "main"
 
