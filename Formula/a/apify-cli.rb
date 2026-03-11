d54a065e2ee6f2b34a2707ca7442dc3eb3dc7bfa
Apify Service Account <apify-service-account@users.noreply.github.com>
2026-03-11 19:33:24 +0000

    apify-cli 1.3.0

--- Formula/a/apify-cli.rb
+++ Formula/a/apify-cli.rb
@@ -1,21 +1,25 @@
 class ApifyCli < Formula
   desc "Apify command-line interface"
   homepage "https://docs.apify.com/cli/"
-  url "https://registry.npmjs.org/apify-cli/-/apify-cli-1.2.1.tgz"
-  sha256 "4969f3ddb1e02dc50f9e18883f7e288e4fc68f35754d425d61b122e29f79cc57"
+  url "https://registry.npmjs.org/apify-cli/-/apify-cli-1.3.0.tgz"
+  sha256 "613717d6a2b4e34ff1c388d7f67f577af03bda6a902c990b0e04f0dc96136f03"
   license "Apache-2.0"
 
   no_autobump! because: :bumped_by_upstream
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "27c1f68d6ded6c117aaf2f7ace988a56707ed18a002a36d64df16f9d081dda5f"
-  end
 
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink libexec.glob("bin/*")
+
+    node_modules = libexec/"lib/node_modules/apify-cli/node_modules"
+
+    # Remove incompatible pre-built `bare-fs`/`bare-os`/`bare-url` binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules.glob("{bare-fs,bare-os,bare-url}/prebuilds/*")
+                .each { |dir| rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}" }
   end
 
   test do
