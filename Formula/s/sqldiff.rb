f154cfe24c902e3cf13a14bfd0e319d54ecac8c0
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2026-03-12 20:02:52 +0000

    sqldiff: update 3.52.0 bottle.

--- Formula/s/sqldiff.rb
+++ Formula/s/sqldiff.rb
@@ -13,14 +13,14 @@ class Sqldiff < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "a403f5df2b55ede4c68c2f2875cf7bc4d891137cd2bad744cf34994758d23314"
-    sha256 cellar: :any,                 arm64_sequoia: "3d8153c5203a7c7415ed7bec58f1208a9bd93afef3703dbb3cdfe644b911ed34"
-    sha256 cellar: :any,                 arm64_sonoma:  "c344234fbe6c50f4e9fa774479b05f7a8b80ca458cd52f27f88f448784b11672"
-    sha256 cellar: :any,                 tahoe:         "e2e142266571dd13804f99e61ce25a17f8403b8710b168b340f76b8c436e7bec"
-    sha256 cellar: :any,                 sequoia:       "91d7e0b920e019425754155d3bb6d30ebe1275f52f0ae4d555cabbf406799884"
-    sha256 cellar: :any,                 sonoma:        "55206b74b6420f74d196eaf931ea578d760802ec5c1b217d89b637500637e50e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "40f9cbd0c8e7282d31e9e1b8a60753f0f46b700a55529ec7c0959931c84ad646"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c032c509444e725199c9199a703ea20f864dfab2e7cbdb16d1c070a21f21bcdd"
+    sha256 cellar: :any,                 arm64_tahoe:   "20810d26d7d0f5ac4c89fd36c1f53d5b80482f546ca40c519d06d6a4fa0390e1"
+    sha256 cellar: :any,                 arm64_sequoia: "eea40ae84ace01a1baf895d01e20223dae5310a38ce2ce945bad79da7ae15951"
+    sha256 cellar: :any,                 arm64_sonoma:  "e9b3bc0597a4195b5e5706d8d893908d571ec4a08ea8b1f55e8fcbe1bf4149da"
+    sha256 cellar: :any,                 tahoe:         "24f59ea5d1c601940f7648873d3e0ef26545926d6d3cda9d7ca35a144b17e5d1"
+    sha256 cellar: :any,                 sequoia:       "e316ba7cbdf4ea6c4cd4256517a3efbdd568006f4809f649d3f2598de4f02171"
+    sha256 cellar: :any,                 sonoma:        "159014e1a6efa2253d140e1f230c597cf82fcce918cd44d671fb0f64aad7e50f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0faea85fb68459201ff0b39eaab5a6acd9b8068887cb6b755d1e4baf3a79fa23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2181db98ea7fbd1ae022e04190084bc6b168daab6212c29070ccd6561092f64"
   end
 
   uses_from_macos "tcl-tk" => :build
