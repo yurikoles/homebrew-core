d20f4554d483cabfcddaac0ea2c6a37d642507c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-12 15:01:25 +0000

    vroom 1.15.0

--- Formula/v/vroom.rb
+++ Formula/v/vroom.rb
@@ -2,8 +2,8 @@ class Vroom < Formula
   desc "Vehicle Routing Open-Source Optimization Machine"
   homepage "http://vroom-project.org/"
   url "https://github.com/VROOM-Project/vroom.git",
-      tag:      "v1.14.0",
-      revision: "1fd711bc8c20326dd8e9538e2c7e4cb1ebd67bdb"
+      tag:      "v1.15.0",
+      revision: "43dd7d0b8b560431eb555bf335cf4797eb7343c4"
   license "BSD-2-Clause"
 
   bottle do
@@ -15,20 +15,39 @@ class Vroom < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d7e36a54cfde07f9cf24edfdc3ff570621414264cd20f4d4b9a83c2e9b8dab3"
   end
 
+  depends_on "asio" => :build
   depends_on "cxxopts" => :build
   depends_on "pkgconf" => :build
   depends_on "rapidjson" => :build
-  depends_on "asio"
   depends_on "openssl@3"
 
-  # Apply changes from open PR to fix build with newer Asio
-  # PR ref: https://github.com/VROOM-Project/vroom/pull/1279
-  patch :DATA
+  on_macos do
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1699
+  end
 
-  # remove jthread usage, upstream PR ref, https://github.com/VROOM-Project/vroom/pull/1065
+  on_sequoia do
+    depends_on xcode: ["26.0", :build] if DevelopmentTools.clang_build_version >= 1700
+  end
+
+  on_linux do
+    depends_on "gcc" # TODO: remove and rebuild bottle on Ubuntu 24.04
+  end
+
+  fails_with :clang do
+    build 1699
+    cause "needs C++20 std::jthreads"
+  end
+
+  fails_with :gcc do
+    version "12"
+    cause "Requires C++20 std::format, https://gcc.gnu.org/gcc-13/changes.html#libstdcxx"
+  end
+
+  # Apply open PR to fix missing include
+  # PR ref: https://github.com/VROOM-Project/vroom/pull/1333
   patch do
-    url "https://github.com/VROOM-Project/vroom/commit/0cd72771fb79840a2a0ff64a58f0c18830665119.patch?full_index=1"
-    sha256 "b271c6a7f27c17fbc5eb47c7b80bd697e29af8f631a4e27d68d00f9b08e9e9f9"
+    url "https://github.com/VROOM-Project/vroom/commit/3bd437aa5951040593d535336a3d7cf86b6ac405.patch?full_index=1"
+    sha256 "f9681c0d96265435e3b15477ec9471116159716a2a868b33e4d46eb1009cd1dd"
   end
 
   def install
@@ -48,9 +67,7 @@ class Vroom < Formula
     ]
     inreplace files, "../include/rapidjson/include/rapidjson", "rapidjson"
 
-    cd "src" do
-      system "make"
-    end
+    system "make", "-C", "src"
     bin.install "bin/vroom"
     pkgshare.install "docs"
   end
@@ -62,49 +79,3 @@ class Vroom < Formula
     assert_equal expected_routes, actual_routes
   end
 end
-
-__END__
-diff --git a/src/routing/http_wrapper.cpp b/src/routing/http_wrapper.cpp
-index 474de70e..80d0131d 100644
---- a/src/routing/http_wrapper.cpp
-+++ b/src/routing/http_wrapper.cpp
-@@ -37,14 +37,12 @@ std::string HttpWrapper::send_then_receive(const std::string& query) const {
-   std::string response;
- 
-   try {
--    asio::io_service io_service;
-+    asio::io_context io_context;
- 
--    tcp::resolver r(io_service);
-+    tcp::resolver r(io_context);
- 
--    tcp::resolver::query q(_server.host, _server.port);
--
--    tcp::socket s(io_service);
--    asio::connect(s, r.resolve(q));
-+    tcp::socket s(io_context);
-+    asio::connect(s, r.resolve(_server.host, _server.port));
- 
-     asio::write(s, asio::buffer(query));
- 
-@@ -86,16 +84,14 @@ std::string HttpWrapper::ssl_send_then_receive(const std::string& query) const {
-   std::string response;
- 
-   try {
--    asio::io_service io_service;
-+    asio::io_context io_context;
- 
-     asio::ssl::context ctx(asio::ssl::context::method::sslv23_client);
--    asio::ssl::stream<asio::ip::tcp::socket> ssock(io_service, ctx);
--
--    tcp::resolver r(io_service);
-+    asio::ssl::stream<asio::ip::tcp::socket> ssock(io_context, ctx);
- 
--    tcp::resolver::query q(_server.host, _server.port);
-+    tcp::resolver r(io_context);
- 
--    asio::connect(ssock.lowest_layer(), r.resolve(q));
-+    asio::connect(ssock.lowest_layer(), r.resolve(_server.host, _server.port));
-     ssock.handshake(asio::ssl::stream_base::handshake_type::client);
- 
-     asio::write(ssock, asio::buffer(query));
