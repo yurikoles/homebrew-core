83f528e86585bcc7001a31980098c94a826a8f6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-12 09:19:14 +0000

    kingfisher: update 1.88.0 bottle.

--- Formula/k/kingfisher.rb
+++ Formula/k/kingfisher.rb
@@ -6,12 +6,12 @@ class Kingfisher < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1c4241805daf4f3ba102c0a0d45e78207fbbc63eace9e72468dde255056751ed"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c3fbcdda62a355344d3937c778adc4703ad2816cac99c8deca62c583976807a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e765b3a9fa05f8821c005b81ad3c69c74dce35c7abc6a0fee2fb205e7a277d51"
-    sha256 cellar: :any_skip_relocation, sonoma:        "201c0d0f20d39fddbde3e3454d62cf079ac3009ece54515254129a5a571b1a99"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "de8b27ca47a99e3c927aa9050f930d05adc6365d564e2af8daa5ccce8f402b71"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13614110990bf4d998f6c52c35ce208b4a7c9e7723bf7e5431e5ce6d605e5515"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc657d98de9d2a4ac6827543a5871f7ac87dc4ad4eb932db24e1818bcb214420"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f79c548b26aa2738aa40450e69dae7befcb7c1862f6672497920cd334b0b3b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5695718a4396d300e8de940429f6c02e72e347eba4d2f2cddd6e9e4bff3a9558"
+    sha256 cellar: :any_skip_relocation, sonoma:        "79305c6714dc74d2acea7124cde7a738a6e84f721dae79214049e2663ebd6d0e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc7cc9bd759764055e0df2080dbbfef11b1228c7c8d97271bcce8dd1b86ab5e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbcbd81693c76d2211a4bdb043f2749464709af9260c76cd46e12712269416df"
   end
 
   depends_on "boost" => :build
