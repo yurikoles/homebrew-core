4f538a7a376f27541c41b9985a389abe6767b3e9
WOSSFOSS <231392499+WOSSFOSS@users.noreply.github.com>
2026-03-11 23:18:45 -0400

    vapoursynth-bm3d r9 (new formula)

---
+++ Formula/v/vapoursynth-bm3d.rb
@@ -0,0 +1,34 @@
+class VapoursynthBm3d < Formula
+  desc "BM3D denoising filter for VapourSynth"
+  homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-BM3D"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-BM3D/archive/refs/tags/r9.tar.gz"
+  sha256 "3eb38c9e4578059042c96b408f5336b18d1f3df44896954713532cff735f1188"
+  license "MIT"
+  head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-BM3D.git", branch: "master"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "fftw"
+  depends_on "vapoursynth"
+
+  def install
+    # Upstream build system wants to install directly into vapoursynth's libdir and does not respect
+    # prefix, but we want it in a Cellar location instead.
+    inreplace "meson.build",
+              "install_dir : join_paths(vapoursynth_dep.get_pkgconfig_variable('libdir'), 'vapoursynth'),",
+              "install_dir : '#{lib}/vapoursynth',"
+
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    python = Formula["vapoursynth"].deps
+                                   .find { |d| d.name.match?(/^python@\d\.\d+$/) }
+                                   .to_formula
+                                   .opt_libexec/"bin/python"
+    system python, "-c", "from vapoursynth import core; core.bm3d"
+  end
+end
