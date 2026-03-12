d0e2a7ef6e10fdd8f6b116f46503de07dc1c251e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-12 17:44:43 +0000

    newrelic-infra-agent 1.72.8

--- Formula/n/newrelic-infra-agent.rb
+++ Formula/n/newrelic-infra-agent.rb
@@ -2,8 +2,8 @@ class NewrelicInfraAgent < Formula
   desc "New Relic infrastructure agent"
   homepage "https://github.com/newrelic/infrastructure-agent"
   url "https://github.com/newrelic/infrastructure-agent.git",
-      tag:      "1.72.7",
-      revision: "e95ae956e68aa6d938997dfe41feb091326c82a7"
+      tag:      "1.72.8",
+      revision: "b328a9ca0d13d3b2fb96be702794d4a2e4baa5e3"
   license "Apache-2.0"
   head "https://github.com/newrelic/infrastructure-agent.git", branch: "master"
 
