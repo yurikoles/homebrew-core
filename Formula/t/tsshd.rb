ff291ae91f2337c8403c609c98771a4ae3352b7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-12 11:50:24 +0000

    tsshd: add 0.1.6 bottle.

--- Formula/t/tsshd.rb
+++ Formula/t/tsshd.rb
@@ -5,6 +5,15 @@ class Tsshd < Formula
   sha256 "a303c14bd5a41303d56254cfa97b93ca5025c0138a8ceb9d0d777eb715b0c1bd"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c6403b83e9b30c8d42596f45df16ec86174ccdadb2e69c52ae8260a5429df447"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c6403b83e9b30c8d42596f45df16ec86174ccdadb2e69c52ae8260a5429df447"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c6403b83e9b30c8d42596f45df16ec86174ccdadb2e69c52ae8260a5429df447"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b62a6704693a2412827cc908035603eb2c8d84c31ab8498dca2dc6eee0e5d1db"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9e8a3dc7d6b5f3e070448a701bffd0ce05e94a708dc4ec04b529219ec38044cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b6aec72067678c7bd8bbed61dae2a76c453c8a565aabedaffda978be35daf038"
+  end
+
   depends_on "go" => :build
 
   def install
