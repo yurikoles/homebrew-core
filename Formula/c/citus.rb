a81ec84a5782094de8be2b8a91510207e44a0eab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 00:59:09 +0000

    citus: update 14.0.0 bottle.

--- Formula/c/citus.rb
+++ Formula/c/citus.rb
@@ -15,12 +15,13 @@ class Citus < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e309102533b5335516bd9497f53b60982dcd7e7010d909c01f9b6bcbdb0e1d28"
-    sha256 cellar: :any,                 arm64_sequoia: "5702c52b34e0db615adde34571abdabe56386672946352b4debc35d050fd33e3"
-    sha256 cellar: :any,                 arm64_sonoma:  "9f119d5071165eda287caceb1d3c927a0d5825a439f841953e0920ff5daba95d"
-    sha256 cellar: :any,                 sonoma:        "2b78fa1d40528914b0f66dbeb1d558ac56314d2bb4474eb99ab019cb8cb3db8a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bd9dfe810a77c8c29d46c5e562d7d2110f1ed3c12fac085d3255d04994d2caba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2844b461a1204133de4f73bfe093091043145be5803b920537b01165b88e2c26"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_tahoe:   "f8e163c1669fe07f49c9efb0bb176edf0b94811bf06c34b2c0ecd8e673983c35"
+    sha256 cellar: :any,                 arm64_sequoia: "f5b32245b518a637010b179d3841df8a3b753a81fad53566147f91272d5a8864"
+    sha256 cellar: :any,                 arm64_sonoma:  "41fbcc16dc152cbaca91c37ff3b1ccf6cb091933a23aba97477847cf143d61ff"
+    sha256 cellar: :any,                 sonoma:        "eb79e0942e177c7e27b30b58a0e0e1b76db9e1acff793ddc192920dd0e1f2e78"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2ccad29edead2a14d8c95f04fd5f59c76b7d605631890618c059cc3fd7f94794"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53c1a2f45e6439acff9b72cccf9f5a745c5cf18c42feb6d8dd329d268703fb77"
   end
 
   depends_on "postgresql@17" => [:build, :test]
