1644d04e16384db6864e9892c88c51cc34976b94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 18:57:14 +0000

    goshs: update 1.1.4 bottle.

--- Formula/g/goshs.rb
+++ Formula/g/goshs.rb
@@ -7,12 +7,12 @@ class Goshs < Formula
   head "https://github.com/patrickhener/goshs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "53a33b2135b2badf0530d5fd3ec4cc09d170e79cd8ec590174295f9327952b7e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "53a33b2135b2badf0530d5fd3ec4cc09d170e79cd8ec590174295f9327952b7e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "53a33b2135b2badf0530d5fd3ec4cc09d170e79cd8ec590174295f9327952b7e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "407c85ce4ac2b51e2172e60523df14aea6d59bbea23290bc0503bd6f5b7532b4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fcd11471619fc6e1e73f3e94a64db521b2dc0f76ba7f8df9ed516af0f16afc54"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c89a25d370dd64cb1ce997d12af0de43fa5a074a5630761e6057824cfdc6a3dc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d81f736d07e22d606584dafc04556d84147e65352e0f31f8baf5513961ea4aeb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d81f736d07e22d606584dafc04556d84147e65352e0f31f8baf5513961ea4aeb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d81f736d07e22d606584dafc04556d84147e65352e0f31f8baf5513961ea4aeb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a8291b84c09976ada002a75bba2ea5d227f8faf9ff73550e5b19505c213c0214"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3f2bf59276b33c84e30e64392c3b9d6286c0df7fa192726b25c6de594ccc53e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "506fc493854e9c24f88d35c7db5e613e2e3c0696acd72ff29a3bfe327923ca0e"
   end
 
   depends_on "go" => :build
