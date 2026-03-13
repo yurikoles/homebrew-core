4a0d78dd4fa53669165b7fa2e0af960469e8f38c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 19:07:13 +0000

    zxc: add 0.8.3 bottle.

--- Formula/z/zxc.rb
+++ Formula/z/zxc.rb
@@ -6,6 +6,15 @@ class Zxc < Formula
   license "BSD-3-Clause"
   head "https://github.com/hellobertrand/zxc.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "637a11622ad33dc1b2aa6d552ea92d34b1dce5b9e53bcfbf6b46f3d808548b98"
+    sha256 cellar: :any,                 arm64_sequoia: "74dc1cdd8e3a74bc7eb0ba40d369e35fbbd82d54b964a0a31a323243abfa3184"
+    sha256 cellar: :any,                 arm64_sonoma:  "d4ebc84041ad66af46f617707e4f57a69e791710602bceca7da7ea86b7c21109"
+    sha256 cellar: :any,                 sonoma:        "6c304ca798bea13f7eccf692f15b8f250c6a991787712597435d8bf271bced03"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fc199288202235f4d3cc8e6a20373ea2ba9569c74fd8b4b4931c514a9ef503ee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58393ba17bc31bf08c5e33f3d43f4ab20141c97af23d40d5bb67f055f3916a75"
+  end
+
   depends_on "cmake" => :build
 
   def install
