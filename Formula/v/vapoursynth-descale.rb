23fc1ee1e14145121741c06e315bc9153cc893de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 22:42:46 +0000

    vapoursynth-descale: add 8 bottle.

--- Formula/v/vapoursynth-descale.rb
+++ Formula/v/vapoursynth-descale.rb
@@ -6,6 +6,15 @@ class VapoursynthDescale < Formula
   license "MIT"
   head "https://github.com/Irrational-Encoding-Wizardry/descale.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "367adf1613ed0354d8a93588f765437e072bc9c2fcfa7cd8cbcef9291f38475d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9faad8e0a5ee9b78fbe86a7119fae7b316f96565f2ab72881f42301f5bcfa931"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b77deed08ec1f7fa494acb387ea8af43a2a81afb2aee8431c6b30b27f5f63f3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "46b58d656b8b7a374e1a759ca4e10e786d29356084308ed06d97f9919c56ce7d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "268756b29d7c04d9f28f6142462a51c77d6ab779b06011d12b42e6c71f18e279"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b57118f0638ab218aadc619c10a0790d97676ee8f0fee8de3ae9e8daf027fc8"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
