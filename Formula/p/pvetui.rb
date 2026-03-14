01bb5a53c7d64536781fd8c56e5bc1ec1b33a215
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 23:54:38 +0000

    pvetui: update 1.2.1 bottle.

--- Formula/p/pvetui.rb
+++ Formula/p/pvetui.rb
@@ -7,12 +7,12 @@ class Pvetui < Formula
   head "https://github.com/devnullvoid/pvetui.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "da804e03c8c78d15dba47da93828c4e8a99047b87c74309e5af559033dd024c0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da804e03c8c78d15dba47da93828c4e8a99047b87c74309e5af559033dd024c0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da804e03c8c78d15dba47da93828c4e8a99047b87c74309e5af559033dd024c0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ba087ada44ab865264570f119854f6b1c80c690cc443a4703937925730203239"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0167b1c847b73c1208b895862a66ffa4dc87de90060980320cd41c779ee000b4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "218f48a091654af692fad688b3f0ea46f3c7bbf0ec23caef6fb882b4102b7704"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b59196c5fb810a8c4d3881653e8d260bc4a94d8f42f9368b5e4fe97430126e25"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b59196c5fb810a8c4d3881653e8d260bc4a94d8f42f9368b5e4fe97430126e25"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b59196c5fb810a8c4d3881653e8d260bc4a94d8f42f9368b5e4fe97430126e25"
+    sha256 cellar: :any_skip_relocation, sonoma:        "db54bfbf28b71c8eed6baebe16cbab29f6e3ba67a870b550263e8d428a591aa6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c1f9903d2d53293e61ce4065dff154476103a69b8bb901e45bb513afc0957cb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3d429b501243b01c854c99c8c92d094c2b58f7d71f9396597a2e81ddecb8fa7"
   end
 
   depends_on "go" => :build
