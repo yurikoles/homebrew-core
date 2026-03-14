8e169af1c2affa4f2431c632d4719d5ab9954c45
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 15:19:14 +0000

    marp-cli: update 4.3.0 bottle.

--- Formula/m/marp-cli.rb
+++ Formula/m/marp-cli.rb
@@ -6,12 +6,12 @@ class MarpCli < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "7e9ceb263b61d6f55746ab7c5ab6f4e12ce5bb749a618b17912e49ae7315e417"
-    sha256 cellar: :any,                 arm64_sequoia: "17bc3847ffd84cf69b128b48a2220f0610e4715a45f76d9809bf376156a0083b"
-    sha256 cellar: :any,                 arm64_sonoma:  "17bc3847ffd84cf69b128b48a2220f0610e4715a45f76d9809bf376156a0083b"
-    sha256 cellar: :any,                 sonoma:        "b62c5e31eff56dae8f99b73947a2d343a32f321797b559d1eac71e6127776b34"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "17b66788e0faf313b438502fd14fa066c6c589073c7c2f4df78b3556b2cc7869"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ea40b743d6849ed00af718af64ce987ca7564569eb42f6fda93430ca64566db"
+    sha256 cellar: :any,                 arm64_tahoe:   "aa13b22059ce6b8755595dfdfad7db308f3a89d79892d38c7fb315843ab531f8"
+    sha256 cellar: :any,                 arm64_sequoia: "f171d94b6328184555c5b68a73317afce2ed7572b04ae62dd9186cbaf2954b20"
+    sha256 cellar: :any,                 arm64_sonoma:  "f171d94b6328184555c5b68a73317afce2ed7572b04ae62dd9186cbaf2954b20"
+    sha256 cellar: :any,                 sonoma:        "32d27d395804c63b210ad408352f19471c3bb2ca25db5372c95cbd7482c0d93d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1dd878c09b09f0f7d2a6d9e579076f8c163d51ffddc8197933bbcb221a882fc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "821db6be0855e372344cf2ea64e8ca836600aadef798cce2d4d98583f303502e"
   end
 
   # Remove when Node 25 is fixed upstream: https://github.com/nodejs/node/issues/61971
