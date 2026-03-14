d585a8bcf261a43fd87e24dc5313aefbb88f9e40
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 11:34:09 +0000

    ryelang 0.2.3

--- Formula/r/ryelang.rb
+++ Formula/r/ryelang.rb
@@ -1,8 +1,8 @@
 class Ryelang < Formula
   desc "Rye is a homoiconic programming language focused on fluid expressions"
   homepage "https://ryelang.org/"
-  url "https://github.com/refaktor/rye/archive/refs/tags/v0.2.2.tar.gz"
-  sha256 "7cfcca283100c306de14e2b0f0baec2be21568e93e00cd54850ac10af175bdf4"
+  url "https://github.com/refaktor/rye/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "79a8a9b18bace82e304c5f6bb349da1b2352822d6fd303faa3380e15e6608a23"
   license "BSD-3-Clause"
   head "https://github.com/refaktor/rye.git", branch: "main"
 
