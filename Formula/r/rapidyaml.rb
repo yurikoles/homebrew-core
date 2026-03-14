facde2bc1e3521bd461365264087c2e41c98d9c6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 11:32:11 +0000

    rapidyaml 0.11.0

--- Formula/r/rapidyaml.rb
+++ Formula/r/rapidyaml.rb
@@ -1,8 +1,8 @@
 class Rapidyaml < Formula
   desc "Library to parse and emit YAML, and do it fast"
   homepage "https://github.com/biojppm/rapidyaml"
-  url "https://github.com/biojppm/rapidyaml/releases/download/v0.10.0/rapidyaml-0.10.0-src.tgz"
-  sha256 "54eb1050789809a26c780f80857b7668a5b3123405d6514a65d733e4292c690b"
+  url "https://github.com/biojppm/rapidyaml/releases/download/v0.11.0/rapidyaml-0.11.0-src.tgz"
+  sha256 "46e42f656a767b9ecaf9b903b6f6a9c72453359ee9cbf488d510d0558ff5ab78"
   license "MIT"
 
   bottle do
