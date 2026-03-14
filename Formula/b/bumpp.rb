6d488e63e63a260a4ce57cfee363a61614dff925
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 22:59:45 +0000

    bumpp 11.0.0
    bumpp: update test

--- Formula/b/bumpp.rb
+++ Formula/b/bumpp.rb
@@ -1,8 +1,8 @@
 class Bumpp < Formula
   desc "Interactive CLI that bumps your version numbers and more"
   homepage "https://github.com/antfu-collective/bumpp"
-  url "https://registry.npmjs.org/bumpp/-/bumpp-10.4.1.tgz"
-  sha256 "373b62e7841ba156fba34372b0b35e0bc481c1ded0f055f45b9bd2efaad00e93"
+  url "https://registry.npmjs.org/bumpp/-/bumpp-11.0.0.tgz"
+  sha256 "49da810e3d7e3fa57949c7f603390b69b0997b95ee5cf6e9cee5941b4d8c0089"
   license "MIT"
 
   bottle do
@@ -18,21 +18,24 @@ class Bumpp < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/bumpp --version")
-    system "git", "init"
-    (testpath/"package.json").write <<~JSON
-      {
-        "name": "bumpp-homebrew-test",
-        "version": "1.0.0"
-      }
-    JSON
 
-    system "git", "add", "package.json"
-    system "git", "commit", "-m", "Initial commit"
+    mkdir "repo" do
+      system "git", "init"
+      (testpath/"repo/package.json").write <<~JSON
+        {
+          "name": "bumpp-homebrew-test",
+          "version": "1.0.0"
+        }
+      JSON
 
-    system bin/"bumpp", "--yes", "--push", "false", "--no-commit", "--release", "patch"
+      system "git", "add", "package.json"
+      system "git", "commit", "-m", "Initial commit"
 
-    package_json = (testpath/"package.json").read
-    package_hash = JSON.parse(package_json)
-    assert_match "1.0.1", package_hash["version"]
+      system bin/"bumpp", "--yes", "--push", "false", "--no-commit", "--release", "patch"
+
+      package_json = (testpath/"repo/package.json").read
+      package_hash = JSON.parse(package_json)
+      assert_match "1.0.1", package_hash["version"]
+    end
   end
 end
