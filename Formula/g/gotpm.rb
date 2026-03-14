83f2f925761666bc03e6102041357a7ebc9255be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 01:01:59 +0000

    gotpm: update 0.4.8 bottle.

--- Formula/g/gotpm.rb
+++ Formula/g/gotpm.rb
@@ -7,13 +7,12 @@ class Gotpm < Formula
   head "https://github.com/google/go-tpm-tools.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e1fbf201152c1d92e0fee354a2736c1c9ecd1cf1f657d3b9df4f0c2bbf547424"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e1fbf201152c1d92e0fee354a2736c1c9ecd1cf1f657d3b9df4f0c2bbf547424"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1fbf201152c1d92e0fee354a2736c1c9ecd1cf1f657d3b9df4f0c2bbf547424"
-    sha256 cellar: :any_skip_relocation, sonoma:        "37e6a3f7449e763691e10f7355279e2d3043923cce437f25ff5db98ea80af47d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d097bb0ed3355e982eeee89dadcd9d3da64eb5ab14e0c44d6bc77749956f5eef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0be077482b2c4d14b4a8f86a6b5a4235215b4a9d5b4f22659e94dc72e3a282e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d1a03aff56a0ccc4cd88b51dd98addb93b56bfbb08a2ea78c5f69f1da2dc14f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d1a03aff56a0ccc4cd88b51dd98addb93b56bfbb08a2ea78c5f69f1da2dc14f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d1a03aff56a0ccc4cd88b51dd98addb93b56bfbb08a2ea78c5f69f1da2dc14f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fbbb360dc8e2b14aefecbb317830ad694cc8d7aaac34d9defc660311b038f784"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4868c1cd5540ae182dda21a39baf5245d5172d27562cc755e7db043c43e20c3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "235c5a1458fc87042665a765ef4bf66d7e4f4a5cbd9129f37c311b5f485c6efa"
   end
 
   depends_on "go" => :build
