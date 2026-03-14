0ebdb642fbeb3130148160a7fb2bd5fd55232e2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 05:23:54 +0000

    gjs 1.88.0

--- Formula/g/gjs.rb
+++ Formula/g/gjs.rb
@@ -1,8 +1,8 @@
 class Gjs < Formula
   desc "JavaScript Bindings for GNOME"
   homepage "https://gitlab.gnome.org/GNOME/gjs/wikis/Home"
-  url "https://download.gnome.org/sources/gjs/1.86/gjs-1.86.0.tar.xz"
-  sha256 "63448f7a57804d4c2a8d0c7f5e90e224d04d4eb2d560142c076c65a8eda00799"
+  url "https://download.gnome.org/sources/gjs/1.88/gjs-1.88.0.tar.xz"
+  sha256 "30a0b9f3317e8e60b1896db2903c70e8b0cd33df953c328755803a75191dc453"
   license all_of: ["LGPL-2.0-or-later", "MIT"]
   head "https://gitlab.gnome.org/GNOME/gjs.git", branch: "master"
 
