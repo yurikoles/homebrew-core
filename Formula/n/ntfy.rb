f1ee804d18e438907d3fc7fc38c53c809b16d39a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-15 15:16:28 +0000

    ntfy: update 2.19.0 bottle.

--- Formula/n/ntfy.rb
+++ Formula/n/ntfy.rb
@@ -7,12 +7,12 @@ class Ntfy < Formula
   head "https://github.com/binwiederhier/ntfy.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f92bc6723764cc1c2e9224b4204341ea5ca5d3870621d7858688d57d582b83ff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f92bc6723764cc1c2e9224b4204341ea5ca5d3870621d7858688d57d582b83ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f92bc6723764cc1c2e9224b4204341ea5ca5d3870621d7858688d57d582b83ff"
-    sha256 cellar: :any_skip_relocation, sonoma:        "8e28a004d7ebeff1cfd1c2641d61ccfe3a02fda74441dd7b62c05c68fe3a543b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "42c6d351722d6032ad967f8abf1e4e512084be03ff90d2eeac5ec9557cf87703"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e498cf5aec3ae097af17e83884c9ff028580d92ef2e6f52eabb04a6a182b75c5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "85f0ab4c0815f184f473f9b58166d5df144bc211160c43d1c64892cdca3a61eb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "85f0ab4c0815f184f473f9b58166d5df144bc211160c43d1c64892cdca3a61eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "85f0ab4c0815f184f473f9b58166d5df144bc211160c43d1c64892cdca3a61eb"
+    sha256 cellar: :any_skip_relocation, sonoma:        "055c91ae2a80395fc4a9a61bfbcea3d959059a87fecc999a9cdc556ab11d82cc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e0de09c45e5fdcfb855b0859857f5b96d158bb3233bffc2871983b0dadd2cff3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0ea45865b2d079ee749eef7dc9bfde3f46bd0dd622ffadc89f0a548a24d08f0"
   end
 
   depends_on "go" => :build
