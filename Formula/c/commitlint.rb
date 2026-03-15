5e964ad442ad99bc5490c0e15ae836293261de35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-15 14:08:03 +0000

    commitlint 20.5.0

--- Formula/c/commitlint.rb
+++ Formula/c/commitlint.rb
@@ -1,8 +1,8 @@
 class Commitlint < Formula
   desc "Lint commit messages according to a commit convention"
   homepage "https://commitlint.js.org/#/"
-  url "https://registry.npmjs.org/commitlint/-/commitlint-20.4.4.tgz"
-  sha256 "5b474f5d6b854a41317becf31fda806959ff2606c393a9aeb870ffa8676547e0"
+  url "https://registry.npmjs.org/commitlint/-/commitlint-20.5.0.tgz"
+  sha256 "311d285f00d94f0abbfa8bcd7e22201ee97b9212730903ebc3d1f2ab210518b6"
   license "MIT"
 
   bottle do
