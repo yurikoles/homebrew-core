3d8148c7eb451e32364c11b07a4eeaf36cdc8e0d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 09:52:40 +0000

    luv: update 1.52.1-0 bottle.

--- Formula/l/luv.rb
+++ Formula/l/luv.rb
@@ -7,13 +7,12 @@ class Luv < Formula
   head "https://github.com/luvit/luv.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any,                 arm64_tahoe:   "175c16bb90e1d7e1e77a65ce0ed206efb1b4785f24a55d125899accdb902629f"
-    sha256 cellar: :any,                 arm64_sequoia: "64a16e8ebdb0606d3dd04dd3e64745214c72f461cc713ae8ba60736e3c5e8f78"
-    sha256 cellar: :any,                 arm64_sonoma:  "4c5ec7765e751e883ce332339f54542e00a3eccf008061329375501f3635207e"
-    sha256 cellar: :any,                 sonoma:        "3dc3da670e1e4862e41568f01b94e6977611de3dc098d04d5adb2bd4e20c3094"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a2c1181142d063be705f53b9e0eb665c1d7b19663d98f24e8f701f9f932d79f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d219fead8b00d7a33c14c9d857f7e8495bcce74102416091cdc753fd96ddf8a"
+    sha256 cellar: :any,                 arm64_tahoe:   "749819a8255ec6cb1ae287fc3aba617eb990301df4dc89211ce0d8b639f62c0e"
+    sha256 cellar: :any,                 arm64_sequoia: "bcd21234139140236ad2a94c0dc28f78b5f810a4d96fd799dc85cc1499a22390"
+    sha256 cellar: :any,                 arm64_sonoma:  "8b863c574a8daaf828c99dbbf1ac8e67d543122ea9c2154c20770ef70ec13117"
+    sha256 cellar: :any,                 sonoma:        "a643e41957c33342174f7cf205d47b3c1ed10d433b423823f87414cd4eb4d807"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fa89efaaf09f951f575767cb079b4da38566e39af1b94fdeee0b9c81b13dc6a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ece75e8ec4e4de53025c4a2047ad3768e73a6aaa1fb093796158b69db1a9d82c"
   end
 
   depends_on "cmake" => :build
