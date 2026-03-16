92e1ead46023f8cbb9a40c7e1c27c00eed4d1d04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 18:48:33 +0000

    libbpf: update 1.7.0 bottle.

--- Formula/lib/libbpf.rb
+++ Formula/lib/libbpf.rb
@@ -6,9 +6,8 @@ class Libbpf < Formula
   license "BSD-2-Clause"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "c6434f21ee8213728fa2282fa9c7212576b6b930c04fb89080184693d597cca9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "82f1148d37fd262f39b42d8a3edb7f152e2028c6fd39efad34117b8a10b2a3d0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "c568bbfccbfe4b8c0a9e4d10bce51e32bd0a016c99dcf74baf050e3cc73e1c2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b097e3b166ab53800e559459b64a4167c8ebe57383f6e4cff627cf5ddbd14299"
   end
 
   depends_on "pkgconf" => :build
