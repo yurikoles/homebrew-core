9bdb8ff63cd49248fc0fbf767ec9738c7591afda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 02:53:00 +0000

    fzf-tab 1.3.0

--- Formula/f/fzf-tab.rb
+++ Formula/f/fzf-tab.rb
@@ -1,8 +1,8 @@
 class FzfTab < Formula
   desc "Replace zsh completion selection menu with fzf"
   homepage "https://github.com/Aloxaf/fzf-tab"
-  url "https://github.com/Aloxaf/fzf-tab/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "6b9c82c73ceb70ab13d1b6ef07c7bda7d43a3c507152dc156f783f1f1a6774ad"
+  url "https://github.com/Aloxaf/fzf-tab/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "d75ac08c2c8af5a6a0478787b0f11fabbe24951973b7841ae963431e2070ee9a"
   license "MIT"
 
   bottle do
