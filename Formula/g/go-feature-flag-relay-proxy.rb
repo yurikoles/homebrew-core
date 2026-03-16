dc0b841e8bc5b7cb00f3d4aaede035d4283358f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 14:58:03 +0000

    go-feature-flag-relay-proxy: update 1.52.0 bottle.

--- Formula/g/go-feature-flag-relay-proxy.rb
+++ Formula/g/go-feature-flag-relay-proxy.rb
@@ -7,12 +7,12 @@ class GoFeatureFlagRelayProxy < Formula
   head "https://github.com/thomaspoignant/go-feature-flag.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "03c9ca43b166d415ca504ddb0273a4bad14d4f2dc813580272f3636a42396180"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb70a86ab0c70a1e1fd28f480eaeb2c393666a812b5634594836de9b07392cd0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ebd85d835f938c30b01d8a14c538b3ad9c71c63e15897f687e168dfa91738944"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d04e3da63bbb10b1259091eac34566a5a84f143ff808e975a855f58bef74f8b4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fbec252660c1e1a2c382768416ac5f46849eb31f8a382f4ff9d3f62fb8bfd2fd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6787e8b3cff9053f291cc49430e1be029a2171b6e10f67278474a74352068303"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3bcf84b34a331e431cfede3b80ef365b47a0aa1144d35e36cf695ecac7895bca"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d888d76a6be00c46503f9c2bc70cf5d7baa6fac6636798ea3251b4c21cb125c1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b658a56a2169efe96d7e3a2f050a1413d9e88101912c5361876b97015ac4bfa0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ffefb6c739bd41ede09f8897f28b84ec9b1a46835d71765708a3d1f3f7b4208d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ddea071945b4db081d69166fe92cdd289a0af7828fe6789018c3756a6c0a646d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b016e0d6c8f08b5e6a12509c965f394796aacea3a248b9690987561bce96d76c"
   end
 
   depends_on "go" => :build
