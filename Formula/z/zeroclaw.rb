aeb5bcbbf8dd239de0c6b5540f0de064b253061e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 18:49:16 +0000

    zeroclaw: update 0.4.0 bottle.

--- Formula/z/zeroclaw.rb
+++ Formula/z/zeroclaw.rb
@@ -7,12 +7,12 @@ class Zeroclaw < Formula
   head "https://github.com/zeroclaw-labs/zeroclaw.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bc0c0c10fba0ac5f27b0776cdcea1fbe69380dfb22b5ca4f96d2239c14471946"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f001194a3ed2b51264f9ee70be296869fb2c00a8aa54d4234ae601c8aec5d510"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0071d1eae3fdb36b92e3c73dbebb1c816fe8926666ed971f30f5dd34fd3c279"
-    sha256 cellar: :any_skip_relocation, sonoma:        "0a04c9acd4cb3c7db85ed963159cb992b256a42af99e275b211b3a6b68c2841f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b33a6a23463638971d0e92712a1ee28589b3d68c3f7aba928fbfaa651eec4528"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3176cc6c4bd8d440817188f75d5d0ab597b042607b2e26b540dd896fac0e9a33"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a26b1b76feb639758e1d60716c15ad036f1a528c88d02429ab705f9f2f4ba2b3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9474bcfd6b06ddba5ea53a1b1d0702378e499503b9d83d8ed9433f3b81071ddd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6993e9adad559a98c913c687f1b3edf9008065568b30f81796d79ef3b4e9c896"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8b14587f9e64f253db7114a2026e1a98116fcde0c67efe90c7c7c82306ec14c0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "330ef43998d56cf5f650229f3b6fa6e91042d2c7a1d3ea9fce6d9f6d90317602"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "edf52c44b7c5215fa8241315ac7f5a91150ee243d935816ca338f8b43cc1cdc7"
   end
 
   depends_on "rust" => :build
