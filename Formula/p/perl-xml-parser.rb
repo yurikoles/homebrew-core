55e693b58bbbb103fdcd938486dff2ca4809c4f2
Michael Cho <michael@michaelcho.dev>
2026-03-16 18:04:33 -0400

    perl-xml-parser: install into vendor prefix

--- Formula/p/perl-xml-parser.rb
+++ Formula/p/perl-xml-parser.rb
@@ -4,8 +4,8 @@ class PerlXmlParser < Formula
   url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.47.tar.gz"
   sha256 "ad4aae643ec784f489b956abe952432871a622d4e2b5c619e8855accbfc4d1d8"
   license "Artistic-2.0"
-  revision 1
-  head "https://github.com/cpan-authors/XML-Parser.git", branch: "master"
+  revision 2
+  head "https://github.com/cpan-authors/XML-Parser.git", branch: "main"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fee3b040b53302d6739156e3fe6ec3d20d4a68976db97596fbc69ae67ca6f478"
@@ -16,24 +16,13 @@ class PerlXmlParser < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9b950e9fe9030de2638c912c2325831376ec6be4a485eb72c76cf9e909f3253"
   end
 
-  # macOS Perl already has the XML::Parser module
-  depends_on "perl"
+  depends_on "perl" # macOS Perl already has the XML::Parser module
   uses_from_macos "expat"
 
   def install
-    system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}"
+    system "perl", "Makefile.PL", "INSTALLDIRS=vendor", "PREFIX=#{prefix}"
     system "make"
     system "make", "install"
-
-    share.install prefix/"man"
-    perl_version = Formula["perl"].version.major_minor.to_s
-    site_perl = lib/"perl5/site_perl"/perl_version
-    (lib/"perl5").find do |pn|
-      next unless pn.file?
-
-      subdir = pn.relative_path_from(lib/"perl5").dirname
-      (site_perl/subdir).install_symlink pn
-    end
   end
 
   test do
