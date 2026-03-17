2120c7ae2812c331c5d0cc843d9263d32a8cdb98
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 14:35:44 +0000

    appwrite: update 15.0.0 bottle.

--- Formula/a/appwrite.rb
+++ Formula/a/appwrite.rb
@@ -6,12 +6,12 @@ class Appwrite < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5badbe6341c662802092b9e42c9d8783d0b30570556c90c257d7924e978a492b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "773c1447431fed6ed9b1c61b6ff5929087c41c71c87700c0409b17bb2d038f14"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "773c1447431fed6ed9b1c61b6ff5929087c41c71c87700c0409b17bb2d038f14"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d500cba193197a3fe69988274d7138fe3c5809be4648c6add4c41c08f535edfa"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0f01b52a85e4b96a956f8d186097fda3a799f93791a2be2aaee1d72f2b70fe28"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f01b52a85e4b96a956f8d186097fda3a799f93791a2be2aaee1d72f2b70fe28"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2c6653fca4e42a2281d0a75969004b1c90aaf017046b862c5d06f07cedf7f196"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "38243c9b255b4b332644f1a81178885eb10bc5265a19b885c444068a1154083c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "38243c9b255b4b332644f1a81178885eb10bc5265a19b885c444068a1154083c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4d4ab4baae8369b985d15dd6aa607f620ff6910573d029e984f13b21df4e8a37"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "57c3e5c89e9f576f19199f4da2d5e4ace0be39829502f9463b440a457aa4deb6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57c3e5c89e9f576f19199f4da2d5e4ace0be39829502f9463b440a457aa4deb6"
   end
 
   depends_on "node"
