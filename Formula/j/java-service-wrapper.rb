c391420b5bf6f47488cb882853a4e48ebd814604
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 14:40:53 +0000

    java-service-wrapper: update 3.6.5 bottle.

--- Formula/j/java-service-wrapper.rb
+++ Formula/j/java-service-wrapper.rb
@@ -8,12 +8,12 @@ class JavaServiceWrapper < Formula
   no_autobump! because: :incompatible_version_format
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "50f02c8a22fd1aaf7fdaaa368d6157331857801e0e7bf9a37d650b1221186232"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b470ea97a958934558739c40336c45903e533376b0273e51c6267fd4e9a7d953"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a70b5e29df4af3c8c06ab23e6b116bec094a27a5fe5f07d0c0874aaaa3cb8514"
-    sha256 cellar: :any_skip_relocation, sonoma:        "191cce9ca2610ecd1e78bb8adecd93c4a0cecd7a944d45fba843e86d6ea84062"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "609f36514596c3655739cbe1e39fbd842c0da0fd4b8a5cdcf9be41816283ecbf"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91b2cc21432a561472471c185e163d99786e0e299e7933dc039656939c27ab16"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "02d66dd4cb0b7c091065fe9a4ca074a9f1cde335a118fd78649ab83f2ef6b8a4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e5e79a5972a4e23d6a78762f16f5542044a507ff6bfae7f64b82a68ee9b8913b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a6706caef4cbceeb709a644463443c0d30d7648b2bf13205864a3bed6d71ccc4"
+    sha256 cellar: :any_skip_relocation, sonoma:        "78dfa3dd0a4ddfa5a47e120eb72ba8b7fb3f64d7f5fe466aecf1237d60e39eac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9fa2df42bef313633426f63cf7967f4871d9b18f1cf5ed5c4428b7206ba6618a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e1884526297eddb52a7b9b445049fb5d58cdadb03fe6890690893a39011d011a"
   end
 
   depends_on "ant" => :build
