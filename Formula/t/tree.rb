2ad9ab618fc5179bde53fb05bba4e3477346bc2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 00:07:34 +0000

    tree 2.3.2

--- Formula/t/tree.rb
+++ Formula/t/tree.rb
@@ -1,8 +1,8 @@
 class Tree < Formula
   desc "Display directories as trees (with optional color/HTML output)"
   homepage "https://oldmanprogrammer.net/source.php?dir=projects/tree"
-  url "https://github.com/Old-Man-Programmer/tree/archive/refs/tags/2.3.1.tar.gz"
-  sha256 "621ff2b4faf214d7023143f6f9d496117c7c75131927837750b904140aff48a1"
+  url "https://github.com/Old-Man-Programmer/tree/archive/refs/tags/2.3.2.tar.gz"
+  sha256 "22cf32e84e3eb508d97a9e991c2c3cc006b9dcf4afed201d96311c5c57d08fcf"
   license "GPL-2.0-or-later"
 
   bottle do
