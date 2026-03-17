6617a69f1b7a32c2e40e89751bdb76617d96ba05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 12:03:36 +0000

    claude-agent-acp: update 0.22.0 bottle.

--- Formula/c/claude-agent-acp.rb
+++ Formula/c/claude-agent-acp.rb
@@ -7,12 +7,12 @@ class ClaudeAgentAcp < Formula
   head "https://github.com/zed-industries/claude-agent-acp.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "d6e79ac2daa1e9b008bf06ed369d9b9ed9f6c0640389ad1a5e3d73756cc5b350"
-    sha256 cellar: :any,                 arm64_sequoia: "1ca044d322cd466406a50defd736fe8beadcf1cf914e14ba813d52c2fa988178"
-    sha256 cellar: :any,                 arm64_sonoma:  "1ca044d322cd466406a50defd736fe8beadcf1cf914e14ba813d52c2fa988178"
-    sha256 cellar: :any,                 sonoma:        "d2e7974c19b4b545e337a8f0b79139fc008c5df719de44cc24a6358a6cb60571"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bf114d4feee5274a207c4c8145d51fbe01764c11097f62e78cffe4398d4370c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ad93f0351aac79eb661b5d4b9b0fcac81d605db114c4a7e90954e622ef036c5"
+    sha256 cellar: :any,                 arm64_tahoe:   "632cc4dcdd8b0576f958864146b2a68e721ec42c82d82976d9a9708d602a03ff"
+    sha256 cellar: :any,                 arm64_sequoia: "50fb102f99d8a2e2968ae2c8908b970866800d951e5e7ec7a01c25b86abeae13"
+    sha256 cellar: :any,                 arm64_sonoma:  "50fb102f99d8a2e2968ae2c8908b970866800d951e5e7ec7a01c25b86abeae13"
+    sha256 cellar: :any,                 sonoma:        "38d3d03d7c1bb9270bef72865b84be4dbf1ea5717c528f58375253f1a5a5353f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d9f23bc48d3002e939aa00660ab95992ec03db09da9b8accb2395b87dfa68f60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f27af1f6ece880a4ad2a0a762e2292422f8420a36210c2cb12afc34565be87c"
   end
 
   depends_on "node"
