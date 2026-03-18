e377758ca51838c853d92594b7f7005f6aa89b14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 13:38:33 +0000

    ktfmt: add 0.62 bottle.

--- Formula/k/ktfmt.rb
+++ Formula/k/ktfmt.rb
@@ -5,6 +5,15 @@ class Ktfmt < Formula
   sha256 "266c4d774be0f61de17687a49b3acf454d5674dd30caace947c086ec2e00cd42"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a78b1d6dd17daf32f24b39c56462fd9b64fe78e110ee03f5cce9d7e720eaf1b1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8730ebfede680e8298d21438c5bad4cf3a088d088b3e8adaf1275fec54a18a3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8730ebfede680e8298d21438c5bad4cf3a088d088b3e8adaf1275fec54a18a3a"
+  end
+
   depends_on "gradle" => :build
   depends_on "openjdk@17"
 
