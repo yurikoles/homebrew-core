52dea32951b28766bae645fadb994f232646093d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 20:09:52 +0000

    eleventy 3.1.5

--- Formula/e/eleventy.rb
+++ Formula/e/eleventy.rb
@@ -1,19 +1,16 @@
 class Eleventy < Formula
   desc "Simpler static site generator"
   homepage "https://www.11ty.dev"
-  url "https://registry.npmjs.org/@11ty/eleventy/-/eleventy-3.1.2.tgz"
-  sha256 "d2c9d4399fc628076392b21237d97cd9574e96accca830c38f678efb1c0ba829"
+  url "https://registry.npmjs.org/@11ty/eleventy/-/eleventy-3.1.5.tgz"
+  sha256 "65941649a92338aad8021fc0d0df1954b632f31299579f3e0ac72ef2a20a70d4"
   license "MIT"
   head "https://github.com/11ty/eleventy.git", branch: "main"
-
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "6fa69586d48a0b58842bd657083c0ce11b2759790e10491ff2b3ca587e970aa6"
-  end
 
   depends_on "node"
 
   def install
     system "npm", "install", *std_npm_args
+    deuniversalize_machos libexec/"lib/node_modules/@11ty/eleventy/node_modules/fsevents/fsevents.node" if OS.mac?
     bin.install_symlink libexec.glob("bin/*")
   end
 
