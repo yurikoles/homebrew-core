bbc9f018b6ec161f75f15b1044978bbf28308bb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 17:48:54 +0000

    googleworkspace-cli: update 0.18.0 bottle.

--- Formula/g/googleworkspace-cli.rb
+++ Formula/g/googleworkspace-cli.rb
@@ -7,12 +7,12 @@ class GoogleworkspaceCli < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d0902c58fe2bb1220d30486f919a4da6450461259a5c852075e6e0474f7455c3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "87075526a592df13bf106741e894961f2cfb7bc3dd959c927bf1ea633ec037df"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "df8471aca68a49284f8b2e805fb7f0f43bb9de95afb5521648084d8fe7e22d92"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3e4635d407797fa35543761ebd569536c624ce7a3ea4c8993f85ad99c9ac0470"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "332701352c69f9f755d9371f2a28c347b157917e2c1e9a8d1ce55b3add1afa13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf74615d1ae9c2444ac43ab4b60be92457702af17f539b5b4ccd07a2805d8ec2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e917a5bc19f5deb5b8180dce2eb4b9659a4a4f818295bc5a4d0fc7b0b854613"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1527b3bf7e1eac9f73a3299850c4ff6915ce6e28167dd44b127fefe248538d48"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd3400f84d0d5864d344753d80a55ec7904c82b4021a92723a287e19cb6bfa3b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6c73f7fb60b47eafd5a1d5f5ebf0560a2610cfb5ed2b32517677cb6e86f54097"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "61abaad304e88593df0c8db500ac4a66f2ae7113620ade3c88fa0442ea8b4fb8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e708d5db85a216d796b6dc8341f4379391b266d1700b7727fd0ce6242485c78"
   end
 
   depends_on "rust" => :build
