5bafd6f648a30ef554f3d2dafe4ce8e1f5e0d47b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 09:43:09 +0000

    mistral-vibe: update 2.5.0_1 bottle.

--- Formula/m/mistral-vibe.rb
+++ Formula/m/mistral-vibe.rb
@@ -10,12 +10,12 @@ class MistralVibe < Formula
   head "https://github.com/mistralai/mistral-vibe.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e5936da4b02584944bbbd7601b79d9f52dd7b5e526b59140b6e06af62f031809"
-    sha256 cellar: :any,                 arm64_sequoia: "c791497223f30590cf2e589a7825b4b9471c6eb56a68c421d7c7d1e938aad539"
-    sha256 cellar: :any,                 arm64_sonoma:  "344671050f90bbc95dfd75065e553d2b6d167edb69262e7168d5e1b2123a1b10"
-    sha256 cellar: :any,                 sonoma:        "7a8a07c7bc5394b3e3a7de54a43b62df18b6b9b1aa440d362dc4f82b3d152dcc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cef11f56c214a88729f2eaed5360ede51499ebb40782600936212d9631d54a4d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc7edea595897317637aee57b3b129df941cc9e398b534340c43043968db992b"
+    sha256 cellar: :any,                 arm64_tahoe:   "f21a573e47baa603794e3fa487b86c46d2e133b921eea069d688bbb4bcb27a04"
+    sha256 cellar: :any,                 arm64_sequoia: "7395456ea62e8d09b53ce2d36c454d199a4dd9f963b6a1039d537a9b767745aa"
+    sha256 cellar: :any,                 arm64_sonoma:  "a5e73d8a69fe068e9c4b620f1fd1f030f39e9d8e9620464b7d615560ae256795"
+    sha256 cellar: :any,                 sonoma:        "7c0257ddbbef59180d9c00f144de7a167a23d4fe1d92ac9e853852bad4790dec"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f9cacebaf3e25caef3c42b4527511b2f97196d49a17805f8a32fc847f6d347f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7f38c90967abd6e42b5c4e3a8cec90567fc36503165d2de84c39a5db104227b5"
   end
 
   depends_on "rust" => :build
