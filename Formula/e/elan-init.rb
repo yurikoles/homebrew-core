4992a93847a61f762bdcaf39af128b581b71956e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 14:27:27 +0000

    elan-init 4.2.1

--- Formula/e/elan-init.rb
+++ Formula/e/elan-init.rb
@@ -1,8 +1,8 @@
 class ElanInit < Formula
   desc "Lean Theorem Prover installer and version manager"
   homepage "https://github.com/leanprover/elan"
-  url "https://github.com/leanprover/elan/archive/refs/tags/v4.2.0.tar.gz"
-  sha256 "451f194ab00395524d3843e3a0841273ce91ac2136bd1ef6f62c7d8f63c3dd17"
+  url "https://github.com/leanprover/elan/archive/refs/tags/v4.2.1.tar.gz"
+  sha256 "637cf1d0746d05979ec9f97f577ac57f38f1606fc5c596d10579b9b367a3157d"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/leanprover/elan.git", branch: "master"
 
