afa759d552f5868087000d8ba7e206a16961f084
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 17:39:56 +0000

    llmfit 0.7.7

--- Formula/l/llmfit.rb
+++ Formula/l/llmfit.rb
@@ -1,8 +1,8 @@
 class Llmfit < Formula
   desc "Find what models run on your hardware"
   homepage "https://github.com/AlexsJones/llmfit"
-  url "https://github.com/AlexsJones/llmfit/archive/refs/tags/v0.7.6.tar.gz"
-  sha256 "187bb2f5df15faad7eaf049742b474e0433561277340b7556043a57505af2d89"
+  url "https://github.com/AlexsJones/llmfit/archive/refs/tags/v0.7.7.tar.gz"
+  sha256 "b214a57ea9ea4f4f110183f57b4cb21ecb8d817c4e0ef16d0d42bf0e1f0aad3e"
   license "MIT"
   head "https://github.com/AlexsJones/llmfit.git", branch: "main"
 
