bf65b10c467e24e57925bab7961d56b3a2c781e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 12:09:51 +0000

    radare2 6.1.2

--- Formula/r/radare2.rb
+++ Formula/r/radare2.rb
@@ -1,8 +1,8 @@
 class Radare2 < Formula
   desc "Reverse engineering framework"
   homepage "https://radare.org"
-  url "https://github.com/radareorg/radare2/archive/refs/tags/6.1.0.tar.gz"
-  sha256 "52847eefc0fa9713b8c36e9afe45e0eee0e2d140e4f284c1ad43ae442f76a0a9"
+  url "https://github.com/radareorg/radare2/archive/refs/tags/6.1.2.tar.gz"
+  sha256 "f0677266eeb505bec4df12961cbb2adb92cf8202ff3dab05690bcb24c4bf5c52"
   license "LGPL-3.0-only"
   head "https://github.com/radareorg/radare2.git", branch: "master"
 
