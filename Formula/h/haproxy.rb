edb19422c3fb68fcc0ea4728a7e02b64e8b24882
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 17:30:05 +0000

    haproxy 3.3.6

--- Formula/h/haproxy.rb
+++ Formula/h/haproxy.rb
@@ -1,8 +1,8 @@
 class Haproxy < Formula
   desc "Reliable, high performance TCP/HTTP load balancer"
   homepage "https://www.haproxy.org/"
-  url "https://www.haproxy.org/download/3.3/src/haproxy-3.3.5.tar.gz"
-  sha256 "9de6e765b426f07c1080aadd2fba5b682a1cc175fe8eb45d5eb948292a866e02"
+  url "https://www.haproxy.org/download/3.3/src/haproxy-3.3.6.tar.gz"
+  sha256 "e69cb5dc59e4eb1ff72bcebf30d55f0919803c686e428c0c3a5903f2cf7c1fb6"
   license "GPL-2.0-or-later" => { with: "openvpn-openssl-exception" }
 
   livecheck do
