80d0b1b05aa9d318124f0cf8526e10cb00f37a11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 08:57:51 +0000

    varlock 0.6.0

--- Formula/v/varlock.rb
+++ Formula/v/varlock.rb
@@ -1,8 +1,8 @@
 class Varlock < Formula
   desc "Add declarative schema to .env files using @env-spec decorator comments"
   homepage "https://varlock.dev"
-  url "https://registry.npmjs.org/varlock/-/varlock-0.5.0.tgz"
-  sha256 "2672047ba0fd2d6a2a73f3925f8f0d404f31cce31ccaa759cae8ae6c171f8bb2"
+  url "https://registry.npmjs.org/varlock/-/varlock-0.6.0.tgz"
+  sha256 "b3b203c138c9d7795518e3b3babfc5bc9955549b1b4f931d8299c1abecace634"
   license "MIT"
 
   bottle do
