c3cf49e16ff178228e0e0e7124e5acf9fe1fe985
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-19 23:03:00 +0000

    blueprint-compiler 0.20.3

--- Formula/b/blueprint-compiler.rb
+++ Formula/b/blueprint-compiler.rb
@@ -4,8 +4,8 @@ class BlueprintCompiler < Formula
 
   desc "Markup language and compiler for GTK 4 user interfaces"
   homepage "https://gnome.pages.gitlab.gnome.org/blueprint-compiler/"
-  url "https://download.gnome.org/sources/blueprint-compiler/0.20/blueprint-compiler-0.20.0.tar.xz"
-  sha256 "ec786d66f583e8296c845f1f82834d27b369f39d55a6380b34880493e22db382"
+  url "https://download.gnome.org/sources/blueprint-compiler/0.20/blueprint-compiler-0.20.3.tar.xz"
+  sha256 "4b28ef6fcf85bdc22657f4add4fb6051d370f85978bf8bbb9ac814514c7b10f9"
   license "LGPL-3.0-or-later"
   head "https://gitlab.gnome.org/GNOME/blueprint-compiler.git", branch: "main"
 
