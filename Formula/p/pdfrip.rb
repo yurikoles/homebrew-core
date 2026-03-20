eb9cd08879ebd6541eeb8b3078f661cb895a94ed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 05:44:09 +0000

    pdfrip: update 3.0.0 bottle.

--- Formula/p/pdfrip.rb
+++ Formula/p/pdfrip.rb
@@ -7,13 +7,12 @@ class Pdfrip < Formula
   head "https://github.com/mufeedvh/pdfrip.git", branch: "main"
 
   bottle do
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1036db7676a0822b721390e43e797e7e846dbd3b4f0c35acc3fb71fee338f938"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68c75d4a427d4512dce141506acc4d3b02e8640d3fb54a30bfc0c9be4ffa525a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eeac32a01baca1d879c37adc93518653405ffc1437ef2551eed0d7bee45a9c13"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b27b773f78bba7b8d64b60cc5c8b8cdef12d45574be2d4881223bace41d64acd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0db2c8b883091a68cdda3fe1f4da41213bd041fbe8b93862b86e744baa60c75"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de8fc88b6d291c32c7e3b0ed9fdda602f2a237e8478921f07b56bf550dbcbbde"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b5856a2f08d97cb02cfca8c54d867f4265ca284b864ec7c7818c3a3beb4523a3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4f70dca6d1b3856a4082fc53d5532cdd89a827109475fc856c9f63e4623de35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "db92291f34f5c62fbf965660d077527b507d34d7f0bc4c43465fdcb515bd434c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b5e175aa463c916e4ad738afdde1454cad704e10ab03c7eeae7b3ab2e1ff062a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c56527b7206b55fa93791339360963c87a49e8f7af73d617b65b985c3617b852"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fc702689e1725749aef7fa49952a53a740a1e21ca15032928d97d3ed3933c2a"
   end
 
   depends_on "cmake" => :build
