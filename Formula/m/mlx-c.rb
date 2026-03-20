e13faad3ecdf5fd184b1145874fc6132c1f0f967
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 03:19:46 +0000

    mlx-c 0.6.0

--- Formula/m/mlx-c.rb
+++ Formula/m/mlx-c.rb
@@ -1,10 +1,9 @@
 class MlxC < Formula
   desc "C API for MLX"
   homepage "https://ml-explore.github.io/mlx-c/build/html/index.html"
-  url "https://github.com/ml-explore/mlx-c/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "dcfc404d7004e6da70170c669dbc920913cb25a59c9f7dac781caf92e524cc86"
+  url "https://github.com/ml-explore/mlx-c/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "6ec2eab86ed3ce661c0d9b834027870651546138b7b4470fa8ef5533498c79aa"
   license "MIT"
-  revision 1
 
   bottle do
     sha256 cellar: :any, arm64_tahoe:   "4308e8bf4632333803c8c2e2d91bdcdd540469a2d216f9be7e79c536beea0f31"
@@ -24,12 +23,6 @@ class MlxC < Formula
   fails_with :clang do
     build 1500
     cause "Requires C++20 support"
-  end
-
-  # fix build with mlx 0.31.0 API changes, upstream pr ref, https://github.com/ml-explore/mlx-c/pull/103
-  patch do
-    url "https://github.com/chenrui333/mlx-c/commit/1a51ff8cd618e1967f71fc37cba7044fcf3e746f.patch?full_index=1"
-    sha256 "2256935dce8e552bd8f143ec279bb0d11fb802eeb23ac868e4d28c506ab72a8a"
   end
 
   def install
