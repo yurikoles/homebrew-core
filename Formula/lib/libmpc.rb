57b6e2d8017225e16553e0804918fd913f357646
GunniBusch <leon@adomaitis.de>
2026-03-20 13:54:56 +0000

    libmpc 1.4.0

--- Formula/lib/libmpc.rb
+++ Formula/lib/libmpc.rb
@@ -1,9 +1,9 @@
 class Libmpc < Formula
   desc "C library for the arithmetic of high precision complex numbers"
   homepage "https://www.multiprecision.org/"
-  url "https://ftpmirror.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz"
-  sha256 "ab642492f5cf882b74aa0cb730cd410a81edcdbec895183ce930e706c1c759b8"
+  url "https://ftpmirror.gnu.org/gnu/mpc/mpc-1.4.0.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/mpc/mpc-1.4.0.tar.xz"
+  sha256 "3210b3a546b1cb00c296ca360891d7740ee6ff06deb02a27a35b20cd3c0bb1a5"
   license "LGPL-3.0-or-later"
 
   bottle do
