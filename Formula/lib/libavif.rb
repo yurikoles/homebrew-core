8d9a249860eb158431140f2c494a004433159fec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 20:18:06 +0000

    libavif 1.4.1

--- Formula/lib/libavif.rb
+++ Formula/lib/libavif.rb
@@ -1,8 +1,8 @@
 class Libavif < Formula
   desc "Library for encoding and decoding .avif files"
   homepage "https://github.com/AOMediaCodec/libavif"
-  url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "713e2b998ca0bf5473fe4624afdbc7fa9f6e4799dd414020fe67d56f6997bf4e"
+  url "https://github.com/AOMediaCodec/libavif/archive/refs/tags/v1.4.1.tar.gz"
+  sha256 "d4aea31a4becb3273ba7968221be2e48148ba05eb8a68d14e671963e17785648"
   license "BSD-2-Clause"
 
   bottle do
@@ -25,7 +25,14 @@ class Libavif < Formula
     depends_on "zlib-ng-compat"
   end
 
+  resource "libargparse" do
+    url "https://github.com/kmurray/libargparse/archive/ee74d1b53bd680748af14e737378de57e2a0a954.tar.gz"
+    sha256 "7727b0498851e5b6a6fcd734eb667a8a231897e2c86a357aec51cc0664813060"
+  end
+
   def install
+    resource("libargparse").unpack(buildpath/"ext/libargparse")
+
     args = %W[
       -DCMAKE_INSTALL_RPATH=#{rpath}
       -DAVIF_CODEC_AOM=SYSTEM
