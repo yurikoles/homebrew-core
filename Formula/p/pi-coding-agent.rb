aa1fc21034ce5b7e4eb7acf2f85e71d32f14b578
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 04:00:18 +0000

    pi-coding-agent: update 0.61.0 bottle.

--- Formula/p/pi-coding-agent.rb
+++ Formula/p/pi-coding-agent.rb
@@ -6,12 +6,12 @@ class PiCodingAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "02132a2807aa635de813dbff22c96e607038f416899c0b4fe5de8d466fca0bcc"
-    sha256 cellar: :any,                 arm64_sequoia: "f68b7926a07a5c7bb9d249d4b5cf7d47c934346aa149cfae58432cb3475b1c1b"
-    sha256 cellar: :any,                 arm64_sonoma:  "f68b7926a07a5c7bb9d249d4b5cf7d47c934346aa149cfae58432cb3475b1c1b"
-    sha256 cellar: :any,                 sonoma:        "ee86f4b7486766fe17e22b72088f3a22020e2367e94f1413ba0c4d9a527beb72"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "910337a3817b99323d14aa01eddb7d80edabb118d7dc782a7f071669feae531b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d27d8410d2d76a3757f32e840e19f90864293265bbd05a679eac5a58d8530e0d"
+    sha256 cellar: :any,                 arm64_tahoe:   "b53301a7be527af97ca7c196d361baf99bec460c0eca173461333fd774209407"
+    sha256 cellar: :any,                 arm64_sequoia: "eb922ce011476af8d12043fda86ffdf438be95d9d1314a7b77428ed3c0be00d9"
+    sha256 cellar: :any,                 arm64_sonoma:  "eb922ce011476af8d12043fda86ffdf438be95d9d1314a7b77428ed3c0be00d9"
+    sha256 cellar: :any,                 sonoma:        "9e8568cf67833afe0acf4ba0385928314d9ee098b079b68261dcf5e80e959e5c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0f37b8907dd8fd471ba12fb8b89fa5b06dbb92a48c9ce7810641683ab5e20fd2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "09de4f76ce21b85be007f632143ee5338f8d8b0e4b0a6b4375ad96866abbc723"
   end
 
   depends_on "node"
