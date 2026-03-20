cff35bdc50e618471860e5e8c0c4120f5f372f59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 00:12:25 +0000

    radicle: update 1.7.1 bottle.

--- Formula/r/radicle.rb
+++ Formula/r/radicle.rb
@@ -13,12 +13,12 @@ class Radicle < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "97d2522d6ee9bc1c2df563018f2587a38b9d7e2001f1f0a266991693e84dd2d7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96886e6d55ac7d5ced083a6cd8d8f1f727f5af2d5e53504cd13fbb43a3c1c24b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "61e003a802363d21b7aa4705def39deec67c4dfdfdb971a59d2b724cfcfbeea2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "e7f5a01c3d30c08e6da3e52b4ea62aee7a6a0ac67680e7580eae93c71256a82b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e421678261a5a3daa478cfb8babc995241d3c883c3af5708e8a820661c5994c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a342d483535e21aa749ef8876c2c286bfa1a75380a0983589b82fcf881709b2e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e71464307cab48d0fa169d010bb1b6207289a3ed02e54b31cf6d6a2a4cfdcc0a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78a078aea6cf4d58e7f973890afef6a318186c5e5cf02102297365380b105509"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd67bfec110bd744f7c16cd41a0d95bb0067553182f2130e968acb0fa3695512"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c03efe8cd243725f5b7bcbdb4fd8f89863cef6d29a06d91c39ff2dc9ce5f4a8d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c667b42aff1b507a927bb5033f70c11cf84dd27cb84e3f05bbc2bc0f86bbcf7b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "727c6f48e6f98916cf6d61188c15bfb13e86b103816cdfbe381b3ee764a38257"
   end
 
   depends_on "asciidoctor" => :build
