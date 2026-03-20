cb85628023c845044d711cc4addc616c355cd769
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 18:11:09 +0000

    zsh-completions: update 0.36.0 bottle.

--- Formula/z/zsh-completions.rb
+++ Formula/z/zsh-completions.rb
@@ -17,8 +17,7 @@ class ZshCompletions < Formula
   head "https://github.com/zsh-users/zsh-completions.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, all: "98d3985c4c7883eceb5136fef89f1e169067fe290eb540efeb5fec4ace5396be"
+    sha256 cellar: :any_skip_relocation, all: "b049deb74035408154c97b57d9834510d55503c64b172b8a89937b7e6118cca5"
   end
 
   uses_from_macos "zsh" => :test
