a07f568b5dc801b13c1e916441862faf02157ce7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 15:28:23 +0000

    parca: update 0.27.0 bottle.

--- Formula/p/parca.rb
+++ Formula/p/parca.rb
@@ -7,12 +7,12 @@ class Parca < Formula
   head "https://github.com/parca-dev/parca.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "013e81c3488c7873674587696da1a96c74117bfe6054482af72b017fbac58e4d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8753efb3daa40735c3894fc645dcfa319118b9c90ae5ccd9c9ebc3729278f5f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d02a0d2854894ed89815fff65d7fb129f981accdea5b72287f2cc3d29351303"
-    sha256 cellar: :any_skip_relocation, sonoma:        "2ebf3ad65142faa07cdc2ea21a035b0bacbc497488406514ae1f278db0ef427b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "24d5715a616b84334d157f371dbbd80503c321afda66e33a9bf3f6a6a96588b7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "744789f3b103028f71511b288dd5dc00c2811c4a17d61977a590ae3a9ece8f23"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "145db40463bfee22b901119aed62e55b4342edd4cdffd913fba630ed56273294"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb77f3c422b9565a154c4137db5f3bee9b16819a57e73442fbefcc92f1d97fe5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43c752566c0925b105f039e9c8662beaf3b76519d62d761f91d495197d6cccf6"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8b65d914d447ac64d70cc756b992b32856845ae72fb78598cca26f0b51f6edba"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ead8cf9c6ac99816525079145f126228ee949ca6d226667d9b7f47d59246a13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "117b281cfed284ebcbec777f97fef9c1f7567ee22133eecdeae4f53f95120ec2"
   end
 
   depends_on "go" => :build
