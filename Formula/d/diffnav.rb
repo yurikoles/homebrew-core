d26600a05e93011a9a1d3e08e95e213ff9093ac6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 17:20:58 +0000

    diffnav: update 0.11.0 bottle.

--- Formula/d/diffnav.rb
+++ Formula/d/diffnav.rb
@@ -7,12 +7,12 @@ class Diffnav < Formula
   head "https://github.com/dlvhdr/diffnav.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "85a032f50014ba0f72476a7ccef89658a25d9eadda5505fb2c5003a4608b3c2f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "85a032f50014ba0f72476a7ccef89658a25d9eadda5505fb2c5003a4608b3c2f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "85a032f50014ba0f72476a7ccef89658a25d9eadda5505fb2c5003a4608b3c2f"
-    sha256 cellar: :any_skip_relocation, sonoma:        "87168ecedd81acf1d48f881bddba4de4d47bca912aa66801b47c4ad5a383b60e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "375724998a7289c111b9a3a881aa87a087e8574858b3c08d093261bdbb00a7f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b788c32808d36e472591f72725c18c972c1e97821410b7d16a9ca96ba8387908"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2fab53e6a649edf1434ee108d81cf4d8810713ef5fd14c7cbe6b05059f22c850"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fab53e6a649edf1434ee108d81cf4d8810713ef5fd14c7cbe6b05059f22c850"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2fab53e6a649edf1434ee108d81cf4d8810713ef5fd14c7cbe6b05059f22c850"
+    sha256 cellar: :any_skip_relocation, sonoma:        "4d11a088fb274bda33190e0acdc786157ae32d4b303ddb42a1442a903df1e4dd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3e777d7c97922dda39af604506d950694fa8c3bf4fb2d6110210bc49eda676ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42d4a4b37e195334e09191b9186ed35d26e9b152ec46850594f66cd494d6b545"
   end
 
   depends_on "go" => :build
