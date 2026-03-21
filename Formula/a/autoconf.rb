d161698f61dcb2847c35fdfd36894c4b4fe0100e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 07:55:33 +0000

    autoconf 2.73

--- Formula/a/autoconf.rb
+++ Formula/a/autoconf.rb
@@ -1,9 +1,9 @@
 class Autoconf < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf/"
-  url "https://ftpmirror.gnu.org/gnu/autoconf/autoconf-2.72.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.72.tar.gz"
-  sha256 "afb181a76e1ee72832f6581c0eddf8df032b83e2e0239ef79ebedc4467d92d6e"
+  url "https://ftpmirror.gnu.org/gnu/autoconf/autoconf-2.73.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/autoconf/autoconf-2.73.tar.gz"
+  sha256 "259ddfa3bddc799cfb81489cc0f17dfdf1bd6d1505dda53c0f45ff60d6a4f9a7"
   license all_of: [
     "GPL-3.0-or-later",
     "GPL-3.0-or-later" => { with: "Autoconf-exception-3.0" },
