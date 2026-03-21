1943f60d71cee3f9056562f4459722a46ed032ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 02:40:18 +0000

    aws-iam-authenticator 0.7.12

--- Formula/a/aws-iam-authenticator.rb
+++ Formula/a/aws-iam-authenticator.rb
@@ -1,8 +1,8 @@
 class AwsIamAuthenticator < Formula
   desc "Use AWS IAM credentials to authenticate to Kubernetes"
   homepage "https://github.com/kubernetes-sigs/aws-iam-authenticator"
-  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/archive/refs/tags/v0.7.11.tar.gz"
-  sha256 "1869fd23eb19e8276ee6a4d4bdf1334cf82c7baee849eab7a9f85519d4318221"
+  url "https://github.com/kubernetes-sigs/aws-iam-authenticator/archive/refs/tags/v0.7.12.tar.gz"
+  sha256 "a8edf13b033400ae9bf5c0d354b5aa98ac133d3d781dece4568ae9de5e74eda9"
   license "Apache-2.0"
   head "https://github.com/kubernetes-sigs/aws-iam-authenticator.git", branch: "master"
 
