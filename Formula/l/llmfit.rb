8f4ba97504a838197b3dd6ec419942f70e4f8221
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 19:24:42 +0000

    llmfit: update 0.8.3 bottle.

--- Formula/l/llmfit.rb
+++ Formula/l/llmfit.rb
@@ -7,12 +7,12 @@ class Llmfit < Formula
   head "https://github.com/AlexsJones/llmfit.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "272b40922324e835f22310d0f7601fbd8f2e3c896a184c8a479ff2f6b4a44f3a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb332fc9eb5b9eddf9e5b816faef3200cb875e4e6567b908c22c150c4819377b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ecb5dc2300e064c2cbe74ba40f378b0045b8d4dabe9c92f60b46cb06b75d8f8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3ecdc1bdf73eedd357a19159c5005067a1b2d9e42db26b237adb4241af492120"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7209e17d38492c5e38da0d150d193e68b5123b4a0d97f54013d51f79e9199359"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e38772d0e8af02e5e38ac99ea54d0fa17538565f90bf1748ade5e104f627da0f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ca2c5f12f6dd3771f0485d9125bc8b3a44062b3891969e3cde841eae7d42ee8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b1c81c2926a12d00c4f6c56dd798243c7358510a12417c6d788c5f273b0d657"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "59c6414e66f1d2411c12e67def7860a9fbd9a4895a7da95b58635cc99008bd09"
+    sha256 cellar: :any_skip_relocation, sonoma:        "d856649ce609bef4132e9fbadb5b980bf8577be2344a4218f716378d9191fe69"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7a9fd2b20abc9ea166b2998c8fca9b50e723319eecc849af38a38cfb4d8782aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19af86fd03436f9dddda8d1527a1f98e9998e672d15e15530a13cce7debd7400"
   end
 
   depends_on "rust" => :build
