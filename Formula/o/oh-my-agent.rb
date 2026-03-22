4f6ab255da7fb46225a57e38782e50a1bc1939ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 09:51:36 +0000

    oh-my-agent: update 4.2.1 bottle.

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -6,7 +6,7 @@ class OhMyAgent < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "0c3fff8b414e008d897ca0628036fd201e81282a860ef6f2dbaa4d73e59cc79a"
+    sha256 cellar: :any_skip_relocation, all: "d464815ea0b6454cc1e9a0fbb21539aa67463aa46fcb77287d26d76caf5c8e59"
   end
 
   depends_on "node"
