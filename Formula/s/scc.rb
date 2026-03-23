05a3a7fc83f6295a98b0bcff5da90c6ac4556e6d
Thierry Moisan <thierry.moisan@gmail.com>
2026-03-23 18:15:48 -0400

    scc: add shell completions

--- Formula/s/scc.rb
+++ Formula/s/scc.rb
@@ -23,6 +23,7 @@ class Scc < Formula
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
+    generate_completions_from_executable(bin/"scc", shell_parameter_format: :cobra)
   end
 
   test do
