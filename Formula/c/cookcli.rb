5189c3a0d0ef1207c6f695effca297c33db330ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 21:46:21 +0000

    cookcli: update 0.27.0 bottle.

--- Formula/c/cookcli.rb
+++ Formula/c/cookcli.rb
@@ -7,12 +7,12 @@ class Cookcli < Formula
   head "https://github.com/cooklang/cookcli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "06717611403c341717fe0c15488c4788b22451a9960890ccaa5f6dd4392f06a9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "18ebc479f92b9772b81a14f9b912692224b5961001e67397e7da748bfd4e0622"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e94d418efb1c24b302af4897126adfac292734eb7eadcb6dc6f900266595a014"
-    sha256 cellar: :any_skip_relocation, sonoma:        "38e7ade9256daf86dc2b32665ee5f1d4dca9b3542f110608b4afd93a656bd716"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca16c47072b9a548bf9641a36ec1ba84c3832699febbb8fc22b3b223354d1aa3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c81fb52a5e6702f4335a48dfa29a52e53272a8a18f55ad21cdbca619a316cd9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3543077bd30433950f1199e8397b6dc214de9cf942cb024ddf0ea02c45bfb7bc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e34f0c61743a9feb7fb45a035c4322cce176b7e35200734f52d999683b10b9ac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8d7bfd8f3b2d6c90e05c7e53a1b0c7c763306d26aaaa2a63ce6bc2bd28c66e03"
+    sha256 cellar: :any_skip_relocation, sonoma:        "fc945ff3128bc57a21e1c0e0c9f41590c1694f29f616e52ca3b0b06127ee0524"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "52466524cb4b6dabb0752d52bcf1e53261c955b1d40a5aa9be50c4ad319cdef9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e124272fc06acb8189fe4e4506e7ed0a4ade1260a1329d5b767688ef82ad97f"
   end
 
   depends_on "node" => :build
