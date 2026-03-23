c8cd5d043fd984b865dae79887bd2d30df9a8983
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 21:20:58 +0000

    tomcat@10 10.1.53

--- Formula/t/tomcat@10.rb
+++ Formula/t/tomcat@10.rb
@@ -1,9 +1,9 @@
 class TomcatAT10 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.52/bin/apache-tomcat-10.1.52.tar.gz"
-  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.52/bin/apache-tomcat-10.1.52.tar.gz"
-  sha256 "5d3583061085d412d5132a36c5422ad96adc8d5bd5869013d707a920696c9457"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.1.53/bin/apache-tomcat-10.1.53.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.53/bin/apache-tomcat-10.1.53.tar.gz"
+  sha256 "44ef5dc9c99e52e4e981fb6b6ab898023415e300f72e6e0aa0623d0a10413782"
   license "Apache-2.0"
 
   livecheck do
