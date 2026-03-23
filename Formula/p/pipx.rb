1d9ad97fdbc8453fce46c377951844660afb7f29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 21:05:37 +0000

    pipx 1.11.0

--- Formula/p/pipx.rb
+++ Formula/p/pipx.rb
@@ -3,8 +3,8 @@ class Pipx < Formula
 
   desc "Execute binaries from Python packages in isolated environments"
   homepage "https://pipx.pypa.io"
-  url "https://files.pythonhosted.org/packages/37/20/3f7f72992b4e608c8834add957f4bc841dd6b4f77540bff2444c30c24754/pipx-1.10.1.tar.gz"
-  sha256 "3c05159c1d861a9b5ef182dcb15a60acb07b3b4c16162099ec1c84a8011a48a8"
+  url "https://files.pythonhosted.org/packages/5c/f6/a7af5727ecc6f390315471c95ed64eac03b33278d8bc92d9c81229a31f84/pipx-1.11.0.tar.gz"
+  sha256 "fa6ab8ab05084cd86bf38ed78e745c4c82ff5325b43ba73d836521b4815dab28"
   license "MIT"
   head "https://github.com/pypa/pipx.git", branch: "main"
 
