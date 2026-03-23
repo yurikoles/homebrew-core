19953c5fc28283ed3e8f39ab93d996b323eb51b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 01:43:08 +0000

    roapi: update 0.12.7 bottle.

--- Formula/r/roapi.rb
+++ Formula/r/roapi.rb
@@ -6,13 +6,12 @@ class Roapi < Formula
   license "Apache-2.0"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6116ecb02c112d2f990024d034ba242894e9e4a023f6756f883e0ec58b3408e2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3bf66401f133a7d2e9234280571eed8cf06b9f1b98802762b5995b456414077"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1d1d6423d420c65fe3f2b414fdd0c33a67226d5afae78c5ddebf6e4dea35931"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1f2e0e0df5b406870033b3284ad136e6566c4f1fefb32c8d7c36f75c42f3e931"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "528ea8f1d1cb5818ac4259bddd081f9e03069e78cb254f8f74c3de15f0720eb3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8b39ad8324ea2d41fa80ff81a6c623453ab504752e02c915eed48d11b212616"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "206a038df2a8bcfc3b22cae42d0ba36a22c8d75bc5347eef165abca1fd0fd291"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4392cba3e8df84b89d7a1da7a6bf3753172a98058f0dcb109e4c050a9ac2ef82"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a90e2178cf2ef6830efa3d31f53311129508b67b1be6152d2f26b8555946047d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f0cdbe9d524400a20e42412b7335fba2d39c091633b81e268ec8a7370f9d879e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d43120591307e4b3423f72675135854c05a634ce79cb1a28812bb345bbcb8f34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e85bf2d4fd6880821e4300f86781f5ec072c5c5be68301c76c04452e0090138"
   end
 
   depends_on "rust" => :build
