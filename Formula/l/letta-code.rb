d0c55fa71b8714c671544d878ead17fc24a57d72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 04:47:40 +0000

    letta-code: update 0.19.7 bottle.

--- Formula/l/letta-code.rb
+++ Formula/l/letta-code.rb
@@ -6,12 +6,12 @@ class LettaCode < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "8c8d5c5ee2c32e8934069380e5c4366ba37574eae6c3946639b45ce48cb39857"
-    sha256 cellar: :any,                 arm64_sequoia: "7b0139655c9be2226cc210fe9ed0c0eb412a1aa8a0b1d627e9bb1280bf95a67e"
-    sha256 cellar: :any,                 arm64_sonoma:  "7b0139655c9be2226cc210fe9ed0c0eb412a1aa8a0b1d627e9bb1280bf95a67e"
-    sha256 cellar: :any,                 sonoma:        "860cfd3f9f6efe36a45ee43517f7d52b3f252adc8eebaa92aeced0b9bfccf819"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca895c19ba765e440a22c1bb53b65eb6875a85fd8ed62dcdfa505bc53c4ded9b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6d201ab2b61918c3f8a49a79abfee62817ec13aaa31a1b84aeced335a2ef94e9"
+    sha256 cellar: :any,                 arm64_tahoe:   "a390934366643a547223a558030e61d17af55ea616dae208a96f08c0721cdb25"
+    sha256 cellar: :any,                 arm64_sequoia: "9b92507020f2fb4849f572ded1e8f1107a3c48b80ec23557274065fd6ae8f2a3"
+    sha256 cellar: :any,                 arm64_sonoma:  "9b92507020f2fb4849f572ded1e8f1107a3c48b80ec23557274065fd6ae8f2a3"
+    sha256 cellar: :any,                 sonoma:        "0e289613fe4bae1b6cabb64527ee0e9346be6c12c8768a826e948f410af26a30"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a0e28d49526a4662171c6c39ded5e5adc8006191142cb17aab825c0c1e254dff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "711b006ff66a7f9ff2691e03c0f2cb34e2ac07a8250ac5c532492ed7b5934334"
   end
 
   depends_on "node"
