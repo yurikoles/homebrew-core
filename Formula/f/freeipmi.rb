1462cc499936db0d8e01be6ab9acc7549e407424
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 02:54:08 +0000

    freeipmi 1.6.17

--- Formula/f/freeipmi.rb
+++ Formula/f/freeipmi.rb
@@ -1,9 +1,9 @@
 class Freeipmi < Formula
   desc "In-band and out-of-band IPMI (v1.5/2.0) software"
   homepage "https://www.gnu.org/software/freeipmi/"
-  url "https://ftpmirror.gnu.org/gnu/freeipmi/freeipmi-1.6.16.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.16.tar.gz"
-  sha256 "5bcef6bb9eb680e49b4a3623579930ace7899f53925b2045fe9f91ad6904111d"
+  url "https://ftpmirror.gnu.org/gnu/freeipmi/freeipmi-1.6.17.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/freeipmi/freeipmi-1.6.17.tar.gz"
+  sha256 "16783d10faa28847a795cce0bf86deeaa72b8fbe71d1f0dc1101d13a6b501ec1"
   license "GPL-3.0-or-later"
 
   bottle do
