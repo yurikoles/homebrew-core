f5ce860149b2b9b27ce7a5f89f044e6318602b7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 23:16:02 +0000

    googleworkspace-cli 0.22.0

--- Formula/g/googleworkspace-cli.rb
+++ Formula/g/googleworkspace-cli.rb
@@ -2,8 +2,8 @@ class GoogleworkspaceCli < Formula
   desc "CLI for Drive, Gmail, Calendar, Sheets, Docs, Chat, Admin, and more"
   homepage "https://developers.google.com/workspace"
   # We cannot install from the npm registry because it installs precompiled binaries
-  url "https://github.com/googleworkspace/cli/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "35c19b1068c181316b223e1d1e30ec762c24973fd779d96fa2881d27fbd62bcf"
+  url "https://github.com/googleworkspace/cli/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "bfe44bc17113b798522d74339a276ebf7df049a4c6b56d69bb14527da11fb699"
   license "Apache-2.0"
 
   bottle do
@@ -20,7 +20,7 @@ class GoogleworkspaceCli < Formula
   conflicts_with "gws", because: "both install a `gws` binary"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args(path: "crates/google-workspace-cli")
   end
 
   test do
