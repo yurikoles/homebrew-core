471c34f77992e615cb6d8d2ded6dc3bedf9f026b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 08:14:56 +0000

    fedify 2.1.0

--- Formula/f/fedify.rb
+++ Formula/f/fedify.rb
@@ -1,8 +1,8 @@
 class Fedify < Formula
   desc "CLI toolchain for Fedify"
   homepage "https://fedify.dev/cli"
-  url "https://github.com/fedify-dev/fedify/archive/refs/tags/2.0.7.tar.gz"
-  sha256 "0d4ef8dd19b003df46b08e9a2675dd1a6e324d156988502d0409348f82d77052"
+  url "https://github.com/fedify-dev/fedify/archive/refs/tags/2.1.0.tar.gz"
+  sha256 "421cd41101454fda2aa0dc281e68002e36083e642da15344baf38935c999a081"
   license "MIT"
   head "https://github.com/fedify-dev/fedify.git", branch: "main"
 
