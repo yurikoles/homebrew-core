17d238b376395c3ac5228ffbcceb943f7958a8b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 07:15:22 +0000

    git-pages-cli: update 1.7.0 bottle.

--- Formula/g/git-pages-cli.rb
+++ Formula/g/git-pages-cli.rb
@@ -7,12 +7,12 @@ class GitPagesCli < Formula
   head "https://codeberg.org/git-pages/git-pages-cli.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "78eb7f4d76bec02eaa45c9b981a9434d831120a91f4ad0a06e59ab7f7fa9160b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78eb7f4d76bec02eaa45c9b981a9434d831120a91f4ad0a06e59ab7f7fa9160b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78eb7f4d76bec02eaa45c9b981a9434d831120a91f4ad0a06e59ab7f7fa9160b"
-    sha256 cellar: :any_skip_relocation, sonoma:        "bdc6f2725a1f861964e58d868ce6b80b21d1829e972d2cb32938f80b0696d19e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "061ab4fe7c7861a7bdaf99eeb5e8826fc77282ab678c3f18bd1be4839cbec9cd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a8d706a0b4cad5e8b996020ef02bf820b461e65190dca28be2d7388d6dd291e0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f99047c72a81091195b41208910723be10264b03c52fcf3455cf1ced0da1fe69"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f99047c72a81091195b41208910723be10264b03c52fcf3455cf1ced0da1fe69"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f99047c72a81091195b41208910723be10264b03c52fcf3455cf1ced0da1fe69"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0c7b3aaba09b7e538715ffb8c127aa77079d7cb93f2022e92ce5696b59913568"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a64e9518ca11440936e83da7ec6f7f28b4ed17a257e89f90b6deef06aa681129"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2bea91b8ac8a673a197788066ab34009f85e4ff9917805f5d7bbb4190a322493"
   end
 
   depends_on "go" => :build
