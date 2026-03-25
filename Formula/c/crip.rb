4e7dea2516d3cfdfd46ffe34389292f61209f877
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 02:43:41 +0000

    crip: add 2.7.1 bottle.

--- Formula/c/crip.rb
+++ Formula/c/crip.rb
@@ -5,6 +5,15 @@ class Crip < Formula
   sha256 "d73bc25c3ab37467764310e8573895953b4ff80cf045d96c90aa3c24a67a4f05"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ea0964d4b1ddc23e45553ef3c79cf64abe268d68a843889e6aa14d769d000667"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea0964d4b1ddc23e45553ef3c79cf64abe268d68a843889e6aa14d769d000667"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea0964d4b1ddc23e45553ef3c79cf64abe268d68a843889e6aa14d769d000667"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ea0964d4b1ddc23e45553ef3c79cf64abe268d68a843889e6aa14d769d000667"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f168b5098ac9ee43fba009ed7473dc040b97b20ff8339c704dbace009f10f2ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f168b5098ac9ee43fba009ed7473dc040b97b20ff8339c704dbace009f10f2ca"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk"
 
