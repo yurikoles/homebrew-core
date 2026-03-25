9fed2d5dca20446ca0167ab914ffecae532e704e
Michael Cho <michael@michaelcho.dev>
2026-03-25 18:47:30 -0400

    linux-headers@5.15: make keg-only

--- Formula/l/linux-headers@5.15.rb
+++ Formula/l/linux-headers@5.15.rb
@@ -4,6 +4,7 @@ class LinuxHeadersAT515 < Formula
   url "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.15.202.tar.gz"
   sha256 "7459799f7138c67817f587225d453647b2219f5371d0b610823a5fcecbc496d8"
   license "GPL-2.0-only"
+  revision 1
   compatibility_version 1
 
   livecheck do
@@ -15,6 +16,8 @@ class LinuxHeadersAT515 < Formula
     sha256 cellar: :any_skip_relocation, arm64_linux:  "12cc44a56a7a8bb79473df38494801b0989c7250d3e65709b06268c66ab9bb8e"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "1b5ab4028d4aead8e55c71fbcd62811ea37368a5e2dbe26ec8cfe472d2e8ad9b"
   end
+
+  keg_only :versioned_formula
 
   depends_on :linux
 
