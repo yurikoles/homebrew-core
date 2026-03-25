fac4fa60c4b3c697135aec346aa21abb60912aad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 04:16:14 +0000

    storm: update 2.8.5 bottle.

--- Formula/s/storm.rb
+++ Formula/s/storm.rb
@@ -9,7 +9,7 @@ class Storm < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "05480b09397be822d6b250bd1768b8704976e49e5547d5eee50b0c2e2c58461c"
+    sha256 cellar: :any_skip_relocation, all: "e0f7cf833ea579e584e7faf1f7bc7b4f50e30c570481e444e9c67f0566915cae"
   end
 
   depends_on "openjdk"
