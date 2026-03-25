03cf88b03e0b060909e00bc3458b4e351644839a
Michael Cho <michael@michaelcho.dev>
2026-03-25 18:51:07 -0400

    linux-headers@6.8: remove keg-only

--- Formula/l/linux-headers@6.8.rb
+++ Formula/l/linux-headers@6.8.rb
@@ -4,6 +4,7 @@ class LinuxHeadersAT68 < Formula
   url "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.8.12.tar.gz"
   sha256 "1f6134265066aa2bef94103c1e4485d6ed8c2e08ba2a24202ea0c757c6c2c0de"
   license "GPL-2.0-only"
+  revision 1
 
   livecheck do
     skip "Final kernel.org release for 6.8. Consider adding patches from Ubuntu 24.04."
@@ -13,8 +14,6 @@ class LinuxHeadersAT68 < Formula
     sha256 cellar: :any_skip_relocation, arm64_linux:  "64614bcba5f4d9b9535bdd54b68a588aa2c9f56f78fa048b60ddb6e98a21aea5"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "3f3000358f22ee25e733aa9dcf0881b5ec4f3c2805372d4fbe988039deaa1fef"
   end
-
-  keg_only :versioned_formula
 
   depends_on :linux
 
