7c9c1a7ad1ffdc29c967f848062af7e03393c9fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 08:57:28 +0000

    virtctl 1.8.0

--- Formula/v/virtctl.rb
+++ Formula/v/virtctl.rb
@@ -1,8 +1,8 @@
 class Virtctl < Formula
   desc "Allows for using more advanced kubevirt features"
   homepage "https://kubevirt.io/"
-  url "https://github.com/kubevirt/kubevirt/archive/refs/tags/v1.7.2.tar.gz"
-  sha256 "5864123d569c749db0b2bad9a4b014e30bcffac89076c4e99165b388929eb1ce"
+  url "https://github.com/kubevirt/kubevirt/archive/refs/tags/v1.8.0.tar.gz"
+  sha256 "250de85d0d3ff0c0357c19e21540798a49e986e49e4f4123175a665d026d9c8b"
   license "Apache-2.0"
   head "https://github.com/kubevirt/kubevirt.git", branch: "main"
 
