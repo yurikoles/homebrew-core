d13e49bb8507fcda7717e883f5df183819924073
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 05:24:20 +0000

    csprecon 0.4.4

--- Formula/c/csprecon.rb
+++ Formula/c/csprecon.rb
@@ -1,8 +1,8 @@
 class Csprecon < Formula
   desc "Discover new target domains using Content Security Policy"
   homepage "https://github.com/edoardottt/csprecon"
-  url "https://github.com/edoardottt/csprecon/archive/refs/tags/v0.4.3.tar.gz"
-  sha256 "d86f960f21d029a5ac6e6b4087f1b1dcc7ad7632007d8646b8185d3e39fbc7d6"
+  url "https://github.com/edoardottt/csprecon/archive/refs/tags/v0.4.4.tar.gz"
+  sha256 "86fb95ccf78a032c4ec47b5c70d859936824e69a05b18170a5b5db67c88a93da"
   license "MIT"
   head "https://github.com/edoardottt/csprecon.git", branch: "main"
 
