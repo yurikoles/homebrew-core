9b348e4e66ccdcb3c338869d2cc936c8a146f3d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 12:32:52 +0000

    mill: update 1.1.5 bottle.

--- Formula/m/mill.rb
+++ Formula/m/mill.rb
@@ -11,7 +11,7 @@ class Mill < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "93beeecf615003e03c3537e1c574fcf58d1c6ea480543ce3c7a3b0e889f98446"
+    sha256 cellar: :any_skip_relocation, all: "49aa9546a86a45e67e4d5e3b77723f8cd9c604b2ccbd43abba3fe8ad9fbb0a7c"
   end
 
   depends_on "openjdk"
