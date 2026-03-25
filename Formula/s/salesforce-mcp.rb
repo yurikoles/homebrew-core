754149737eeef78e558433ed2c0f4de7a421562c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 16:40:51 +0000

    salesforce-mcp: update 0.29.2 bottle.

--- Formula/s/salesforce-mcp.rb
+++ Formula/s/salesforce-mcp.rb
@@ -6,7 +6,7 @@ class SalesforceMcp < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "3f5eba20399d07bb57db8aa0a1b437163b32dc88acda96576724d08778e86199"
+    sha256 cellar: :any_skip_relocation, all: "0b4d57094136a8e706e418341c8413601401b0e6a256fdf7e1a31a1e8baa8ce4"
   end
 
   depends_on "node"
