1267c318c0dc071a2d2e1f7341db79d98afe8f30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 05:38:53 +0000

    libclc 22.1.2

--- Formula/lib/libclc.rb
+++ Formula/lib/libclc.rb
@@ -1,8 +1,8 @@
 class Libclc < Formula
   desc "Implementation of the library requirements of the OpenCL C programming language"
   homepage "https://libclc.llvm.org/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.1/llvm-project-22.1.1.src.tar.xz"
-  sha256 "9c6f37f6f5f68d38f435d25f770fc48c62d92b2412205767a16dac2c942f0c95"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.2/llvm-project-22.1.2.src.tar.xz"
+  sha256 "62f2f13ff25b1bb28ea507888e858212d19aafb65e8e72b4a65ee0629ec4ae0c"
   license "Apache-2.0" => { with: "LLVM-exception" }
   compatibility_version 1
 
