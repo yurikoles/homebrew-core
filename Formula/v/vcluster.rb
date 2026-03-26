4c2c658b03056aef63d9c58025569b9da481e62f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-26 03:24:54 +0000

    vcluster 0.33.1

--- Formula/v/vcluster.rb
+++ Formula/v/vcluster.rb
@@ -2,8 +2,8 @@ class Vcluster < Formula
   desc "Creates fully functional virtual k8s cluster inside host k8s cluster's namespace"
   homepage "https://www.vcluster.com"
   url "https://github.com/loft-sh/vcluster.git",
-      tag:      "v0.33.0",
-      revision: "69cb3ac90e5d8f25ed11ddb529396c9c7682c901"
+      tag:      "v0.33.1",
+      revision: "ac04311a85bce383aacdda0721408e98f8ef453c"
   license "Apache-2.0"
   head "https://github.com/loft-sh/vcluster.git", branch: "main"
 
