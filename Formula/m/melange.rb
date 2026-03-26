78ef6438e9470706d84a9580bab8d88c87a5695c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-26 05:51:23 +0000

    melange 0.46.1

--- Formula/m/melange.rb
+++ Formula/m/melange.rb
@@ -1,8 +1,8 @@
 class Melange < Formula
   desc "Build APKs from source code"
   homepage "https://github.com/chainguard-dev/melange"
-  url "https://github.com/chainguard-dev/melange/archive/refs/tags/v0.46.0.tar.gz"
-  sha256 "123f230dc5a0af77df162923ef9933cef03ff290f6cbe7f9ed9ad8ef5e3a4e4e"
+  url "https://github.com/chainguard-dev/melange/archive/refs/tags/v0.46.1.tar.gz"
+  sha256 "996aa605c0b1a177f6555cfd14835d96262c14dc42ea24470958a0b5458ce170"
   license "Apache-2.0"
   head "https://github.com/chainguard-dev/melange.git", branch: "main"
 
