1c523da70f6c0daaedb572fe51356e999d5b4f17
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-26 19:14:39 +0000

    antidote: update 2.0.4 bottle.

--- Formula/a/antidote.rb
+++ Formula/a/antidote.rb
@@ -6,7 +6,7 @@ class Antidote < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "af26b498d6b82ff8c1570c81d4bfe619a0e0e4d99e5f6fd1b9ffe5353c42f72c"
+    sha256 cellar: :any_skip_relocation, all: "1e4bed2990ee36e4066c6b1d68323509fe4b6f98f467e4503c4353db27d10f00"
   end
 
   uses_from_macos "zsh"
