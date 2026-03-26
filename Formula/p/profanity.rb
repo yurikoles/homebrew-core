e15f1509dd166ac79ef2515b00b4fefe8673adf1
Leon Adomaitis <leon@adomaitis.de>
2026-03-27 00:27:43 +0100

    profanity 0.17.0

--- Formula/p/profanity.rb
+++ Formula/p/profanity.rb
@@ -1,19 +1,10 @@
 class Profanity < Formula
   desc "Console based XMPP client"
   homepage "https://profanity-im.github.io"
+  url "https://profanity-im.github.io/tarballs/profanity-0.17.0.tar.xz"
+  sha256 "508e18c0e797d46cc38779eb207480fc3e93b814e202a351050f395c1b262804"
   license "GPL-3.0-or-later"
-
-  stable do
-    url "https://profanity-im.github.io/tarballs/profanity-0.16.0.tar.gz"
-    sha256 "1f2e36a081cd299173f1c12b64b1cef971063e67bf226fb3f7048f2e67bd6a70"
-
-    # Define `prefs_changes_print` outside of `prefs_changes`
-    # https://github.com/profanity-im/profanity/pull/2090
-    patch do
-      url "https://github.com/profanity-im/profanity/commit/309c0a64a7636770f6aabe7c55c00a0d0a77031c.patch?full_index=1"
-      sha256 "2f85e7a8e3c503fecc63959e978bdb99cafbceb1b2687952d52088a352de3aed"
-    end
-  end
+  head "https://github.com/profanity-im/profanity.git", branch: "master"
 
   bottle do
     sha256 arm64_tahoe:   "669b57ae421eea14e623d6306d16f62f8a3e7e5ac52fc69b6283d4a15f79ca9d"
@@ -24,22 +15,21 @@ class Profanity < Formula
     sha256 x86_64_linux:  "b041c981823cfc9d4bb96e6d78b49a040834b1cbbb6bdf5e759ec9b5b4824ee6"
   end
 
-  head do
-    url "https://github.com/profanity-im/profanity.git", branch: "master"
-
-    depends_on "autoconf" => :build
-    depends_on "autoconf-archive" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkgconf" => :build
-
+  depends_on "gdk-pixbuf"
   depends_on "glib"
   depends_on "gpgme"
+  depends_on "gtk+3"
+  depends_on "libgcrypt"
+  depends_on "libomemo-c"
   depends_on "libotr"
   depends_on "libstrophe"
+  depends_on "libx11"
+  depends_on "libxscrnsaver"
   depends_on "python@3.14"
+  depends_on "qrencode"
   depends_on "readline"
   depends_on "sqlite"
 
@@ -47,25 +37,41 @@ class Profanity < Formula
   uses_from_macos "ncurses"
 
   on_macos do
-    depends_on "gettext"
-    depends_on "libassuan"
-    depends_on "libgpg-error"
     depends_on "terminal-notifier"
   end
 
+  on_linux do
+    depends_on "libnotify"
+  end
+
+  # Fix missing imports for libomemo-c support: https://github.com/profanity-im/profanity/pull/2133
+  patch do
+    url "https://github.com/profanity-im/profanity/commit/9a501e6ecdaf65d28362e5888a0529fb734a353e.patch?full_index=1"
+    sha256 "ac0f514496890bbcbed9cee3f6a84387c64f3a299d9b2f700e07ae57bb887447"
+  end
+
   def install
-    ENV.prepend_path "PATH", Formula["python@3.14"].opt_libexec/"bin"
+    # Meson shells out to `brew --prefix readline` on macOS if `dependency("readline")`
+    # cannot resolve directly, so keep Homebrew's `brew` executable discoverable.
+    ENV.prepend_path "PATH", File.dirname(HOMEBREW_BREW_FILE)
 
-    system "./bootstrap.sh" if build.head?
+    args = %w[
+      -Dnotifications=enabled
+      -Dpython-plugins=enabled
+      -Dc-plugins=enabled
+      -Dotr=enabled
+      -Dpgp=enabled
+      -Domemo=enabled
+      -Domemo-backend=libomemo-c
+      -Domemo-qrcode=enabled
+      -Dicons-and-clipboard=enabled
+      -Dgdk-pixbuf=enabled
+      -Dxscreensaver=enabled
+    ]
 
-    # We need to pass `BREW` to `configure` to make sure it can be found inside the sandbox in non-default
-    # prefixes. `configure` knows to check `/opt/homebrew` and `/usr/local`, but the sanitised build
-    # environment will prevent any other `brew` installations from being found.
-    system "./configure", "--disable-silent-rules",
-                          "--enable-python-plugins",
-                          "BREW=#{HOMEBREW_BREW_FILE}",
-                          *std_configure_args
-    system "make", "install"
+    system "meson", "setup", "build", *std_meson_args, *args
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
   end
 
   test do
