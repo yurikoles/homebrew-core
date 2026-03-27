ba89e33ef2eb80c3f0768e64a420bb09079d139f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-27 18:06:31 +0000

    static-web-server 2.42.0

--- Formula/s/static-web-server.rb
+++ Formula/s/static-web-server.rb
@@ -1,8 +1,8 @@
 class StaticWebServer < Formula
   desc "High-performance and asynchronous web server for static files-serving"
   homepage "https://static-web-server.net"
-  url "https://github.com/static-web-server/static-web-server/archive/refs/tags/v2.41.0.tar.gz"
-  sha256 "d3b3f7144180b74db2a39f8a21df546e1321336b0cb85d97f6769b6235668d2b"
+  url "https://github.com/static-web-server/static-web-server/archive/refs/tags/v2.42.0.tar.gz"
+  sha256 "7ef8ad8f22c4655979771d0e269aaf8232617b815fd5528342ecfc7061ecacb8"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/static-web-server/static-web-server.git", branch: "master"
 
