092da03696f57e45e87c7eb5c0c2c31a5bc573da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-27 19:59:10 +0000

    awscli 2.34.19
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/awscli.rb
+++ Formula/a/awscli.rb
@@ -3,8 +3,8 @@ class Awscli < Formula
 
   desc "Official Amazon AWS command-line interface"
   homepage "https://aws.amazon.com/cli/"
-  url "https://github.com/aws/aws-cli/archive/refs/tags/2.34.18.tar.gz"
-  sha256 "864c6681e28181445318e40adaf119beb4cd0c9926180f629809d51521ab6bbe"
+  url "https://github.com/aws/aws-cli/archive/refs/tags/2.34.19.tar.gz"
+  sha256 "0fb0a1fddf6ed9f38c6574dbdae8aa0d9368a5dffd769fb32b175d43bf95a299"
   license "Apache-2.0"
   compatibility_version 1
   head "https://github.com/aws/aws-cli.git", branch: "v2"
