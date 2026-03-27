2b98e6f5045310d92f3b7ddf1ee00d444955dc36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-27 03:20:36 +0000

    oh-my-agent 4.12.0

--- Formula/o/oh-my-agent.rb
+++ Formula/o/oh-my-agent.rb
@@ -1,8 +1,8 @@
 class OhMyAgent < Formula
   desc "Portable multi-agent harness for .agents-based skills and workflows"
   homepage "https://github.com/first-fluke/oh-my-agent"
-  url "https://registry.npmjs.org/oh-my-agent/-/oh-my-agent-4.11.0.tgz"
-  sha256 "e87b9357409ab08f08ec478d959b50f9525f2cb187281bc7e319f8dc7aeb77c7"
+  url "https://registry.npmjs.org/oh-my-agent/-/oh-my-agent-4.12.0.tgz"
+  sha256 "405fa90eb6c25bb8436fe0ee4f56ea56d3ff1fe7a9d7833200749bf4acefaaeb"
   license "MIT"
 
   bottle do
