bcfa2298b585d7fa8db573ae73499c63efee00f4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-28 16:55:28 +0000

    pay: add 0.1.0 bottle.

--- Formula/p/pay.rb
+++ Formula/p/pay.rb
@@ -11,6 +11,15 @@ class Pay < Formula
     regex(/^pay[._-]v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "33d4228674d3300180b912bb6926bdb597cfb8a4bad5163ab11f2dc9b7c67c9b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f619a56e0b1b306aa325efdd870963926f1ca3587a9e71bbb22dda86b7ddaa36"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "06e7179dc4f7d90fc44663f2a4976d59a82af42d1ea6f9c1680ea85846c61d53"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c5215f16afbdeabac41a3415bc41026920b263e0f7db24735573c2119bed962a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8bfac7996e30d19e40b39eb7b2c222161956d5b9633b99f351cc316f76927756"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "368336f2edaaa44339cf4517e62450ee81189e0be7ab33e8668c989e51f00f3b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
