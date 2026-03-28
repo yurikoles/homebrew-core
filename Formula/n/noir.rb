d07a495a58460d1f1c73d7db77af9f755c6be96f
hahwul <hahwul@gmail.com>
2026-03-28 23:08:52 +0900

    noir 0.29.0

--- Formula/n/noir.rb
+++ Formula/n/noir.rb
@@ -1,8 +1,8 @@
 class Noir < Formula
   desc "Attack surface detector that identifies endpoints by static analysis"
   homepage "https://owasp.org/www-project-noir/"
-  url "https://github.com/owasp-noir/noir/archive/refs/tags/v0.28.0.tar.gz"
-  sha256 "2480e0d61b4d8670f2a7665f93038f37277e15ab19b4f307418f047929c0349f"
+  url "https://github.com/owasp-noir/noir/archive/refs/tags/v0.29.0.tar.gz"
+  sha256 "fd82153f00997035d3ebc5f9e6429383c1a93b045a10ea87c9eebb0041cb9967"
   license "MIT"
   head "https://github.com/owasp-noir/noir.git", branch: "main"
 
