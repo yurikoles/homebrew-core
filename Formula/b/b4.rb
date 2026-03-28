7302e85f25924a4e06ad34d0898f13f690c1d39e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-28 07:44:47 +0000

    b4: update 0.15.1_1 bottle.

--- Formula/b/b4.rb
+++ Formula/b/b4.rb
@@ -9,12 +9,12 @@ class B4 < Formula
   revision 1
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "5369b5e235fac64693b34d3229b41fa91c2697ddb64250710e4896a3e3a68d4f"
-    sha256 cellar: :any,                 arm64_sequoia: "c84425350c5a17c3331db83b7128b8b48db76a2c9af73af8627ee55de3455f8e"
-    sha256 cellar: :any,                 arm64_sonoma:  "35dfe668e1a3fd2809b2ae92b679c76b0591bbae6b03d8c38b28172f433273e3"
-    sha256 cellar: :any,                 sonoma:        "6062866fcc745998c6f8386ef7d9b7e99567534b057a5415dd36bcc5b8275f48"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ce8cbd340cb27b71721fa807680bdd32b0ddc7e1efccedfd334be4a71b8145c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "54302bb0e25435c286592b7c2a0d412ded660baaa0cab280d7561738b987d8de"
+    sha256 cellar: :any,                 arm64_tahoe:   "04df938b9bdb395cc624cd729897721f1532ba3e064f6c13bb558cb82ef7008b"
+    sha256 cellar: :any,                 arm64_sequoia: "e965a5c051c4a24fa07a0cb1d635f5ba1a7bfda7d8bff705fcb39d754c9e7f1c"
+    sha256 cellar: :any,                 arm64_sonoma:  "e7a222c8a0967c041d7eff4965a2e695b9ffb810380c5eace756d93095fafbf9"
+    sha256 cellar: :any,                 sonoma:        "52721b4552fb4ca2222ebf4d0ec7c747a5b99f4ca5bec9b52aa4c46e51e7e0e1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fb730ad2178b7e153fcf7a0af708139fa423771f21945dda904c2dacc469ea0d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3dea04ec2535e960bf25f0f8581e878241b76be66fab0a11e559727599eb63d0"
   end
 
   depends_on "certifi" => :no_linkage
