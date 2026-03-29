5f7ca37e8394d130870f8473436c128843294b3b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-29 05:14:59 +0000

    nextpnr-ice40: add 0.10 bottle.

--- Formula/n/nextpnr-ice40.rb
+++ Formula/n/nextpnr-ice40.rb
@@ -6,6 +6,15 @@ class NextpnrIce40 < Formula
   license "ISC"
   head "https://github.com/YosysHQ/nextpnr.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "f8d35ebf37436515566c1d2dd1bb0a87e9fb11815d1729ed3d0b7a7fb46d56d8"
+    sha256 cellar: :any,                 arm64_sequoia: "7d29a823778a62e5316b2a60301e256f9c351ba19c3f2e52b0b3b8932c37ded3"
+    sha256 cellar: :any,                 arm64_sonoma:  "db90d1891b3bb2a427704793460190973b4a4f9ee9a93f0c5c702ec1026e2e96"
+    sha256 cellar: :any,                 sonoma:        "4842370686c14ab5cfd921120c8f1bd8f9773103420d58acc65afce1c89785a5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e274bb3f988d996725a644c6461e3a6d0d4e90328cdafdda635141e0774f542f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2cefe1fa9d3b1b178e88c74eb073512487d5f0874c72c13b7ea0452dce528dd7"
+  end
+
   depends_on "cmake" => :build
   depends_on "yosys" => :test
   depends_on "boost"
