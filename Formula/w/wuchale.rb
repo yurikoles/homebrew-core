42e23215d5b6e40a97c8a83e3822715a6fe8380f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-29 11:36:49 +0000

    wuchale 0.22.6

--- Formula/w/wuchale.rb
+++ Formula/w/wuchale.rb
@@ -1,8 +1,8 @@
 class Wuchale < Formula
   desc "Protobuf-like i18n from plain code"
   homepage "https://wuchale.dev/"
-  url "https://registry.npmjs.org/wuchale/-/wuchale-0.22.5.tgz"
-  sha256 "0284b22d86994b7456ce28048286aa6426788088e1253cfee18d5b4b53220a25"
+  url "https://registry.npmjs.org/wuchale/-/wuchale-0.22.6.tgz"
+  sha256 "67a7360b5f02d9449cc5e30b617fe719575b700b5e5f9b088af3146963487fbc"
   license "MIT"
 
   bottle do
