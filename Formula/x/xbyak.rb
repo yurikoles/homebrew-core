49628c550a738545d72696262fcd17ae64a995a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-30 06:40:19 +0000

    xbyak 7.35.4

--- Formula/x/xbyak.rb
+++ Formula/x/xbyak.rb
@@ -1,8 +1,8 @@
 class Xbyak < Formula
   desc "C++ JIT assembler for x86 (IA32), x64 (AMD64, x86-64)"
   homepage "https://github.com/herumi/xbyak"
-  url "https://github.com/herumi/xbyak/archive/refs/tags/v7.35.3.tar.gz"
-  sha256 "84c2e5419062928372cdbe2646a1c7c474606c8d4dabad971ec06e73cd45ea5c"
+  url "https://github.com/herumi/xbyak/archive/refs/tags/v7.35.4.tar.gz"
+  sha256 "a5005b5e9545ca8fb8fdbf83f67a3267776fc2ef8f04ecafa6b2961b528d4116"
   license "BSD-3-Clause"
   head "https://github.com/herumi/xbyak.git", branch: "master"
 
