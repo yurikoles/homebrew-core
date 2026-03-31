791a638726ba344dcec7ae3c318e1299b254b8c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 19:15:04 +0000

    mcat: update 0.5.0 bottle.

--- Formula/m/mcat.rb
+++ Formula/m/mcat.rb
@@ -7,12 +7,12 @@ class Mcat < Formula
   head "https://github.com/Skardyy/mcat.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4b540665d46e453d425a86d38af54b497cbc4a6673ad8c9f2a5d0aaf602706b1"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "535975d10cb2fc90fecf6ef2baf886cb1f1a963370a47eecb5830cc23051b5ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4c1cca18b6aa665c83ca9a21968e6c06d935359d2588ce7e30db1ec11d0ada8"
-    sha256 cellar: :any_skip_relocation, sonoma:        "3e74346a135925395ed249a72bfd08f9ce8339d7b0becb916cc14729039ffdbf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0fd9fefc639638499ac67f90a0e20c6749587b434825889970f73c1600857c36"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7f8a99a903865bb847e95826a7e3328e2f2f31833bc59a309b28d84a21f5346"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "269d3cfa051465882b529d28ad7f96fafcc5d1b64edcb999581ad7c089edd6b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68cce2c63dc9a3d524840cf2d55002e3aab20acf24437932e44ff2859d87671a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1604408da724d64660ecdf4e2feacb3c22a28f85318106b3f27954045ebd208d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "26fc280f47d54338acc7e9c8c7ee6ca5cd84769b0366fddba1f446efa8809827"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eb3c35ea8a17564c543806f812c5f1aa97f21be99bccd37cd0699b810bfb29f3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bfb5e71e4050bf4f92e568aafd4b9ee39142022050c8ff787a2547027e8703b3"
   end
 
   depends_on "rust" => :build
