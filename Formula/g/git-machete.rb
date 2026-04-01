5d3f64390f5117fb364ec28186d83e624ba414d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 14:38:41 +0000

    git-machete 3.40.0

--- Formula/g/git-machete.rb
+++ Formula/g/git-machete.rb
@@ -3,8 +3,8 @@ class GitMachete < Formula
 
   desc "Git repository organizer & rebase workflow automation tool"
   homepage "https://github.com/VirtusLab/git-machete"
-  url "https://files.pythonhosted.org/packages/2d/53/471966b786f77973e4750ad696ccc3811cef8873bd433a66dcc01a93cd11/git_machete-3.39.2.tar.gz"
-  sha256 "8fe093b86122926ecc0a9a4828e07a71526cd72161211fcca2d71f8673692e2b"
+  url "https://files.pythonhosted.org/packages/c6/a9/23f430b81b571b63a61d0df85b33ce85aac9873cf094993fbaed16d807ed/git_machete-3.40.0.tar.gz"
+  sha256 "90e7edcb4fe3e3e536475749a9d557bc43bb8f8be55a89f9c01f0a0eab46ab78"
   license "MIT"
 
   bottle do
