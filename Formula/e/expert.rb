61f56a007687cec7755172eee76567cc61d88b5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 17:06:19 +0000

    expert: add 0.1.0 bottle.

--- Formula/e/expert.rb
+++ Formula/e/expert.rb
@@ -5,6 +5,15 @@ class Expert < Formula
   sha256 "209eebddf264407f43848f2d9584210e1869b0103885c1cfab89ce48a2421c36"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f9f4b95e9ca15c8c2b53c7ee9c346d53c24df1509d1be124868c350b9042c45"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ef5f201bbb3d9d686c40522a172bd791dceeca164e4c61902b7f56b32ca3d36"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8e4d0bdb1935d6edf3ee3ced12625277bef20a197582681b2a4c582899cd9b7"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ae7f9ce56676e96018dc3c0bba839567b514432078891ad8712efc4b1faf316f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c80d566b7bf95b48166a9a1737414f6a9d27a2cb067614344e753338a138de1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9158f8ecf797e2b959ef28491256effb3d447ee23b852a8d57075ac6be194e0b"
+  end
+
   depends_on "elixir" => :build
   depends_on "erlang" => :build
   depends_on "just" => :build
