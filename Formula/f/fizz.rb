ffbf21be72b3bfbe8237d77fb93427c4713eb9cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 12:05:42 +0000

    fizz: update 2026.03.30.00 bottle.

--- Formula/f/fizz.rb
+++ Formula/f/fizz.rb
@@ -8,12 +8,12 @@ class Fizz < Formula
   head "https://github.com/facebookincubator/fizz.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "2892d1556ce5cf6f76be9ed10314d406186264819502d70983fb9b3e9c351b4c"
-    sha256 cellar: :any,                 arm64_sequoia: "a53182e002fa156ceddc2591719ddb2946fa116823ed15b946b2a5597ca7bf3e"
-    sha256 cellar: :any,                 arm64_sonoma:  "cfcc5a37f76758878893f4a2cbefdf5a94e2c6981129179a59bcbdd945a832d6"
-    sha256 cellar: :any,                 sonoma:        "028ac7d4cf0c0225de9b9efe0af7478d5cf453e9b82244e0c2823b865a0ed25f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a54acc5e3c609e804cad1c90f9e12618925ff98cb934fcf7a35fba54c84fc6fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86c0aabdbbe38728192b2067858ea81830141e622d7af2b5d7e89a83b0b997e7"
+    sha256 cellar: :any,                 arm64_tahoe:   "997c8d0117b8540857ffeabcb410cd358d1dd0ac33d6c8680c9637215348c02e"
+    sha256 cellar: :any,                 arm64_sequoia: "1394e9c9a6c4094a671438b9dfb53777c7965aeb6f63e2e8e09f2d07b67d0cfe"
+    sha256 cellar: :any,                 arm64_sonoma:  "0d998567e4883298aed0e17251345c1180b3a70df32f2da38a90f77ebf1de91f"
+    sha256 cellar: :any,                 sonoma:        "157aef22dee65b44b5dbcc6db3f7f809a1d32f1efdb6929c3095f556b73155e3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "51637620c794ebd11d43bc0cffef1430a32ea19ab55c2dc2175de5a4bcd2ad74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "802b03d2a901f972a79e13ad2643b91acc6546a04e668aa381937937a57ba558"
   end
 
   depends_on "cmake" => [:build, :test]
