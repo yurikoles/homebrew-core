ff20725a00c092d4057e4e29fb185ca9b9d7cdb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 20:59:21 +0000

    tock 1.8.1

--- Formula/t/tock.rb
+++ Formula/t/tock.rb
@@ -1,8 +1,8 @@
 class Tock < Formula
   desc "Powerful time tracking tool for the command-line"
   homepage "https://github.com/kriuchkov/tock"
-  url "https://github.com/kriuchkov/tock/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "cdd36271a849fc91d01f26a687255a1234056fbf3769facaf8ae36eacc4c1a4c"
+  url "https://github.com/kriuchkov/tock/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "9a3747d4cce00ed2755718a38255b5c0f33d5c09b5eac852c35ae437a85ad0d9"
   license "GPL-3.0-or-later"
   head "https://github.com/kriuchkov/tock.git", branch: "master"
 
