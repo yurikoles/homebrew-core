52233bfb7388ca4d882c5813121b864460e6a781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 05:46:10 +0000

    libmicrohttpd 1.0.3

--- Formula/lib/libmicrohttpd.rb
+++ Formula/lib/libmicrohttpd.rb
@@ -1,9 +1,9 @@
 class Libmicrohttpd < Formula
   desc "Light HTTP/1.1 server library"
   homepage "https://www.gnu.org/software/libmicrohttpd/"
-  url "https://ftpmirror.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.2.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.2.tar.gz"
-  sha256 "df324fcd0834175dab07483133902d9774a605bfa298025f69883288fd20a8c7"
+  url "https://ftpmirror.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.3.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.3.tar.gz"
+  sha256 "7816b57aae199cf5c3645e8770e1be5f0a4dfafbcb24b3772173dc4ee634126a"
   license "LGPL-2.1-or-later"
   compatibility_version 1
 
