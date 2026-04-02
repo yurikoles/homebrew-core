6ea9903837e676f427f71f97038887737b7218ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 11:53:17 +0000

    rtk 0.34.3

--- Formula/r/rtk.rb
+++ Formula/r/rtk.rb
@@ -1,8 +1,8 @@
 class Rtk < Formula
   desc "CLI proxy to minimize LLM token consumption"
   homepage "https://www.rtk-ai.app/"
-  url "https://github.com/rtk-ai/rtk/archive/refs/tags/v0.34.2.tar.gz"
-  sha256 "b314ad0f7498aba843706cfaf2b474cbd7697ed06854813fe6151ca41b17c946"
+  url "https://github.com/rtk-ai/rtk/archive/refs/tags/v0.34.3.tar.gz"
+  sha256 "c06bd083ad956a96ab16669518e1631880c86f24cce524dbefe51450f9f5557e"
   license "Apache-2.0"
   head "https://github.com/rtk-ai/rtk.git", branch: "develop"
 
