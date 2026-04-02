e5cc7f95f816e09edeca8b34c66ed91c2a1632c4
Michael Cho <michael@michaelcho.dev>
2026-04-02 00:44:14 -0400

    arcade-learning-environment: autobump resources

--- Formula/a/arcade-learning-environment.rb
+++ Formula/a/arcade-learning-environment.rb
@@ -21,7 +21,6 @@ class ArcadeLearningEnvironment < Formula
 
   depends_on "cmake" => :build
   depends_on "ninja" => :build
-  depends_on "pybind11" => :build
   depends_on "numpy"
   depends_on "opencv"
   depends_on "python@3.14"
@@ -31,18 +30,25 @@ class ArcadeLearningEnvironment < Formula
     depends_on "zlib-ng-compat"
   end
 
-  pypi_packages exclude_packages: "numpy",
-                extra_packages:   "gymnasium"
+  pypi_packages package_name:     "ale-py[vector]",
+                exclude_packages: %w[numpy opencv-python],
+                extra_packages:   "pybind11==3.0.1" # TODO: remove in next release
 
   # See https://github.com/Farama-Foundation/Arcade-Learning-Environment/blob/master/scripts/download_unpack_roms.sh
   resource "roms" do
     url "https://gist.githubusercontent.com/jjshoots/61b22aefce4456920ba99f2c36906eda/raw/00046ac3403768bfe45857610a3d333b8e35e026/Roms.tar.gz.b64"
+    version "00046ac3403768bfe45857610a3d333b8e35e026"
     sha256 "02ca777c16476a72fa36680a2ba78f24c3ac31b2155033549a5f37a0653117de"
+
+    livecheck do
+      url "https://raw.githubusercontent.com/Farama-Foundation/Arcade-Learning-Environment/refs/tags/v#{LATEST_VERSION}/scripts/download_unpack_roms.sh"
+      regex(%r{/jjshoots/61b22aefce4456920ba99f2c36906eda/raw/(\h+)/Roms\.t}i)
+    end
   end
 
   resource "cloudpickle" do
-    url "https://files.pythonhosted.org/packages/52/39/069100b84d7418bc358d81669d5748efb14b9cceacd2f9c75f550424132f/cloudpickle-3.1.1.tar.gz"
-    sha256 "b216fa8ae4019d5482a8ac3c95d8f6346115d8835911fd4aefd1a445e4242c64"
+    url "https://files.pythonhosted.org/packages/27/fb/576f067976d320f5f0114a8d9fa1215425441bb35627b1993e5afd8111e5/cloudpickle-3.1.2.tar.gz"
+    sha256 "7fda9eb655c9c230dab534f1983763de5835249750e85fbcef43aaa30a9a2414"
   end
 
   resource "farama-notifications" do
@@ -51,8 +57,13 @@ class ArcadeLearningEnvironment < Formula
   end
 
   resource "gymnasium" do
-    url "https://files.pythonhosted.org/packages/b3/de/b923d09654df8f8ee29a3cc7ec7829ac057efd0d969cc3da0c8a7b219d59/gymnasium-1.2.1.tar.gz"
-    sha256 "4e6480273528523a90b3db99befb6111b13f15fa0866de88c4b675770495b66c"
+    url "https://files.pythonhosted.org/packages/76/59/653a9417d98ed3e29ef9734ba52c3495f6c6823b8d5c0c75369f25111708/gymnasium-1.2.3.tar.gz"
+    sha256 "2b2cb5b5fbbbdf3afb9f38ca952cc48aa6aa3e26561400d940747fda3ad42509"
+  end
+
+  resource "pybind11" do
+    url "https://files.pythonhosted.org/packages/2f/7b/a6d8dcb83c457e24a9df1e4d8fd5fb8034d4bbc62f3c324681e8a9ba57c2/pybind11-3.0.1.tar.gz"
+    sha256 "9c0f40056a016da59bab516efb523089139fcc6f2ba7e4930854c61efb932051"
   end
 
   resource "typing-extensions" do
@@ -65,6 +76,10 @@ class ArcadeLearningEnvironment < Formula
   end
 
   def install
+    venv = virtualenv_create(libexec, python3)
+    venv.pip_install resources.select { |r| r.url.start_with?("https://files.pythonhosted.org/") }
+    ENV.prepend_path "CMAKE_PREFIX_PATH", venv.site_packages/"pybind11"
+
     system "cmake", "-S", ".", "-B", "build",
                     "-DSDL_SUPPORT=ON",
                     "-DSDL_DYNLOAD=ON",
@@ -93,8 +108,6 @@ class ArcadeLearningEnvironment < Formula
     end
     # We build without XLA and jax has no sdists
     inreplace "pyproject.toml", '"jax >= 0.4.31', "#"
-    venv = virtualenv_create(libexec, python3)
-    venv.pip_install resources.select { |r| r.url.start_with?("https://files.pythonhosted.org/") }
     venv.pip_install_and_link Pathname.pwd
     (prefix/Language::Python.site_packages(python3)/"homebrew-ale.pth").write venv.site_packages
 
