901b2f258a10ad45b13b8eb4e9364312cefe8742
Michael Cho <michael@michaelcho.dev>
2026-04-02 16:33:46 -0400

    yelp-tools: replace `yelp-xsl` resource

--- Formula/y/yelp-tools.rb
+++ Formula/y/yelp-tools.rb
@@ -7,7 +7,7 @@ class YelpTools < Formula
   url "https://download.gnome.org/sources/yelp-tools/42/yelp-tools-42.1.tar.xz"
   sha256 "3e496a4020d4145b99fd508a25fa09336a503a4e8900028421e72c6a4b11f905"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "aadb2689c5b993d7f4905f462f40ef9f83c2460e35122d580da4e7fd5303b3db"
@@ -18,29 +18,33 @@ class YelpTools < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "116324ef6b430649dec205fb66c33adc09ba9f719cadf7f33cf9d32e54c48de1"
   end
 
-  depends_on "gettext" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
   depends_on "itstool"
   depends_on "libxml2"
   depends_on "python@3.14"
+  depends_on "yelp-xsl" => :no_linkage
 
   uses_from_macos "libxslt"
+
+  pypi_packages package_name:   "",
+                extra_packages: "lxml"
 
   resource "lxml" do
     url "https://files.pythonhosted.org/packages/aa/88/262177de60548e5a2bfc46ad28232c9e9cbde697bd94132aeb80364675cb/lxml-6.0.2.tar.gz"
     sha256 "cd79f3367bd74b317dda655dc8fcfa304d9eb6e4fb06b7168c5cf27f96e0cd62"
   end
 
-  resource "yelp-xsl" do
-    url "https://download.gnome.org/sources/yelp-xsl/42/yelp-xsl-42.1.tar.xz"
-    sha256 "238be150b1653080ce139971330fd36d3a26595e0d6a040a2c030bf3d2005bcd"
-  end
-
   resource "mallard-rng" do
-    url "https://deb.debian.org/debian/pool/main/m/mallard-rng/mallard-rng_1.1.0.orig.tar.bz2"
+    url "https://github.com/projectmallard/projectmallard.org/raw/refs/tags/mallard-rng-1.1.0/download/mallard-rng-1.1.0.tar.bz2"
+    mirror "https://deb.debian.org/debian/pool/main/m/mallard-rng/mallard-rng_1.1.0.orig.tar.bz2"
     sha256 "66bc8c38758801d5a1330588589b6e81f4d7272a6fbdad0cd4cfcd266848e160"
+
+    livecheck do
+      url :url
+      regex(/^mallard-rng[._-]v?(\d+(?:\.\d+)+)$/i)
+    end
   end
 
   def install
@@ -49,14 +53,8 @@ class YelpTools < Formula
       system "make", "install"
     end
 
-    resource("yelp-xsl").stage do
-      system "./configure", "--disable-silent-rules", *std_configure_args
-      system "make", "install"
-      ENV.append_path "PKG_CONFIG_PATH", share/"pkgconfig"
-    end
-
     venv = virtualenv_create(libexec, "python3.14")
-    venv.pip_install resource("lxml")
+    venv.pip_install resources.reject { |r| r.name == "mallard-rng" }
     ENV.prepend_path "PATH", venv.root/"bin"
 
     system "meson", "setup", "build", *std_meson_args
