fd956d60527f912b39eda0e5ca08d4991e6b6228
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 20:05:11 +0000

    bitwarden-cli 2026.3.0

--- Formula/b/bitwarden-cli.rb
+++ Formula/b/bitwarden-cli.rb
@@ -1,8 +1,8 @@
 class BitwardenCli < Formula
   desc "Secure and free password manager for all of your devices"
   homepage "https://bitwarden.com/"
-  url "https://github.com/bitwarden/clients/archive/refs/tags/cli-v2026.2.0.tar.gz"
-  sha256 "ba42bd84e6d0e18e714eb2db0764bbcf78d45e7be1581ba6e05e333e4b6dded8"
+  url "https://github.com/bitwarden/clients/archive/refs/tags/cli-v2026.3.0.tar.gz"
+  sha256 "85554eade98e3cb972d4fb66d6687f752d7a78af4a4824a2668960f5258af62e"
   license "GPL-3.0-only"
   head "https://github.com/bitwarden/clients.git", branch: "main"
 
