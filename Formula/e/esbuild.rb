a36a221ab985f14d5bc4775dd4787ae882ece3d7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 00:26:07 +0000

    esbuild: update 0.28.0 bottle.

--- Formula/e/esbuild.rb
+++ Formula/e/esbuild.rb
@@ -7,12 +7,12 @@ class Esbuild < Formula
   head "https://github.com/evanw/esbuild.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc467eb1c46c7822f56a01bc83d64957e6fcd300ef41a06adc37b3d46b0e7104"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc467eb1c46c7822f56a01bc83d64957e6fcd300ef41a06adc37b3d46b0e7104"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc467eb1c46c7822f56a01bc83d64957e6fcd300ef41a06adc37b3d46b0e7104"
-    sha256 cellar: :any_skip_relocation, sonoma:        "064f2b7f4821b182dc351ba22c6eaee53d52b3750741ec8ea3d8d3666fa6cf7c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a06cbe5618e26a20e260a202cc4cb57ff17026d0b958dfd801eb9e21ef005f13"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d5002bde1f4c2996b20d27633b08c935fbc54f7ebaae154e07d46e4be9296c1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f62ac013c8317d78e0523e136ddaf21bd1e5165f774cc27d31ed6059ff0df855"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f62ac013c8317d78e0523e136ddaf21bd1e5165f774cc27d31ed6059ff0df855"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f62ac013c8317d78e0523e136ddaf21bd1e5165f774cc27d31ed6059ff0df855"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c64ef57deb512ce3c553ddc910cbcebe2102b5846c555d39467578037a3937cf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "847f32ae2438b0afd916a011be8e2325ddf42cb2956d63c7d009b1fae3fc9207"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "852fa72d9edbcabb3c7174d2d6170d18817203bd978b3e0644d4aac0a951bb95"
   end
 
   depends_on "go" => :build
