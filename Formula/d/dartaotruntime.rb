acce05ee87270fabf400d2a357b91547a6fd7dd4
Michael Cho <michael@michaelcho.dev>
2026-04-03 15:03:53 -0400

    dartaotruntime 3.11.4 (new formula)

---
+++ Formula/d/dartaotruntime.rb
@@ -0,0 +1,65 @@
+class Dartaotruntime < Formula
+  desc "Command-line tool for running AOT-compiled snapshots of Dart code"
+  homepage "https://dart.dev/tools/dartaotruntime"
+  # NOTE: Using a placeholder file because the build source is fetched by gclient
+  url "https://raw.githubusercontent.com/dart-lang/sdk/refs/tags/3.11.4/README.md"
+  sha256 "ff4301ec8e5c1259c5778c4abc947e303308cd31af30acd55575f5ca7ed6f405"
+  license "BSD-3-Clause"
+
+  livecheck do
+    formula "dart-sdk"
+  end
+
+  depends_on "ninja" => :build
+  depends_on xcode: :build # for xcodebuild
+  depends_on "dart-sdk" => :test
+
+  uses_from_macos "curl" => :build
+  uses_from_macos "python" => :build
+  uses_from_macos "xz" => :build
+
+  # always pull the latest commit from https://chromium.googlesource.com/chromium/tools/depot_tools.git/+/refs/heads/main
+  resource "depot-tools" do
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git",
+        revision: "4ce8ba39a3488397a2d1494f167020f21de502f3"
+    version "4ce8ba39a3488397a2d1494f167020f21de502f3"
+
+    livecheck do
+      url "https://chromium.googlesource.com/chromium/tools/depot_tools.git/+/refs/heads/main?format=JSON"
+      regex(/"commit":\s*"(\h+)"/i)
+    end
+  end
+
+  def install
+    resource("depot-tools").stage(buildpath/"depot-tools")
+    ENV["DEPOT_TOOLS_UPDATE"] = "0"
+    ENV.append_path "PATH", buildpath/"depot-tools"
+
+    system "gclient", "config", "--name", "sdk", "https://dart.googlesource.com/sdk.git@#{version}"
+    system "gclient", "sync", "--no-history"
+
+    # FIXME: Workaround for https://github.com/dart-lang/sdk/issues/63089
+    if OS.mac? && MacOS::Xcode.version >= "26.4"
+      inreplace "sdk/build/config/compiler/BUILD.gn",
+                "\"-Wno-tautological-constant-compare\",",
+                "\"-Wno-tautological-constant-compare\", \"-Wno-deprecated-declarations\","
+    end
+
+    cd "sdk" do
+      arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+      out = OS.mac? ? "xcodebuild" : "out"
+      system "./tools/build.py", "--mode=release", "--arch=#{arch}", "copy_dart_aotruntime"
+      bin.install "#{out}/Release#{arch.upcase}/dart-sdk/bin/dartaotruntime"
+      prefix.install_metafiles Pathname.pwd
+    end
+  end
+
+  test do
+    dart = Formula["dart-sdk"].bin/"dart"
+    system dart, "create", "dart-test"
+    cd "dart-test" do
+      system dart, "compile", "aot-snapshot", "bin/dart_test.dart"
+      assert_match "Hello world: 42!", shell_output("#{bin}/dartaotruntime bin/dart_test.aot")
+    end
+  end
+end
