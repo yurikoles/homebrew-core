7cebc2a7e0ca8c555982424f3748afba2cbab525
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 14:30:21 +0000

    cloudquery: update 6.35.5 bottle.

--- Formula/c/cloudquery.rb
+++ Formula/c/cloudquery.rb
@@ -12,12 +12,12 @@ class Cloudquery < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "11ff60276af8d1fa3d5e0c5c36b9031cbf3d62974658c03854163abf626e29d0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "11ff60276af8d1fa3d5e0c5c36b9031cbf3d62974658c03854163abf626e29d0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "11ff60276af8d1fa3d5e0c5c36b9031cbf3d62974658c03854163abf626e29d0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "b8c179e10ce5efb17921ca9ce754bc697d93c59edf06b9f68858c3f761ae6e9f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9152e16942afd6fab6068d765124fbc32c5e40843eddb3d78d9492dc6e8c8c62"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f67abd452f587951dc32d7fcfce4529ec523808dca2e3496bc6ff9a2774d3fa6"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ae2752ee4dac84c129cb5bf56a02047f942ad522787496c5fbdd6df8d410d4d5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ae2752ee4dac84c129cb5bf56a02047f942ad522787496c5fbdd6df8d410d4d5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae2752ee4dac84c129cb5bf56a02047f942ad522787496c5fbdd6df8d410d4d5"
+    sha256 cellar: :any_skip_relocation, sonoma:        "1487848b6e63a38b846eca078627ac8d5c8e9ab1152337a42dc75d73ad89be46"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "553f4687e25cbe5205296f0f911fa2ec4ea8426a836f176cfc81a2d4b9770b23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "472d5b2a42511639acede99263c1331e1b621528766b21b6976468d1e9de226b"
   end
 
   depends_on "go" => :build
