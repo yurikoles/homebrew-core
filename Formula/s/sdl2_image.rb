999df8fb18eb8783e97341a3fda06497bde1c0cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-04 23:30:51 +0000

    sdl2_image 2.8.10

--- Formula/s/sdl2_image.rb
+++ Formula/s/sdl2_image.rb
@@ -1,8 +1,8 @@
 class Sdl2Image < Formula
   desc "Library for loading images as SDL surfaces and textures"
   homepage "https://github.com/libsdl-org/SDL_image"
-  url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.8/SDL2_image-2.8.8.tar.gz"
-  sha256 "2213b56fdaff2220d0e38c8e420cbe1a83c87374190cba8c70af2156097ce30a"
+  url "https://github.com/libsdl-org/SDL_image/releases/download/release-2.8.10/SDL2_image-2.8.10.tar.gz"
+  sha256 "ebc059d01c007a62f4b04f10cf858527c875062532296943174df9a80264fd65"
   license "Zlib"
   head "https://github.com/libsdl-org/SDL_image.git", branch: "main"
 
