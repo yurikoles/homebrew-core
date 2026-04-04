26a13a9f5570c8e6fb653f3f1438ecb4c7096592
Branch Vincent <branchevincent@gmail.com>
2026-04-04 09:57:21 -0700

    nuitka: migrate to `python@3.14`

--- Formula/n/nuitka.rb
+++ Formula/n/nuitka.rb
@@ -5,7 +5,8 @@ class Nuitka < Formula
   homepage "https://nuitka.net"
   url "https://files.pythonhosted.org/packages/ed/0e/2537066db2458843e4a1edfc524409069ad58a456fc4b312b18b1d327431/nuitka-4.0.7.tar.gz"
   sha256 "26543bfed6009a466ae8608bdc643add81a497e8662e4aaf157cd00aa7fd5b9f"
-  license "Apache-2.0"
+  license "AGPL-3.0-only"
+  head "https://github.com/Nuitka/Nuitka.git", branch: "develop"
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "446a6c056e9ee1dbe150a3baecebb7f56710424006a6e45d8ddec6430ae6ea4e"
@@ -17,7 +18,7 @@ class Nuitka < Formula
   end
 
   depends_on "ccache"
-  depends_on "python@3.13" # planning to support Python 3.14, https://github.com/Nuitka/Nuitka/issues/3630
+  depends_on "python@3.14"
 
   on_linux do
     depends_on "patchelf"
@@ -25,11 +26,11 @@ class Nuitka < Formula
 
   def install
     virtualenv_install_with_resources
-    man1.install Dir["doc/*.1"]
+    man1.install buildpath.glob("doc/*.1")
   end
 
   test do
-    (testpath/"test.py").write <<~EOS
+    (testpath/"test.py").write <<~PYTHON
       def talk(message):
           return "Talk " + message
 
@@ -38,7 +39,7 @@ class Nuitka < Formula
 
       if __name__ == "__main__":
           main()
-    EOS
+    PYTHON
     assert_match "Talk Hello World", shell_output("#{libexec}/bin/python test.py")
     system bin/"nuitka", "--onefile", "-o", "test", "test.py"
     assert_match "Talk Hello World", shell_output("./test")
