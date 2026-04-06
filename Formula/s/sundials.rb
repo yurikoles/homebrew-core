554f47000c59b62771dda9e9c5d31fc46991a3ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-06 20:45:11 +0000

    sundials: update 7.7.0 bottle.

--- Formula/s/sundials.rb
+++ Formula/s/sundials.rb
@@ -6,12 +6,12 @@ class Sundials < Formula
   license "BSD-3-Clause"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "b051128524fb8b2c609e35b4449028196bc24613c499bd1110e0d49f54a6b9b3"
-    sha256 cellar: :any,                 arm64_sequoia: "a8dc589a643e0381247a0b83d3084907b0ba7ee86e8403efc44fe29154d21694"
-    sha256 cellar: :any,                 arm64_sonoma:  "6f2eb069ddfafcbb491cb8b17409c32f22a884308e22fa31c26b832f95ad4af0"
-    sha256 cellar: :any,                 sonoma:        "fe7a35cecf06f0db23ead8ddba87ffae08166c2685354244ec584c964c54982f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "82ca7dc8f48310ed5d19dc8b5b21ebf6d7d0884e820e7e0ce3a765f0056d32eb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4d4449de953ce1f0ffc1eb592e178c39a2ca7bbd192672edb2c467b969f9a30f"
+    sha256 cellar: :any,                 arm64_tahoe:   "98e1ddacb1e65111a29e9e4f5ab3e21f8ee232b3f517d779cfa5a33df1582189"
+    sha256 cellar: :any,                 arm64_sequoia: "29b15da812a8da99b803d2b2c40b00a2e89ed20609055d071eac008a7355e3f6"
+    sha256 cellar: :any,                 arm64_sonoma:  "30a2da3fd9660935f6700c250f66cda384a97e5fb44c72fe9d77f383ac0ff122"
+    sha256 cellar: :any,                 sonoma:        "fa77a74a7082d97259498b211ee12426a21dacb8d58598507df710cc57559e5a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "31184238f55e92112ed494f036a5310b4bae98bc15e30daa3fd6ef04a4fa1597"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ead433c94afc1e547801d3e841404bfe1068d073eeb70c0d485bcdf26844dd04"
   end
 
   depends_on "cmake" => :build
