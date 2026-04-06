3306a298a398674230101a24f82e1945dc0e02d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-06 04:11:37 +0000

    gitnr: update 0.3.0 bottle.

--- Formula/g/gitnr.rb
+++ Formula/g/gitnr.rb
@@ -7,12 +7,12 @@ class Gitnr < Formula
   head "https://github.com/reemus-dev/gitnr.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6f55ca6ffd7a5014925821fe4377caf19874f75837f9a5396dbd12482221d0a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a46abfed2d446a26fd0bdf62c04f7136282411a585f6191f8c95a2b2018586a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f43cc5851f5fc45ffada1fc2f12ec45ea95358659833402f29821541579012ec"
-    sha256 cellar: :any_skip_relocation, sonoma:        "96444e77d1b7399ac511fdd49fe358bffe34717f97f820047d09e21f59981f9c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d72e48d034a9e4298b71f1cf912286668a9b898381ab8ffc2c960918d18df611"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcb4d3b8d42018f3f4c5273361a9f375000bfecccfb59b32f04f45246ab166a0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "494ca6dec62622f6a21fcb8c6f35ba5e125e5aed2efa9f3c31ad9cbf3536e229"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cebed9b4a162160145612f92dee63eb5ca070b79fac981c88081d1d0ce63ab9b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7f5982f311a61d7438d4a09473a1b0f76f7d746b8e2d36975b61815ceff00e0"
+    sha256 cellar: :any_skip_relocation, sonoma:        "52f8cae5b076f50a940e44a114d385475ee9678601aa007e77cfbefdafb73f0c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "868a9851e7d7199029195cddaf7e34fbf849b197d10e24b75497936a6d385df1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57e5b49b99fa4499d6f75e46c89f965ed488c956841c4f2d349ce1596c9b1b80"
   end
 
   depends_on "pkgconf" => :build
