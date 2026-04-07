c3d12205592336f9fd6d2fd0d9caf5a9f001c1f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 20:18:11 +0000

    flagd 0.15.1

--- Formula/f/flagd.rb
+++ Formula/f/flagd.rb
@@ -2,8 +2,8 @@ class Flagd < Formula
   desc "Feature flag daemon with a Unix philosophy"
   homepage "https://github.com/open-feature/flagd"
   url "https://github.com/open-feature/flagd.git",
-      tag:      "flagd/v0.15.0",
-      revision: "b4fc89c758a7083513f4fe0a187f1b7ac5c0a2d6"
+      tag:      "flagd/v0.15.1",
+      revision: "18ad1db202aa5e623b99d53c5f1cc690d151e78c"
   license "Apache-2.0"
   head "https://github.com/open-feature/flagd.git", branch: "main"
 
