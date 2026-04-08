ae266e754e9b45b9fdc83412144d4e9df5667096
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 18:00:43 +0000

    sheets 0.2.0

--- Formula/s/sheets.rb
+++ Formula/s/sheets.rb
@@ -1,8 +1,8 @@
 class Sheets < Formula
   desc "Terminal based spreadsheet tool"
   homepage "https://github.com/maaslalani/sheets"
-  url "https://github.com/maaslalani/sheets/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "34220baa77439bc5d19ae53edae05a691f05f8c7eec3d65b4f1f6c5de3125f41"
+  url "https://github.com/maaslalani/sheets/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "e33563769858abba1812d6f9e1f427241a9a5c65da3a34a87d8784c8a050e25e"
   license "MIT"
   head "https://github.com/maaslalani/sheets.git", branch: "main"
 
