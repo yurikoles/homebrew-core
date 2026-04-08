ad875a90d9053325d0e016d2d824f2a997d8aaef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 00:03:00 +0000

    terraform-mcp-server 0.5.1

--- Formula/t/terraform-mcp-server.rb
+++ Formula/t/terraform-mcp-server.rb
@@ -1,8 +1,8 @@
 class TerraformMcpServer < Formula
   desc "MCP server for Terraform"
   homepage "https://github.com/hashicorp/terraform-mcp-server"
-  url "https://github.com/hashicorp/terraform-mcp-server/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "27eaeaf9c2ef42ee842a29556798c9ccc456982a8ab1e92e04b8d8a699893d75"
+  url "https://github.com/hashicorp/terraform-mcp-server/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "2c47012663357a46ab5702999b25f17efe8d0cc2214231944202aa87bceb8139"
   license "MPL-2.0"
   head "https://github.com/hashicorp/terraform-mcp-server.git", branch: "main"
 
