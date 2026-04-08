87c6f1ed66ab0d97073253dd46ed0aef6f49f218
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 00:43:53 +0000

    vals: update 0.43.8 bottle.

--- Formula/v/vals.rb
+++ Formula/v/vals.rb
@@ -7,12 +7,12 @@ class Vals < Formula
   head "https://github.com/helmfile/vals.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c88cd03ca3942fee69f5765ce993b5fe923d580786637339198ea149081656a0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c88cd03ca3942fee69f5765ce993b5fe923d580786637339198ea149081656a0"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c88cd03ca3942fee69f5765ce993b5fe923d580786637339198ea149081656a0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "46efb8cf7a447674f9725656b32ac6f89cfea4bb5c54fd22410baf48d9ef50ad"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "be9368c8b07106eaea39ad04d05854f29d4f9bc82a21bd69cd0d07249fba7276"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86a3fab80178b5ebd24a084499b295a170eb1e2c2edf4a71a677c8c902c8546d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be1a04501634a831cf422dded590c36dba2836421c14eef0422244e1e8120b47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "be1a04501634a831cf422dded590c36dba2836421c14eef0422244e1e8120b47"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be1a04501634a831cf422dded590c36dba2836421c14eef0422244e1e8120b47"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c940bc6cedc81e65ef80ab220579c6a09fb7024aab8d41e0f11e46fe457493d7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a5686e6205ce3af53d24499bb045d092de040b28e9e44d3b9dfcbf56ea6fd71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8761ceb2e2c3bca33e030e6c41fc0502e3983ec98756e8e95f5b7e7bc8ab8d0"
   end
 
   depends_on "go" => :build
