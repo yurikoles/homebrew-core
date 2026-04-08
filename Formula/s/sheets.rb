0cfd57b81604977062dfff0ef76398ba43f574c5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 16:10:33 +0000

    sheets: add 0.1.0 bottle.

--- Formula/s/sheets.rb
+++ Formula/s/sheets.rb
@@ -6,6 +6,15 @@ class Sheets < Formula
   license "MIT"
   head "https://github.com/maaslalani/sheets.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "75ac510571c868f70119177cf4bcef943606c5f3d237ef2047f506a49af9a910"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75ac510571c868f70119177cf4bcef943606c5f3d237ef2047f506a49af9a910"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75ac510571c868f70119177cf4bcef943606c5f3d237ef2047f506a49af9a910"
+    sha256 cellar: :any_skip_relocation, sonoma:        "3f91eb68787c27e42198dfb1f555dcefef29fd5365992139af490aff676dbe19"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "32df9b00652e83279c9c75e594aad51f4dd9e772a21c21401acc114433bd83b5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4e4bdfd490e1ae698abf3a4d89be2722f0b0679fa43ddbf47a9484f6a0c2596"
+  end
+
   depends_on "go" => :build
 
   def install
