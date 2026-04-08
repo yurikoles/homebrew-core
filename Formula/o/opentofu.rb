76df3cc18d5de31a373a329ca185f052c858ccf9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 11:40:22 +0000

    opentofu 1.11.6

--- Formula/o/opentofu.rb
+++ Formula/o/opentofu.rb
@@ -1,8 +1,8 @@
 class Opentofu < Formula
   desc "Drop-in replacement for Terraform. Infrastructure as Code Tool"
   homepage "https://opentofu.org/"
-  url "https://github.com/opentofu/opentofu/archive/refs/tags/v1.11.5.tar.gz"
-  sha256 "450f962f262d9f484ad1fb73454650740cdce0d83a854ad8b6c183cc5822eb09"
+  url "https://github.com/opentofu/opentofu/archive/refs/tags/v1.11.6.tar.gz"
+  sha256 "4c16aaac1c8db7386488abb13226f93fed4141698d0ebc02711029e6d6676a82"
   license "MPL-2.0"
   head "https://github.com/opentofu/opentofu.git", branch: "main"
 
