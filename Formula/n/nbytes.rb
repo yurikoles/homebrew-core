74fbcdf6d4db4db6c72bc66ba9fdf058111f9e58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 20:09:04 +0000

    nbytes: add 0.1.3 bottle.

--- Formula/n/nbytes.rb
+++ Formula/n/nbytes.rb
@@ -5,6 +5,15 @@ class Nbytes < Formula
   sha256 "556b4bbe3ed747cea1d4466133f7abb82595f38c8f808be1d248b0bf8682e509"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "5e400f4460fca0bd6bf25d2239342bffdfa73ba62a525fbaaaaf2f79731e185e"
+    sha256 cellar: :any,                 arm64_sequoia: "fe1bc7b5397bd0322aa723cae5808c7cb7acc3d52147788dc872d621dc0470f5"
+    sha256 cellar: :any,                 arm64_sonoma:  "a756061ca6d2657a8c00de66ea4c66efa53562d2e2224a141c6b4c81904150a3"
+    sha256 cellar: :any,                 sonoma:        "1ff8ca849ec2a74b332e0aff1b97d510a393c1bf79c19d60ccf87b2d5feda5a9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "67f911482f5523014176fc48f06dfb3e8cb0e1bb5e9b89f336170200d1b37c42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0a78948ebf2c939802226061ccdffc538a6b9918bdbd22e88659134cc51f12a3"
+  end
+
   depends_on "cmake" => :build
 
   def install
