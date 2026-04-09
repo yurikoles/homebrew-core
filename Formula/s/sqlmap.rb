b0af8621243e50f19eb530a6ed8ec1cb4f7550a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-09 18:28:03 +0000

    sqlmap: update 1.10.4 bottle.

--- Formula/s/sqlmap.rb
+++ Formula/s/sqlmap.rb
@@ -9,7 +9,7 @@ class Sqlmap < Formula
   head "https://github.com/sqlmapproject/sqlmap.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "49665dd265de2071595686613b6e83500f5f24bb0c81b59add925ae49755f1b5"
+    sha256 cellar: :any_skip_relocation, all: "f799831090eb12cf955f46a0e6c5b8b95f975ae5421b95edab06d9bd602b8935"
   end
 
   depends_on "python@3.14"
