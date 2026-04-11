2e5554e3e46116a9e70e9b2d42b632395ae6f5d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-11 17:34:30 +0000

    php-cs-fixer 3.95.0

--- Formula/p/php-cs-fixer.rb
+++ Formula/p/php-cs-fixer.rb
@@ -1,8 +1,8 @@
 class PhpCsFixer < Formula
   desc "Tool to automatically fix PHP coding standards issues"
   homepage "https://cs.symfony.com/"
-  url "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/download/v3.94.2/php-cs-fixer.phar"
-  sha256 "aaafc3a53b877c6741ddccac0a783f0fb73fff5f84fba5870d5dd306ee22dde0"
+  url "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/download/v3.95.0/php-cs-fixer.phar"
+  sha256 "66866a7240e05c3912eae15fafd0484ee0b28823173e7f2ff574076cd746f532"
   license "MIT"
 
   bottle do
