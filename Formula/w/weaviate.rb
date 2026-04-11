1df100f2e7e8a9c23215676470aac71cdc44ac57
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-11 11:41:08 +0000

    weaviate 1.36.10

--- Formula/w/weaviate.rb
+++ Formula/w/weaviate.rb
@@ -1,8 +1,8 @@
 class Weaviate < Formula
   desc "Open-source vector database that stores both objects and vectors"
   homepage "https://weaviate.io/developers/weaviate/"
-  url "https://github.com/weaviate/weaviate/archive/refs/tags/v1.36.9.tar.gz"
-  sha256 "92c5746ad1a55520bb2798cf54a13531568f6fae58ef89a6a3a155b1d8c8cb01"
+  url "https://github.com/weaviate/weaviate/archive/refs/tags/v1.36.10.tar.gz"
+  sha256 "418ed13ccc0a014ba2c3e46c91924129e189f37797629afa2c6014064ac63ec9"
   license "BSD-3-Clause"
 
   bottle do
