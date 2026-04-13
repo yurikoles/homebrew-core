4bdec0f2904280a01659976eb59c231dcf97a684
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 06:23:37 +0000

    tagref 1.12.1

--- Formula/t/tagref.rb
+++ Formula/t/tagref.rb
@@ -1,8 +1,8 @@
 class Tagref < Formula
   desc "Refer to other locations in your codebase"
   homepage "https://github.com/stepchowfun/tagref"
-  url "https://github.com/stepchowfun/tagref/archive/refs/tags/v1.12.0.tar.gz"
-  sha256 "cbed599298e23ed526c9f6d5463eaf203cb75a88797ac2609e5eb45111451b8d"
+  url "https://github.com/stepchowfun/tagref/archive/refs/tags/v1.12.1.tar.gz"
+  sha256 "3d0017911afb6d9b887444c6da32f1642ad4046b4098af3d412fae1b58fece8c"
   license "MIT"
 
   bottle do
