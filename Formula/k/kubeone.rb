9302f088a3d4380b756750eb00bc8e19be8382e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 18:26:50 +0000

    kubeone: update 1.13.2 bottle.

--- Formula/k/kubeone.rb
+++ Formula/k/kubeone.rb
@@ -7,12 +7,12 @@ class Kubeone < Formula
   head "https://github.com/kubermatic/kubeone.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8c2cb9eb89b74c150ec01418799a4caf6d5bc452cbe5ddde13fce7759b7de741"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0f002c2cba194b7415a71d1e28b6af919bd358bcfeb57e34bdc5961938f2b0de"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f8a597e963ddc35c158b5a022192b5b75f98b27758495104ea8fb07b996a287"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4541f2baf910a07ac8a269be81a0edb9838e7cb1828d92ceb9da09e099b3fc89"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "227e8a0d062893f0e2c9ebad50689a68882252b9b6de13a013f5d6c87ec1c4c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a97ab73db225d6451b4fe5c92d394d090b40eb4c78b2a664be2c83170440e505"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "86a623a98fb6291e42fb6017d6ec3a47eabcdfc3dac5edfdeaac129f888a98cd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da05cecb8a9d4a2c32e48e3a5b61a455fd01d678407a4abe333ed047f7555f5a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62a362f92f36dff54f847f6632ed96eacd1a8ce29792b3d95fcf959846c3665b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a120b07a4edb0d96756d83c5d3ee66004422d5cc55016c9df973c20bf16918f4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "967ddf9cba182ab9daf552f5e5a6d44e111562c54b90e101ec53584b3579ef2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3546931f670cf5a7961ab4a53d07ebb315ce2bdd830d6e94738bde0dc479461c"
   end
 
   depends_on "go" => :build
