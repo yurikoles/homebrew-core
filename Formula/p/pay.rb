b08c55b1ddb387d4fdb9771d8dcb10c147584c0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-14 18:27:42 +0000

    pay: update 0.6.0 bottle.

--- Formula/p/pay.rb
+++ Formula/p/pay.rb
@@ -12,12 +12,12 @@ class Pay < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e9ee826e12b18611f3bec40d9e8a97c705c3a323099195f03aa309e593f971ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bfb96bd369fae9f8ea6ad6632582b060206843074b57161344c563f0b3c2ae54"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c249cd9617a0bff08a3a27ba62a61da57baa19a02f8c82fd26a54780a3d124d"
-    sha256 cellar: :any_skip_relocation, sonoma:        "fdfa314eff66e7192f5a34d8cf5831efd9921dd5d90ea9ae1a136fc4c8fef64e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ce8da52c8abd9a6d582ee8e8dc4a4490187fd450a96414a0165197fdeee5719"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6fb1f64ed722966362e2d5a095cb23e219ae3d3da92ff7995b8d7b536321afb2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d247cd9096126c7f456c16bea41e7a092c7081ef539fb2619b16882643148d3c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72e310def7165098fb7a0e15d0a53e1ce97535821005d467c6de5ed0009512c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6620c71819fbb86a096367cd89bf2fe52ff829f43600ac5261f597b041f50f8b"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c4bea46ef3493b1f3afc2d2b14ec28d8faab93d6177fc0e90bcc1b6c6c95bc4f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8b3246aaca8b38186a226bf1420b3e00f29ab70331c9b6a304479cbf8269a8a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6639250300bb3e9599efccb35970adc6ebdb8df2a7b980386fbbc07b1e57810"
   end
 
   depends_on "pkgconf" => :build
