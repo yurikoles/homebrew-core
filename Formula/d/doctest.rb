e734fa5175c6023d3197138fb1c7a4b45dc0a42d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-14 14:34:45 +0000

    doctest 2.5.2

--- Formula/d/doctest.rb
+++ Formula/d/doctest.rb
@@ -1,8 +1,8 @@
 class Doctest < Formula
   desc "Feature-rich C++11/14/17/20/23 single-header testing framework"
   homepage "https://github.com/doctest/doctest"
-  url "https://github.com/doctest/doctest/archive/refs/tags/v2.5.1.tar.gz"
-  sha256 "d4ebd26061d5a5d05355f52289c3f595d744aac8d70c547a012b2be96bc2f014"
+  url "https://github.com/doctest/doctest/archive/refs/tags/v2.5.2.tar.gz"
+  sha256 "9189960c2bbbc4f3382ce0773b2bb5f13e3afd8fed47f55f193e11e85a4f9854"
   license "MIT"
 
   bottle do
