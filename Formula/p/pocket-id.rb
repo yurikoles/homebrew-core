930b55a3aa12c2b124e9c08bba1c68f000235208
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 18:01:45 +0000

    pocket-id: add 2.5.0 bottle.

--- Formula/p/pocket-id.rb
+++ Formula/p/pocket-id.rb
@@ -6,6 +6,15 @@ class PocketId < Formula
   license "BSD-2-Clause"
   head "https://github.com/pocket-id/pocket-id.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "28f536b00fc54f979ac9a8a47c409b0fbb394f3adc486c650b6c5d8fe601ec20"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69b60408b9f13441fd6d7ac110833438624ab376d674ea526066210f28a03a23"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eed4801a058be610f3c182c462479d6b9cb596d54668276040670792e367d296"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5f93366eebf1c4602f654bcf71d8081a542b03eaf5f068099bbf14459a35a0aa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0439b67d8ca0618309abcb08d7908d625c263faa8d7b3f91285626f536f6b1c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de3ce3b86f1f00388a7a117f093a90f7e8918c32e693088dea4e01fcbce538b7"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
   depends_on "pnpm" => :build
