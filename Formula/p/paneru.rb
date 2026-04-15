b655be065d1c96a70c8ed394b3cab24397708f02
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 20:33:00 +0000

    paneru: add 0.4.0 bottle.

--- Formula/p/paneru.rb
+++ Formula/p/paneru.rb
@@ -5,6 +5,13 @@ class Paneru < Formula
   sha256 "f0dab14876c3e6d37fd99a7e5ba2d9982a00bc165973d1a02f1209f633a2286a"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5abc62f6b6045f7818df9480f7d61a1e284dbe76c6aa42699ab20a3d71f77c80"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a277e14be273b0dca5ad0966a68a1afc727bdcaf1c97be0d0d09a7b61bcb715f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d1898491e0cda909f0d3162b970ebf2ef4a3965cf7cb359fc016affbeb914a3"
+    sha256 cellar: :any_skip_relocation, sonoma:        "916677612ab26e46d25ed669e5aec19a27868a6f1049b1c3c2a1b1fd7661dd65"
+  end
+
   depends_on "rust" => :build
   depends_on :macos
 
