7761307aeca7bcaa0039296831736a7e45a51970
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-15 11:50:09 +0000

    rio-terminal 0.3.10

--- Formula/r/rio-terminal.rb
+++ Formula/r/rio-terminal.rb
@@ -1,8 +1,8 @@
 class RioTerminal < Formula
   desc "Hardware-accelerated GPU terminal emulator powered by WebGPU"
   homepage "https://rioterm.com/"
-  url "https://github.com/raphamorim/rio/archive/refs/tags/v0.3.8.tar.gz"
-  sha256 "c9ebd12a9c2e625ea6d2214269f1f9ee1ea5f8bcb415f3355c4d1bdb58d14e5d"
+  url "https://github.com/raphamorim/rio/archive/refs/tags/v0.3.10.tar.gz"
+  sha256 "11e84859757d9872f0c39be8cc9c751d2b8329e222cc5a49dacab43e94627b6d"
   license "MIT"
   head "https://github.com/raphamorim/rio.git", branch: "main"
 
