526cbaa698de9b4ad13077380f8ef348a7f01c1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 17:44:10 +0000

    espflash: update 4.4.0 bottle.

--- Formula/e/espflash.rb
+++ Formula/e/espflash.rb
@@ -6,12 +6,12 @@ class Espflash < Formula
   license any_of: ["MIT", "Apache-2.0"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b1e30cbba1b4711890fbe1f435352ba3b06da0054710ba519a1f91e33a4cd14e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "26b303b44d33cac494f540f795cc17fec581827f3b3f835b0d36385469ecbfad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e9eba0a5f7280288a9b6e1a1b666af5220492bec7995ee2c25e7b2148b791a7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4d5e2121b6dd229722b0bca9868113a369e898b09524207dd2fbe17377a7a7bb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ae3c812a3e2c2a0a2ee029fb71317e8c4f46a242258a2e370dcbd9edfe7f552f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38f2c7d557115250dcedb4fb1599883298479d8843b47d49ec8598618f0c11ad"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8ce6d471a3ac8576d0a23338601cdb4e7a04ca970456f7c15054fcc3f6eb32ee"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "64d9779c15d95f959261cf45237af189180656d86535bd97e9f8bb1f17c07801"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9f0e4d819490ce64519baf03e97024ffbaa89be6e0b33378a9349947f2774ae1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "57f86958210e14a20f67adfebf0f9eb2194a16f025cab683edeca5afe1fa7fc6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1d9209050191bed2bd021b91d7f1eebfeca647635e6c457ee3d32bd5e5e85308"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae73809637c9666fef1d5ee9b594e58f6049a4257ca2a2c0f3a5eed6bd7d49b2"
   end
 
   depends_on "pkgconf" => :build
