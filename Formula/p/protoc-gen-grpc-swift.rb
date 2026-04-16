3cb5453d7394a00f74229167d018d569f91ae939
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 15:34:45 +0000

    protoc-gen-grpc-swift: update 2.3.0 bottle.

--- Formula/p/protoc-gen-grpc-swift.rb
+++ Formula/p/protoc-gen-grpc-swift.rb
@@ -13,10 +13,10 @@ class ProtocGenGrpcSwift < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "101dc5f42466d23a5a3d0a1bfbe4d4a91bc36ca80991b82d96c9eb13e75e93bd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e540f96b066c95b58652ee648b42f91801974d4e3b3800daaa6f0976891d8756"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7961d6b4bea1e05f9253e78cce62c92fe6b4b62c45c46654838c02ac89a40a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5592931cc95de58efc7fe3c4eca6bc0ee0b03b99f983478bddea9c4f6baa6cf1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9cd9e70551c661413509f39e1ae84985f402d6e7a613102bba9cdbd6f6ef0e47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3309e2cef17759593d4ab36e098ae4b8cd97814e105572acd08b0b685612bcba"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "72cebc2409038d77bfa8aa9ba6278e8ed1e59efacf6f6c63e7fd196986d991a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e424f49b131eee0119bd7f47849b0603b9e3f71dad51fe345abd6bcb54d381ad"
   end
 
   depends_on xcode: ["15.0", :build]
