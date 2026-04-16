f333c5cbe883de10da22e5223a394b13f7c93cca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 17:45:18 +0000

    libmpc 1.4.1

--- Formula/lib/libmpc.rb
+++ Formula/lib/libmpc.rb
@@ -1,9 +1,9 @@
 class Libmpc < Formula
   desc "C library for the arithmetic of high precision complex numbers"
   homepage "https://www.multiprecision.org/"
-  url "https://ftpmirror.gnu.org/gnu/mpc/mpc-1.4.0.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/mpc/mpc-1.4.0.tar.xz"
-  sha256 "3210b3a546b1cb00c296ca360891d7740ee6ff06deb02a27a35b20cd3c0bb1a5"
+  url "https://ftpmirror.gnu.org/gnu/mpc/mpc-1.4.1.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/mpc/mpc-1.4.1.tar.xz"
+  sha256 "91204cd32f164bd3b7c992d4a6a8ce6519511aadab30f78b6982d0bf8d73e931"
   license "LGPL-3.0-or-later"
   compatibility_version 1
 
