41480e9b4922dbca986e19ddb156d20dd683bdf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 04:20:13 +0000

    sheen-bidi: add 3.0.0 bottle.

--- Formula/s/sheen-bidi.rb
+++ Formula/s/sheen-bidi.rb
@@ -6,6 +6,15 @@ class SheenBidi < Formula
   license "Apache-2.0"
   head "https://github.com/Tehreer/SheenBidi.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "da90384ffa651f7306b191c8675d335e8d487bd1bc792e78e1f7f8fdec25a2f5"
+    sha256 cellar: :any,                 arm64_sequoia: "dc4a4d77ebe229806b7e24cd588e6d00f79a3a9b8e0ae3633a61139c09a93149"
+    sha256 cellar: :any,                 arm64_sonoma:  "21d7babed02a83a93e7b0da726688902fa1c76e949b1453e66443041bbf3b999"
+    sha256 cellar: :any,                 sonoma:        "859191bbfb6d0580b49f4b4a920ac594e93677e50570556daa730929ff13c8c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11ee3016f512270391c63197b69ff51120cb9675ce7030fd5b5bced267bbe840"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "794317a822672e57f16c1c717522d0bcd6c0338b150468c5ff55f6e374482a05"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :test
 
