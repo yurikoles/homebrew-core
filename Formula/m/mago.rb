0c0b7d60c3969a8b35010949b8196764a962e131
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-17 09:32:41 +0000

    mago: update 1.21.0 bottle.

--- Formula/m/mago.rb
+++ Formula/m/mago.rb
@@ -6,12 +6,12 @@ class Mago < Formula
   license any_of: ["Apache-2.0", "MIT"]
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b4547529d81a010dff75de06bd5b92c9c93be8e40498871bac511d344a5ae672"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2be46fb7b0b261b738b22048fe65bffec0d2d95ac85bd17680c676df0d8c5ec5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1e768fc8b0748c32c81f7a2b240dc3e2190c863f0c5e42875323e827c3deaac2"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7d957b886c94d4c5dc78a79b75a8efbfab7e0b727e47d8e288026c475cc5044e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "febef9c2dff3fb0b10c5e0efd06609dd9884f76091e0e043f42d9927cdc9da7a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb68d4ee56fb7e57df104035738a8334752bbe937e0256e9d42025879fee61d7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "951b8ffc8e9914e107591b3a515a1515a7294bc8442a5ace0236c2e1afbb4707"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de12492ed30235c7bd9071462fb8844ac450c295adaff45e3f3d926776fbc7a6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd91503c2d18ef0975493d08f81ea32b17420405b0e8099bba9710ea58b96abd"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f413d9af3375e4c1cc4e0eabf99c2fa477c60c540e923bbe6fbfb736d3c47653"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "54ab5efe9ebd58f8a7c01223504ea6314aa44bea0484ba355da719d252cb4a0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7764fbd2b0dd15294e308a88c2a63eb8c343f3445081377f0584bfca59210c4f"
   end
 
   depends_on "pkgconf" => :build
