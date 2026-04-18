4b3a670bfd1db94e0fd942027b49fcf6406c9cd7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-18 06:41:48 +0000

    pake 3.11.3

--- Formula/p/pake.rb
+++ Formula/p/pake.rb
@@ -1,8 +1,8 @@
 class Pake < Formula
   desc "Turn any webpage into a desktop app with Rust with ease"
   homepage "https://github.com/tw93/Pake"
-  url "https://registry.npmjs.org/pake-cli/-/pake-cli-3.11.2.tgz"
-  sha256 "077afecc8b7a3326fcfc7e8218948277d3f2bb50532ae0962106b56c7fc88f75"
+  url "https://registry.npmjs.org/pake-cli/-/pake-cli-3.11.3.tgz"
+  sha256 "9af106b4a8a99ddadf72bad6356c85b6032112a57091d17f7b7f23cb4caa8bb7"
   license "MIT"
 
   bottle do
