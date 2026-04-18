e3c6085521ffe506b08293a8c8f1a84250147dad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-18 14:21:28 +0000

    git-quick-stats 2.11.0

--- Formula/g/git-quick-stats.rb
+++ Formula/g/git-quick-stats.rb
@@ -1,8 +1,8 @@
 class GitQuickStats < Formula
   desc "Simple and efficient way to access statistics in git"
   homepage "https://git-quick-stats.sh/"
-  url "https://github.com/git-quick-stats/git-quick-stats/archive/refs/tags/2.10.0.tar.gz"
-  sha256 "1ddd005867b5260f51416d64c43e6076ccccbeb2966d08d2385ef4e711c4d9fa"
+  url "https://github.com/git-quick-stats/git-quick-stats/archive/refs/tags/2.11.0.tar.gz"
+  sha256 "7f522e604849a1c645d1379bca8e69d14382df3f1a29cb4a7cf1421c514f5562"
   license "MIT"
 
   bottle do
