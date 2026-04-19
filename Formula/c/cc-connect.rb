c79b5293f2e3af2cd0deec251b7d90090535c58e
Leon Adomaitis <leon@adomaitis.de>
2026-04-19 18:05:49 +0200

    cc-connect 1.3.0

--- Formula/c/cc-connect.rb
+++ Formula/c/cc-connect.rb
@@ -1,8 +1,8 @@
 class CcConnect < Formula
   desc "Bridges local AI coding agents to messaging platforms"
   homepage "https://github.com/chenhg5/cc-connect"
-  url "https://github.com/chenhg5/cc-connect/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "0ae471e625966cc80b17b062c3013927861be4d4527d8d7d90bdbf5892d1bf51"
+  url "https://github.com/chenhg5/cc-connect/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "b4823bcdf34273b4699df20903d8d1feda9f7e7397b5ca62b76d1ca0a1996632"
   license "MIT"
 
   bottle do
@@ -15,6 +15,7 @@ class CcConnect < Formula
   end
 
   depends_on "go" => :build
+  depends_on "node" => :build
 
   def install
     ldflags = %W[
@@ -24,19 +25,22 @@ class CcConnect < Formula
       -X main.buildTime=#{time.iso8601}
     ]
 
-    # Fix checksum mismatch: https://github.com/chenhg5/cc-connect/commit/92d4d81c205c346758170ac55bf17413fc47fd34
-    # TODO: Remove on next release.
-    inreplace "go.sum", "MEaUJLQJKFxTNo0xg+dKyOJA2Nu4O8kPVKuJ/gBiyjc=", "dRaEfpa2VI55EwlIW72hMRHdWouJeRF7TPYhI+AUQjk="
+    cd "web" do
+      system "npm", "install", *std_npm_args(prefix: false)
+      system "npm", "run", "build"
+    end
+
     system "go", "build", *std_go_args(ldflags:), "./cmd/cc-connect"
 
     pkgetc.install "config.example.toml" => "config.toml"
+    (var/"cc-connect").mkpath
   end
 
   service do
-    run opt_bin/"cc-connect"
+    run [opt_bin/"cc-connect", "--config", etc/"cc-connect/config.toml"]
+    working_dir var/"cc-connect"
     keep_alive true
-    error_log_path var/"log/cc-connect.log"
-    log_path var/"log/cc-connect.log"
+    environment_variables CC_LOG_FILE: var/"log/cc-connect.log", PATH: std_service_path_env
   end
 
   test do
