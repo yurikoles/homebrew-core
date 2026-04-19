aac5cd79317624037af8037cd7bcd8d92519ef35
tomkel <520225+tomkel@users.noreply.github.com>
2026-04-19 02:11:42 -0700

    aria2: Remove AppleTLS
    AppleTLS doesn't support TLS1.3 and throws errors when encountering
    TLS1.3.

--- Formula/a/aria2.rb
+++ Formula/a/aria2.rb
@@ -4,7 +4,7 @@ class Aria2 < Formula
   url "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0.tar.xz"
   sha256 "60a420ad7085eb616cb6e2bdf0a7206d68ff3d37fb5a956dc44242eb2f79b66b"
   license "GPL-2.0-or-later"
-  revision 1
+  revision 2
 
   bottle do
     rebuild 2
@@ -34,6 +34,7 @@ class Aria2 < Formula
 
   def install
     ENV.cxx11
+    ENV.append "LIBS", "-framework Security" if OS.mac?
 
     args = %w[
       --disable-silent-rules
@@ -42,14 +43,9 @@ class Aria2 < Formula
       --without-libgmp
       --without-libnettle
       --without-libgcrypt
+      --without-appletls
+      --with-openssl
     ]
-    if OS.mac?
-      args << "--with-appletls"
-      args << "--without-openssl"
-    else
-      args << "--without-appletls"
-      args << "--with-openssl"
-    end
 
     system "./configure", *args, *std_configure_args
     system "make", "install"
