129c24d2bd9ac9e271b4e175e55fd98a3a30a160
Leon Adomaitis <leon@adomaitis.de>
2026-04-19 15:13:46 +0200

    bibtexconv 2.2.0

--- Formula/b/bibtexconv.rb
+++ Formula/b/bibtexconv.rb
@@ -1,8 +1,8 @@
 class Bibtexconv < Formula
   desc "BibTeX file converter"
   homepage "https://github.com/dreibh/bibtexconv"
-  url "https://github.com/dreibh/bibtexconv/archive/refs/tags/bibtexconv-2.1.2.tar.gz"
-  sha256 "0bd2b14c4e92512dc39ca421fe4b1a11e6dc7bee4d1c948b1afa5a7c81b1bc46"
+  url "https://github.com/dreibh/bibtexconv/archive/refs/tags/bibtexconv-2.2.0.tar.gz"
+  sha256 "841542821ae46eaf4065e6b3b62b5e9466e4c85c17058b1b9c42f69190bd0111"
   license "GPL-3.0-or-later"
   head "https://github.com/dreibh/bibtexconv.git", branch: "master"
 
@@ -23,11 +23,11 @@ class Bibtexconv < Formula
   uses_from_macos "curl"
 
   on_macos do
-    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1500
+    depends_on "llvm" => :build if DevelopmentTools.clang_build_version <= 1600
   end
 
   fails_with :clang do
-    build 1500
+    build 1600
   end
 
   def install
