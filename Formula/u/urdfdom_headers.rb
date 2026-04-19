b61c0a21e7cd9da9e5d56dddbd20a57ab65b257d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-19 03:42:03 +0000

    urdfdom_headers 2.1.2

--- Formula/u/urdfdom_headers.rb
+++ Formula/u/urdfdom_headers.rb
@@ -1,8 +1,8 @@
 class UrdfdomHeaders < Formula
   desc "Headers for Unified Robot Description Format (URDF) parsers"
   homepage "https://wiki.ros.org/urdfdom_headers/"
-  url "https://github.com/ros/urdfdom_headers/archive/refs/tags/2.1.1.tar.gz"
-  sha256 "678484a5e857188a42c413cf5bddd499892b155b49c19880fec0ef509dc26973"
+  url "https://github.com/ros/urdfdom_headers/archive/refs/tags/2.1.2.tar.gz"
+  sha256 "3aeef6325b83f9283c9cee34f1e6f8d232acc7aeef2d47bc782a5193ae8aaec4"
   license "BSD-3-Clause"
 
   # Upstream uses Git tags (e.g. `1.0.0`) to indicate a new version. They
