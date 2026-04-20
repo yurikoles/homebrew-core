4223c3f578c3d1ef055251a622214cc4eb12bd37
Michael Cho <michael@michaelcho.dev>
2026-04-19 21:47:29 -0400

    serveit: use brew `ruby`

--- Formula/s/serveit.rb
+++ Formula/s/serveit.rb
@@ -21,30 +21,23 @@ class Serveit < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "e33ce3c0dbb5df24170e177bf1f0246d923e061304e201b54ed3f78051687a2d"
   end
 
-  uses_from_macos "ruby"
+  depends_on "ruby"
 
   # webrick is needed for ruby 3.0+ (as it is not part of the default gems)
   # upstream report, https://github.com/garybernhardt/serveit/issues/13
   resource "webrick" do
-    on_linux do
-      url "https://rubygems.org/downloads/webrick-1.8.1.gem"
-      sha256 "19411ec6912911fd3df13559110127ea2badd0c035f7762873f58afc803e158f"
-    end
+    url "https://rubygems.org/downloads/webrick-1.8.1.gem"
+    sha256 "19411ec6912911fd3df13559110127ea2badd0c035f7762873f58afc803e158f"
   end
 
   def install
-    bin.install "serveit"
-
-    if OS.linux?
-      ENV["GEM_HOME"] = libexec
-      resources.each do |r|
-        r.fetch
-        system "gem", "install", r.cached_download, "--no-document",
-                      "--install-dir", libexec
-      end
-
-      bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+    ENV["GEM_HOME"] = libexec
+    resources.each do |r|
+      system "gem", "install", r.cached_download, "--ignore-dependencies",
+                    "--no-document", "--install-dir", libexec
     end
+    bin.install "serveit"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 
   test do
