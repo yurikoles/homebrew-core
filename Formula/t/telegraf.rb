222acb4096c76b8a8908e1836d2f1a123c1625ca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-20 18:00:07 +0000

    telegraf 1.38.3

--- Formula/t/telegraf.rb
+++ Formula/t/telegraf.rb
@@ -1,8 +1,8 @@
 class Telegraf < Formula
   desc "Plugin-driven server agent for collecting & reporting metrics"
   homepage "https://www.influxdata.com/time-series-platform/telegraf/"
-  url "https://github.com/influxdata/telegraf/archive/refs/tags/v1.38.2.tar.gz"
-  sha256 "8f67651b9ff690593b57a8ff7703dbf9764bb1a356433503a4d9a27a69d26321"
+  url "https://github.com/influxdata/telegraf/archive/refs/tags/v1.38.3.tar.gz"
+  sha256 "83a8119b09830eb7d9167563add221335d0f59a22499919741ed539697a6fa45"
   license "MIT"
   head "https://github.com/influxdata/telegraf.git", branch: "master"
 
