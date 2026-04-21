a5504a957c84b982655aa31c4feb5685d65964bc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 20:03:55 +0000

    mysql-client@8.0: update 8.0.46 bottle.

--- Formula/m/mysql-client@8.0.rb
+++ Formula/m/mysql-client@8.0.rb
@@ -12,13 +12,12 @@ class MysqlClientAT80 < Formula
   end
 
   bottle do
-    rebuild 1
-    sha256 arm64_tahoe:   "e0eee02438c1171ce3b91f7169fc736ec5726121b2f18f9faabe86d57bfa9762"
-    sha256 arm64_sequoia: "986fbca72239104460859c4c49116ec7487347a9b4559c632bc4a731875b0a59"
-    sha256 arm64_sonoma:  "09fe421464e5cece5defe612b3e7249fc9badd2273f21e462530f5f91a9fb7da"
-    sha256 sonoma:        "a1c6b120c3459343c2904e08998f8b08df8245ed96de9da0fc33a95fd414f0fc"
-    sha256 arm64_linux:   "93b7add6884734fbb9725277ea5ff92d2b683ee422830d8c8412ed8ef0622868"
-    sha256 x86_64_linux:  "60fcf50ff63a630ce3d4e65a0130956a008e55958ca572a128bc2188dde00281"
+    sha256 arm64_tahoe:   "b04aa085d7b540a2a26910f0adc7eaa82776f4a31c050205877e72dc1b0b14d7"
+    sha256 arm64_sequoia: "c62722b6ab50c2608f96655d03136ee76106e30b6ea57e3bd2ed0fa672aa2a96"
+    sha256 arm64_sonoma:  "934915d2baa56d43a1f01f7b8e0d982922e03972ce8d690c7a76e47bfc4fb79d"
+    sha256 sonoma:        "4aaed2075ded190d1494d60f66a66dfccbe09e3ad0e054043524e953104cf495"
+    sha256 arm64_linux:   "73443c2a2c0bc42a5bec2d5f8f110c65cf8cfe952c91dc02cf3db29f8de1fca6"
+    sha256 x86_64_linux:  "5b3762f299d424bde333aae29bc00580ed7f52eb0c622887a0222848372fa089"
   end
 
   keg_only :versioned_formula
