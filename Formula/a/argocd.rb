454d9b8afe55c19a0a00701434d563a7243d20c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 20:10:40 +0000

    argocd 3.3.8

--- Formula/a/argocd.rb
+++ Formula/a/argocd.rb
@@ -2,8 +2,8 @@ class Argocd < Formula
   desc "GitOps Continuous Delivery for Kubernetes"
   homepage "https://argoproj.github.io/cd/"
   url "https://github.com/argoproj/argo-cd.git",
-      tag:      "v3.3.7",
-      revision: "035e8556c451196e203078160a5c01f43afdb92f"
+      tag:      "v3.3.8",
+      revision: "7ae7d2cc723f5408b080a31263e705198af08613"
   license "Apache-2.0"
 
   # There can be a notable gap between when a version is tagged and a
