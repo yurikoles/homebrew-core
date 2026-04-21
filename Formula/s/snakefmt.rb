da12e8bd8ea9e23d237f132f725738ec29775b98
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 04:00:10 +0000

    snakefmt: update 1.1.0 bottle.

--- Formula/s/snakefmt.rb
+++ Formula/s/snakefmt.rb
@@ -9,12 +9,12 @@ class Snakefmt < Formula
   head "https://github.com/snakemake/snakefmt.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4548bf6a04d4c03d1e464e9ff5d193bba550834c52007a153a78da6cbea6c97f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7743b7e02bc16633227d1a91db6cdb3ed2923df4287bb1e2e53d253fdd67acc3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b27f73b2441735167874131bc2ae774758d3ba8a43a183479e02ec00bf1cce77"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4f63c2bb28d23b31b76aedf9103d6873fcf31735b60fce904ebb083a9aa1f05c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8462c8a02dd4b586a9a5e494cb9e9f338ec9ab103b293103dca8bfd1cc38bc68"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0d96ea970c7d8e7149272b97794f15cf1c943b9d117e647b68b124a94a4c009d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c845043c4c1d836761739d24127e92b93e00a02fb8023f35fecbebf60fa49749"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9e83d5daf55b84c936718fc9dbf8bdeef3c7537355786b668ca09d18bc8281d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "be966da9f84044fe6cb67c50a8a4b239ffc03eb43f2d0b8cff972d7258fd0b51"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a349079d63be74621c96f3c1f9db52de82b34dc6f237c89fe33fcf8945702892"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ffc69b62223737020ecb4acb3c2bde8519f49e94ca1b40943b55085790ffb835"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ea2401d251efbbfc23d177b85e3a24b1c18837064ef55b18f3cb1822e9b9adb"
   end
 
   depends_on "python@3.14"
