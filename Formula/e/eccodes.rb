f848bdb69085446095101aa74479c36b53d3ae01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-22 17:24:28 +0000

    eccodes 2.47.0

--- Formula/e/eccodes.rb
+++ Formula/e/eccodes.rb
@@ -1,8 +1,8 @@
 class Eccodes < Formula
   desc "Decode and encode messages in the GRIB 1/2 and BUFR 3/4 formats"
   homepage "https://confluence.ecmwf.int/display/ECC"
-  url "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.46.0-Source.tar.gz"
-  sha256 "7d959253d5e34aeb16caa14d4889ac06486d19821216743142733a32ee7b4935"
+  url "https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.47.0-Source.tar.gz"
+  sha256 "82da819aa9b51831dc14b3bf2918bfee50b1cd53a05088d0c3f4493758aae094"
   license "Apache-2.0"
   compatibility_version 1
 
