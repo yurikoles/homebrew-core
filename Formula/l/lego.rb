6a27cd3398f7acc66471798e80d9398d67777064
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-22 00:05:45 +0000

    lego: update 4.35.1 bottle.

--- Formula/l/lego.rb
+++ Formula/l/lego.rb
@@ -12,12 +12,12 @@ class Lego < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f2c4d4bc15dd376e6fd86fb085a573610c66d4cd0f04b9b3291667dfaae454d6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2c4d4bc15dd376e6fd86fb085a573610c66d4cd0f04b9b3291667dfaae454d6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f2c4d4bc15dd376e6fd86fb085a573610c66d4cd0f04b9b3291667dfaae454d6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "498bbf40ac26cc87286b89b319935abac57b021b767bf053c76e698d4bd148db"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "51401474b6e1d0c51199209002f14315471dda8330957c1f6cc187316f4866a1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7330d2775a0b6ed8af340812de272eeb034894533ed7107394c1112ee051f8e3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ca525b6b74a0d6e90e2837bd2e876aafe16ada4697b615d5414dc23a0f72b12e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ca525b6b74a0d6e90e2837bd2e876aafe16ada4697b615d5414dc23a0f72b12e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ca525b6b74a0d6e90e2837bd2e876aafe16ada4697b615d5414dc23a0f72b12e"
+    sha256 cellar: :any_skip_relocation, sonoma:        "41940e9e0a9c901f287b248db134154a251accf1a446cbe2e41ec0eb4e0ef5d3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dcde88395524aacca967a3224d8c6b3b3210bc1f27134b037acc9518f6921380"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "285dd85b08030fa0d3986991f0ba9bfbc08c8466472c0fdaf1a731723fa9dea0"
   end
 
   depends_on "go" => :build
