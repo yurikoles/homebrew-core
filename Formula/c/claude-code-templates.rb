8254b3fb6a793e374a7da0b11e12229342de5343
Leon Adomaitis <leon@adomaitis.de>
2026-04-23 05:15:12 +0200

    claude-code-templates 1.28.16

--- Formula/c/claude-code-templates.rb
+++ Formula/c/claude-code-templates.rb
@@ -1,19 +1,28 @@
 class ClaudeCodeTemplates < Formula
   desc "CLI tool for configuring and monitoring Claude Code"
   homepage "https://www.aitmpl.com/agents"
-  url "https://registry.npmjs.org/claude-code-templates/-/claude-code-templates-1.28.13.tgz"
-  sha256 "3c221675b08327cda7ac8e7218352a13952585dac1b551156a6cd7496085cc60"
+  url "https://registry.npmjs.org/claude-code-templates/-/claude-code-templates-1.28.16.tgz"
+  sha256 "1e451ce1049ecf5beed9a0f023d51997c7bd0f9868e85b1dfddb9a5b875d8cbd"
   license "MIT"
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, all: "04972771caac1cb6b1a8cd58ec7fcf071c5c426cbcc9072caa9fea75f049e73f"
-  end
-
   depends_on "node"
+
+  # Backport cli-tool version metadata fix.
+  patch do
+    url "https://github.com/davila7/claude-code-templates/commit/55b8c382abc412180f2c30c07644c5b2b5d01892.patch?full_index=1"
+    sha256 "30cc5661d33eb67dbf1981012c9df873b52006c3929ae9fdd37d1c6b0955c16f"
+  end
 
   def install
     system "npm", "install", *std_npm_args(ignore_scripts: false)
     bin.install_symlink libexec.glob("bin/*")
+
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    # Remove incompatible pre-built binaries.
+    libexec.glob("lib/node_modules/claude-code-templates/node_modules/{bufferutil}/prebuilds/*").each do |dir|
+      rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}"
+    end
   end
 
   test do
