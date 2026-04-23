5fcf3675127b2478266fa65719cf2d50c3ac1878
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 12:57:10 +0000

    pnpm: update 10.33.2 bottle.

--- Formula/p/pnpm.rb
+++ Formula/p/pnpm.rb
@@ -14,12 +14,12 @@ class Pnpm < Formula
   end
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "9e36a7b63d317cbd82d4dfe305d60e4e9d2d1dc426934e6885a0f80a7cb14359"
-    sha256 cellar: :any,                 arm64_sequoia: "2394b8bf001c69ec8ec6bf6852167a354e7802f89cd24637ba3765471e39b02b"
-    sha256 cellar: :any,                 arm64_sonoma:  "2394b8bf001c69ec8ec6bf6852167a354e7802f89cd24637ba3765471e39b02b"
-    sha256 cellar: :any,                 sonoma:        "31d48b1ba823fa31a309fa6cc7c6cfd439f7a472c1b90a9d7aadfb84286ba694"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4108822975e9684db3f3bd27386524dd595c8bacab1f00bdb2e7fa649c51483"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4108822975e9684db3f3bd27386524dd595c8bacab1f00bdb2e7fa649c51483"
+    sha256 cellar: :any,                 arm64_tahoe:   "106d1ccc08cded49cdc0114bcf587bd94785f59498499892ae97e6871c996029"
+    sha256 cellar: :any,                 arm64_sequoia: "81f91c9d4c8c1741432e25a858b05743c724750a7c4b71868d6f80c85391a3a7"
+    sha256 cellar: :any,                 arm64_sonoma:  "81f91c9d4c8c1741432e25a858b05743c724750a7c4b71868d6f80c85391a3a7"
+    sha256 cellar: :any,                 sonoma:        "beff7d2e0e0efd1de4c06488ee9e46a640bb8eed6e3a3347dd750abee221378a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e152ade85c78558e0050943548d5288888fc813aa3063e1586a05e96cbe732fe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e152ade85c78558e0050943548d5288888fc813aa3063e1586a05e96cbe732fe"
   end
 
   depends_on "node" => [:build, :test]
