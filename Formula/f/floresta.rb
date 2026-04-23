3a050e1325420ecabad791735ad37de81062bd40
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 20:26:31 +0000

    floresta: add 0.9.0 bottle.

--- Formula/f/floresta.rb
+++ Formula/f/floresta.rb
@@ -9,6 +9,15 @@ class Floresta < Formula
   ]
   head "https://github.com/getfloresta/Floresta.git", branch: "master"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c3f3ee1082b13ca1aa6fc1fdaa98f8b6639b9719e847a737db7c4da08158ba8f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "635e39430ce0d5400db337e9a927827a932e427a16905641cc8f4b751f1ec4db"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "801777d966511da07247471d94656149754d9d94715a0be6e058073b9ff6ea3c"
+    sha256 cellar: :any_skip_relocation, sonoma:        "92fdee27eda28d945e82b8372710be103b6f693b3775e7c537a074258585585f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "39f3dda1ac7e0a6807510eeb868276e7059790cf2e8ef45fbb4ecd98d6598348"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c61969fc684bb549eb75e2f812b33cdfe48c61ef4de877dcd9be76792bb71fe"
+  end
+
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "gcc" => :build
