5bef8b360c2e6fad8b0b82e65f9c49ab423b6ca9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 03:31:48 +0000

    ni 30.1.0

--- Formula/n/ni.rb
+++ Formula/n/ni.rb
@@ -1,8 +1,8 @@
 class Ni < Formula
   desc "Selects the right Node package manager based on lockfiles"
   homepage "https://github.com/antfu-collective/ni"
-  url "https://registry.npmjs.org/@antfu/ni/-/ni-30.0.0.tgz"
-  sha256 "3251ba97aa59ab3a04cc914b8d4807ee5a8a552e2c6c876c0dd10c279254f50f"
+  url "https://registry.npmjs.org/@antfu/ni/-/ni-30.1.0.tgz"
+  sha256 "2b7da96b87e6f1ffe8c3e4af68d8acbfc5b1607fef21af4e5e51d928d45231f1"
   license "MIT"
 
   bottle do
