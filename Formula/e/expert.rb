b7ff60668a91b46e9b352feb752e5cc917e4cdac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 23:42:47 +0000

    expert: update 0.1.1 bottle.

--- Formula/e/expert.rb
+++ Formula/e/expert.rb
@@ -6,12 +6,12 @@ class Expert < Formula
   license "Apache-2.0"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f9f4b95e9ca15c8c2b53c7ee9c346d53c24df1509d1be124868c350b9042c45"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ef5f201bbb3d9d686c40522a172bd791dceeca164e4c61902b7f56b32ca3d36"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e8e4d0bdb1935d6edf3ee3ced12625277bef20a197582681b2a4c582899cd9b7"
-    sha256 cellar: :any_skip_relocation, sonoma:        "ae7f9ce56676e96018dc3c0bba839567b514432078891ad8712efc4b1faf316f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c80d566b7bf95b48166a9a1737414f6a9d27a2cb067614344e753338a138de1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9158f8ecf797e2b959ef28491256effb3d447ee23b852a8d57075ac6be194e0b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b2216f8baef44fbe47d0f4ef4560581ee1c79dfeedbc25fe31b855084bb45f70"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5eaac9fbb54a49c282aa6527ac16798c44b78e060d333400df2e682a83d5b27c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7179f4d3d65cb02fe7f691eb242170e0c53e45c65e410d36344efd728f2ff88"
+    sha256 cellar: :any_skip_relocation, sonoma:        "b45c42c75be50c4409faa8fa68fde3ed9df10c91ac7a5d511a87a658f936a45c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f650d12eba0f434fb2fed80e51e299fac8b85a46aa5a8cf8824bb70625b7fd1a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd38f52de2630682f21d8298836f53365f3354475b0e9a079738eaf89cae4ad0"
   end
 
   depends_on "elixir" => :build
