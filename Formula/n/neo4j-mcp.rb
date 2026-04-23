c8337f8a8d96ea56617040c8f62653d6f5dd64cf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 15:09:16 +0000

    neo4j-mcp 1.5.2

--- Formula/n/neo4j-mcp.rb
+++ Formula/n/neo4j-mcp.rb
@@ -1,8 +1,8 @@
 class Neo4jMcp < Formula
   desc "Neo4j official Model Context Protocol server for AI tools"
   homepage "https://neo4j.com/docs/mcp/current/"
-  url "https://github.com/neo4j/mcp/archive/refs/tags/v1.5.1.tar.gz"
-  sha256 "fa0505c084927210236a6b9f732b01f5143ba09c602d9285fcdcde9cc15edb61"
+  url "https://github.com/neo4j/mcp/archive/refs/tags/v1.5.2.tar.gz"
+  sha256 "c31dfc4f2c63af446d1e27346d94b5fe93d758486b39510a032b74023568b365"
   license "GPL-3.0-or-later"
   head "https://github.com/neo4j/mcp.git", branch: "main"
 
