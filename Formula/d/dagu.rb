f2b2715b87f4c0429c0bc545a2fdf5c34c9115f6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 05:49:24 +0000

    dagu 2.6.1

--- Formula/d/dagu.rb
+++ Formula/d/dagu.rb
@@ -1,8 +1,8 @@
 class Dagu < Formula
   desc "Lightweight and powerful workflow engine"
   homepage "https://dagu.sh"
-  url "https://github.com/dagu-org/dagu/archive/refs/tags/v2.6.0.tar.gz"
-  sha256 "c0ed2d10762a3dd0f75528f407c12ef50470411a50f6b9e00b9cc6868621bfa8"
+  url "https://github.com/dagu-org/dagu/archive/refs/tags/v2.6.1.tar.gz"
+  sha256 "e72106d6965e55b50398720775551678ee437f701021b3daf25037664929a171"
   license "GPL-3.0-only"
   head "https://github.com/dagu-org/dagu.git", branch: "main"
 
