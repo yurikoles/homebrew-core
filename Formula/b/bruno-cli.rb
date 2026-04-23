669ec1c4a8f4987676f19953fb240181c4736911
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 20:15:07 +0000

    bruno-cli 3.3.0

--- Formula/b/bruno-cli.rb
+++ Formula/b/bruno-cli.rb
@@ -1,8 +1,8 @@
 class BrunoCli < Formula
   desc "CLI of the open-source IDE For exploring and testing APIs"
   homepage "https://www.usebruno.com/"
-  url "https://registry.npmjs.org/@usebruno/cli/-/cli-3.2.2.tgz"
-  sha256 "6d97543c6f8b1d2c8652ce12b3cf878f40e3ac31f7c18d45be54f24c5c7d86e3"
+  url "https://registry.npmjs.org/@usebruno/cli/-/cli-3.3.0.tgz"
+  sha256 "b2ace10243eb2de0fb6d0bbb1039163b851f4c01442ba9fca9d983a8edb18bc0"
   license "MIT"
 
   bottle do
