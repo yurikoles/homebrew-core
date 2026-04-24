a6d0a5b45918de579a59b6e7e10d0c8940303554
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 11:37:40 +0000

    little-cms2 2.19

--- Formula/l/little-cms2.rb
+++ Formula/l/little-cms2.rb
@@ -3,8 +3,8 @@ class LittleCms2 < Formula
   homepage "https://www.littlecms.com/"
   # Ensure release is announced at https://www.littlecms.com/categories/releases/
   # (or https://www.littlecms.com/blog/)
-  url "https://downloads.sourceforge.net/project/lcms/lcms/2.18/lcms2-2.18.tar.gz"
-  sha256 "ee67be3566f459362c1ee094fde2c159d33fa0390aa4ed5f5af676f9e5004347"
+  url "https://downloads.sourceforge.net/project/lcms/lcms/2.19/lcms2-2.19.tar.gz"
+  sha256 "49e7e134e4299733dd0eda434fa468997a28ab3d33fa397c642b03644f552216"
   license "MIT"
   version_scheme 1
   compatibility_version 1
