84b9e75c2f4e22305dc3a33ec43c0bc9a7832bc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 06:36:27 +0000

    sq 0.50.2

--- Formula/s/sq.rb
+++ Formula/s/sq.rb
@@ -1,8 +1,8 @@
 class Sq < Formula
   desc "Data wrangler with jq-like query language"
   homepage "https://sq.io"
-  url "https://github.com/neilotoole/sq/archive/refs/tags/v0.50.0.tar.gz"
-  sha256 "36e20553b05aa10069ea3a422bbd98df936f5ca505eae9d61b98ff7ee0b7a279"
+  url "https://github.com/neilotoole/sq/archive/refs/tags/v0.50.2.tar.gz"
+  sha256 "ad39478132f1f056ec894cc36fb7d090eb1131aab8e42e108454bf46ea18f0ee"
   license "MIT"
 
   bottle do
