b1fbb9bb7f3e0c98fcd9e0e98ed4e1241bf8f78b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 15:10:43 +0000

    pumba 1.1.0

--- Formula/p/pumba.rb
+++ Formula/p/pumba.rb
@@ -1,8 +1,8 @@
 class Pumba < Formula
   desc "Chaos testing tool for Docker"
   homepage "https://github.com/alexei-led/pumba"
-  url "https://github.com/alexei-led/pumba/archive/refs/tags/1.0.6.tar.gz"
-  sha256 "5e52173ce725fa90f88d206ee614ad13c626dcc6329a5696c47d283e8f8d4483"
+  url "https://github.com/alexei-led/pumba/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "c9da0446666a9869846fc06069410028b35fb8f54c5bb7b75162c1acd8385a13"
   license "Apache-2.0"
   head "https://github.com/alexei-led/pumba.git", branch: "master"
 
