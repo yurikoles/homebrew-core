54459770a0504c3aa9f6fcf84c85f71111993f11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 13:19:33 +0000

    sdl3_mixer: add 3.2.0 bottle.

--- Formula/s/sdl3_mixer.rb
+++ Formula/s/sdl3_mixer.rb
@@ -11,6 +11,15 @@ class Sdl3Mixer < Formula
     regex(/^(?:release[._-])?v?(3(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "40386376f65c2f80aa65dec1f95af9f0fcd55ad646218df600b686a4001db6b8"
+    sha256 cellar: :any,                 arm64_sequoia: "099f6100d09f996c4d54f798cf199962b97bc4c0f123b2e78cdbbc7afdf02bfc"
+    sha256 cellar: :any,                 arm64_sonoma:  "6adfa053dda12cddf303f134a053f2756ee6840c05160bf62fc5f83d92213b52"
+    sha256 cellar: :any,                 sonoma:        "71dcfbb95c0bc67acc53260e88f8833547a7f631eaf8e8c80970456f77e62b21"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "926eb9a8c8410348e9281ebb85805a736ec8992a3c63b04bf1538ab82598b306"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdb57e94cada153ef7c2069793a292c42e162b2c5ce0586a9313b325a37809db"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkgconf" => :build
   depends_on "flac"
