868e726e0eef7495e5b8585987c1b97b1dab59fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 00:00:58 +0000

    melange: update 0.50.3 bottle.

--- Formula/m/melange.rb
+++ Formula/m/melange.rb
@@ -12,12 +12,12 @@ class Melange < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74d17382fd3ba1462b9f9afaa52ccd7b9bbd2860f1d043e6ecf5565e82b9362d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba337e11215d7657a47d40b3a26cd2bdb3ffdc2fa3347a36f566e057f7630932"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a8ab42baa2cf8978c4e9998cc95a384decb9be638f715ad460399fe23abdee2e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "675286d9ebe09d528516aa20c658bca875f54987f2d251172c82e86d2c538296"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9316448114eeb5e0ca620f427510e15f51881be0d6757faf71abc04ac5a84f52"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49de6e43fa43bf164ef15b970906456c86f5afe28655a6c4c571a5d68ba34071"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2dabae9049058c1048754d8fea35762e3cb550364107111d29f550e3ed67ba77"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec4e9d9068b0ba2f56df6f7b711371b63233009f57523e7ce21e8fd84224cf57"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa710efcdc3e4be82e502ccc25d6a555a698c27a5aabcc4cc3d6f97b6f1e921a"
+    sha256 cellar: :any_skip_relocation, sonoma:        "bee7392607a443d7e80b8d6f7948fafb4839fa54b2d708a6771263601da6a7d0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1c235f6675b4906b6c2ea403ce90141b8c90d2e677089a5fea546648fc163d79"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e0775d9d0c2c9cdc1e24b269b93bbe4e00fe16077fe473cb08cb77b7860e2aa"
   end
 
   depends_on "go" => :build
