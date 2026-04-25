849ea456f1353dc1d050bdd7cfed07588a24db29
Patrick Wyatt <patrickjohnwyatt@gmail.com>
2026-04-25 09:26:40 -0700

    sandvault 1.12.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/s/sandvault.rb
+++ Formula/s/sandvault.rb
@@ -1,8 +1,8 @@
 class Sandvault < Formula
   desc "Run AI agents isolated in a sandboxed macOS user account"
   homepage "https://github.com/webcoyote/sandvault"
-  url "https://github.com/webcoyote/sandvault/archive/refs/tags/v1.11.0.tar.gz"
-  sha256 "7d588a2d45fed00339201a5e159b9f5424a597b36e5037203d5f35bb70eab221"
+  url "https://github.com/webcoyote/sandvault/archive/refs/tags/v1.12.0.tar.gz"
+  sha256 "df41954393ea58a917680c0bffdce7c13295c8c90d06ced678fdc3df62195778"
   license "Apache-2.0"
 
   bottle do
