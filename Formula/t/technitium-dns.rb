cfd5bd67e04f1157e488a2fb1c0b72ccc2396013
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-04-25 15:11:49 +0200

    technitium-dns 15.0.0
    Manual bump because upstream moved from .NET 9 to .NET 10

--- Formula/t/technitium-dns.rb
+++ Formula/t/technitium-dns.rb
@@ -1,11 +1,9 @@
 class TechnitiumDns < Formula
   desc "Self host a DNS server for privacy & security"
   homepage "https://technitium.com/dns/"
-  # Try upgrade to latest `dotnet` on version bump
-  url "https://github.com/TechnitiumSoftware/DnsServer/archive/refs/tags/v14.3.0.tar.gz"
-  sha256 "5f71c0661d22a5dd75374a9888bbd67469d1ba0b6b9947df770ec4833b7f36c3"
+  url "https://github.com/TechnitiumSoftware/DnsServer/archive/refs/tags/v15.0.0.tar.gz"
+  sha256 "f2f7cc829699adacfe3fbf2dff735e02656c6f7ac1076fa4e07c90db62ca21eb"
   license "GPL-3.0-or-later"
-  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "985294f14499dc8b8bf5e88b5864fd63c2f6d0faf4c84694bc80fc6933139788"
@@ -15,7 +13,7 @@ class TechnitiumDns < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b95454ad1fa1891156fec79b9c0ef3fe515e218df1823319591837524a69799"
   end
 
-  depends_on "dotnet@9"
+  depends_on "dotnet"
   depends_on "libmsquic"
   depends_on "technitium-library"
 
@@ -26,7 +24,7 @@ class TechnitiumDns < Formula
   def install
     ENV["DOTNET_CLI_TELEMETRY_OPTOUT"] = "1"
 
-    dotnet = Formula["dotnet@9"]
+    dotnet = Formula["dotnet"]
     args = %W[
       --configuration Release
       --framework net#{dotnet.version.major_minor}
@@ -55,19 +53,21 @@ class TechnitiumDns < Formula
     error_log_path var/"log/technitium-dns.log"
     log_path var/"log/technitium-dns.log"
     working_dir var
+    environment_variables DNS_SERVER_LOG_FOLDER_PATH: var/"log"
   end
 
   test do
-    dotnet = Formula["dotnet@9"]
+    dotnet = Formula["dotnet"]
     tmpdir = Pathname.new(Dir.mktmpdir)
     # Start the DNS server
     require "pty"
-    PTY.spawn("#{dotnet.opt_libexec}/dotnet #{libexec}/DnsServerApp.dll #{tmpdir}") do |r, _w, pid|
+    dns_cmd = "#{dotnet.opt_libexec}/dotnet #{libexec}/DnsServerApp.dll #{tmpdir}"
+    PTY.spawn({ "DNS_SERVER_LOG_FOLDER_PATH" => tmpdir }, dns_cmd) do |r, _w, pid|
       # Give the server time to start
       sleep 2
       # Use `dig` to resolve "localhost"
       assert_match "Server was started successfully", r.gets
-      output = shell_output("dig @127.0.0.1 localhost 2>&1")
+      output = shell_output("dig @127.0.0.1 localhost +tcp 2>&1")
       assert_match "ANSWER SECTION", output
       assert_match "localhost.", output
     ensure
