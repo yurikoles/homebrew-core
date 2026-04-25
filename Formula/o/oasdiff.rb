bcfae894e4199072667766de59c5791de562a222
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-25 17:43:41 +0000

    oasdiff 1.15.0

--- Formula/o/oasdiff.rb
+++ Formula/o/oasdiff.rb
@@ -1,8 +1,8 @@
 class Oasdiff < Formula
   desc "OpenAPI Diff and Breaking Changes"
   homepage "https://www.oasdiff.com/"
-  url "https://github.com/oasdiff/oasdiff/archive/refs/tags/v1.14.0.tar.gz"
-  sha256 "a2cc623689e4aceeeb6656cfd1317d0c020ac50a5367a4477970e7375a0bb4d9"
+  url "https://github.com/oasdiff/oasdiff/archive/refs/tags/v1.15.0.tar.gz"
+  sha256 "536614f37d0f5595717842283d7e9d7381de9dc1f4eaddbedfa3f964282000ea"
   license "Apache-2.0"
   head "https://github.com/oasdiff/oasdiff.git", branch: "main"
 
