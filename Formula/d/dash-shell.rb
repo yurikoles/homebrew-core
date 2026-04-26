53533a5b5c650b671560cdbe7a47876f82c9c3a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-26 14:32:52 +0000

    dash-shell: update 0.5.13.3 bottle.

--- Formula/d/dash-shell.rb
+++ Formula/d/dash-shell.rb
@@ -13,12 +13,12 @@ class DashShell < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7796c200354aacda53355c94825f6ad57db2b06918809bd686c18d718b76facb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c9a9310a9d0d7e25d3da03b9ff6741ef1665d408ee1c1c64251a0bd6e48d997"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b521040d0d3b5cd9f2106dafad60a16ca0f62a9e497eb9798d4a5368c657cb6a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4f7595fb77974b4eb23f06f5ba687d9b812c2b98a77b3a83a4224e576033d859"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8b9a42405cd16446d7ed55e26d2ded21080e2929a8a66e1b905a2f98c3d26867"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8217368265784d89ee03c02e3b7226da9f7ddc24be96c03a8d8ba51813721351"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "65e1f9e282e54b309f98da5b920471b4dde8e1f7a16d30ea97aff9eeaf23003f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a2f9fa078bc85fc0ec14a431572dead40a38e8d43c19f66fd5abfaac6ff8b2ee"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a570c8f3c3d594ca585cfbe9d1c396463c5095d22fc30b8aec3512e1836ede1"
+    sha256 cellar: :any_skip_relocation, sonoma:        "69d63acdc95189907326ae70aae49b7ead0d9872720f8d91dca0f10e4d58b006"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2aebd91e2ef38afab388ea46a50c25862f4522bdfa3eb1ff94d1bd915644cedb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4fb8e6d7e162426021414329950aef5dfa0da7de0be5c4d3418e55b11dc0d3f"
   end
 
   depends_on "autoconf" => :build
