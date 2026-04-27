0199852f88ce860e743de32dad5f828eca8d07d5
AltCode <altcode@vivaldi.net>
2026-04-27 12:34:26 +0200

    elixir-ls: symlink debug adapter

--- Formula/e/elixir-ls.rb
+++ Formula/e/elixir-ls.rb
@@ -4,6 +4,7 @@ class ElixirLs < Formula
   url "https://github.com/elixir-lsp/elixir-ls/archive/refs/tags/v0.30.0.tar.gz"
   sha256 "d8e6c1b7ffc2a122c280ab9790de3cc22b2c12cf98caeabca66e222c8b6a8da1"
   license "Apache-2.0"
+  revision 1
 
   livecheck do
     url :stable
@@ -24,23 +25,44 @@ class ElixirLs < Formula
     system "mix", "deps.get"
     system "mix", "compile"
     system "mix", "elixir_ls.release2", "-o", libexec
-    libexec.glob("*.bat").map(&:unlink)
+    rm Dir[libexec/"*.bat"]
 
     bin.install_symlink libexec/"language_server.sh" => "elixir-ls"
+    bin.install_symlink libexec/"debug_adapter.sh" => "elixir-debug-adapter"
   end
 
   test do
-    assert_path_exists bin/"elixir-ls"
     system "mix", "local.hex", "--force"
 
-    input =
-      "Content-Length: 152\r\n" \
-      "\r\n" \
-      "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"initialize\",\"params\":{\"" \
-      "processId\":88075,\"rootUri\":null,\"capabilities\":{},\"trace\":\"ver" \
-      "bose\",\"workspaceFolders\":null}}\r\n"
-
+    # Test language server
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+    input = "Content-Length: #{json.size}\r\n\r\n#{json}"
     output = pipe_output(bin/"elixir-ls", input, 0)
-    assert_match "Content-Length", output
+    assert_match(/^Content-Length: \d+/i, output)
+
+    # Test debug adapter
+    json = <<~JSON
+      {
+        "seq": 1,
+        "type": "request",
+        "command": "initialize",
+        "arguments": {
+          "threadId": 1
+        }
+      }
+    JSON
+    input = "Content-Length: #{json.size}\r\n\r\n#{json}"
+    output = pipe_output(bin/"elixir-debug-adapter", input, 0)
+    assert_match(/^Content-Length: \d+/i, output)
   end
 end
