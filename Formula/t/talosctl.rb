d51cab24d20aa1d4c126af90e596eadda43554fd
Dharsan Baskar <git@dharsanb.com>
2026-04-28 17:02:55 +0530

    talosctl: fixed autocomplete to use cobra autocomplete
    The previous autocomplete did not work properly because talosctl was
    using it's own autocomplete script. This has been fixed upstream. This has been merged in the new 1.13.0 version.

--- Formula/t/talosctl.rb
+++ Formula/t/talosctl.rb
@@ -26,7 +26,7 @@ class Talosctl < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/talosctl"
 
-    generate_completions_from_executable(bin/"talosctl", "completion")
+    generate_completions_from_executable(bin/"talosctl", shell_parameter_format: :cobra)
   end
 
   test do
