2e457e3cd89948d944a534f1615f6e4a0a1bc766
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-29 15:11:26 +0000

    ddns-go: update 6.16.13 bottle.

--- Formula/d/ddns-go.rb
+++ Formula/d/ddns-go.rb
@@ -7,12 +7,12 @@ class DdnsGo < Formula
   head "https://github.com/jeessy2/ddns-go.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0941330e4dc7921c32b77e998e3efd28523c337a968e7640ba339a54ee37482e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0941330e4dc7921c32b77e998e3efd28523c337a968e7640ba339a54ee37482e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0941330e4dc7921c32b77e998e3efd28523c337a968e7640ba339a54ee37482e"
-    sha256 cellar: :any_skip_relocation, sonoma:        "4e4fad7219e39b8720c470cd6d3c61c029e27f4ac7048cc46957893e21f7d2eb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f54dafb7b5c0e74b1c792e2724d93300fd7112afdf6e45d2ab39aa849ae94c73"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d44c08e12a71582487e6d5a32a68bd0922983d87032fa1c7fec61c2669445128"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d933c6d1f98eb1ecb40aba137f906245406fdc785baf258ab275116646822d28"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d933c6d1f98eb1ecb40aba137f906245406fdc785baf258ab275116646822d28"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d933c6d1f98eb1ecb40aba137f906245406fdc785baf258ab275116646822d28"
+    sha256 cellar: :any_skip_relocation, sonoma:        "ec211a3f905c6f800be60fbc7870b3a42ab7256ed72e0ac0cf49c64c0a2deaac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca6c539298e9dc9c7adb83e3e69e689cfa5868b0e2aca4e03e6f6f37a2c718bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa238a2a21bac61998b6a6436d0d5681730bf25fb2e3ce4cebf596968a4e2b97"
   end
 
   depends_on "go" => :build
