cf7ee59bb9b1aeb28df3773abdb3a2002a654318
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-29 13:04:28 +0000

    pipenv: update 2026.6.1 bottle.

--- Formula/p/pipenv.rb
+++ Formula/p/pipenv.rb
@@ -9,7 +9,7 @@ class Pipenv < Formula
   head "https://github.com/pypa/pipenv.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "c05ab4bad1038616a90a0cbdfd24936a90bec5618f35eea9420e933dc0373385"
+    sha256 cellar: :any_skip_relocation, all: "e196def743743f880ca01b6b2a2e76140a06d11d26810543a76e9988d2b75544"
   end
 
   depends_on "certifi" => :no_linkage
