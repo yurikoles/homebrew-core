c035be149749939d0b268729df5911a88ca201f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 00:40:55 +0000

    worktrunk: update 0.46.0 bottle.

--- Formula/w/worktrunk.rb
+++ Formula/w/worktrunk.rb
@@ -7,12 +7,12 @@ class Worktrunk < Formula
   head "https://github.com/max-sixty/worktrunk.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7dfde4b0da17bbd9b00f13b7bc798bdd600a1f17ca10d4723b9651de76409a27"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a3c6fbdcd78a4ca7e6f7e7a0381938a093f493579b5d8256c42f7977f6e84039"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6bba499d6c62365687174b5c7c3f700ae4da1df589c11b657194eb453e2746f0"
-    sha256 cellar: :any_skip_relocation, sonoma:        "c983ffb3f6feac248a6bc2f87d81531f362839935c3c7cf4560a4c46510a6412"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "573a2e26862ab6a2c5654313dfd62450bc3173763239ad28bb84fcdddbdb9db8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "436f62fc1219dcb3336270733d6e11421e301ada25a4f3edbca32122d00682d7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c856ea2cf0722f3a348dab0055d17d389642eee79b1950cd5169a1720204acd8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f952bc5e17d223c6022e35591d88754af75c49535e1d422b7c1718414f79ff0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc71c15b0d5bcfd28570479f4de0494d6943310ae3daa406bcd672db95052173"
+    sha256 cellar: :any_skip_relocation, sonoma:        "f93fa5f27cd5a712fa718a35a437def8b61669d5bacff8969c54f56f5e6eb074"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d049cf44f7e8abaaa11e570a5a48925b695ca2e9c2a8f4f64b5bf58010f73017"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a78e3ffa5f58d2c7eaff388f1da95f86d8601f9688668d1f44e677037904b179"
   end
 
   depends_on "rust" => :build
