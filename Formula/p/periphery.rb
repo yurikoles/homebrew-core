f0ff3e57ab932309d887af0227f41526db72da61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 07:50:20 +0000

    periphery: update 3.7.4_1 bottle.

--- Formula/p/periphery.rb
+++ Formula/p/periphery.rb
@@ -8,11 +8,10 @@ class Periphery < Formula
   head "https://github.com/peripheryapp/periphery.git", branch: "master"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3e1c491a5c1dc82e247ca241433a5e06e9102d5b990dd990041bf6a8bb14e0ea"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a795a73bb0537e622e971e79eeeb1e15376961c726ef252a8a2315a51f697aad"
-    sha256                               arm64_linux:   "b987c8031b3d4fdb652cd846eff55a3c4414995b64f4a87afc9651ea44fc9f21"
-    sha256                               x86_64_linux:  "703779b1a182165b076a38704fc21045826d6f091f703c9681c9229275b33e35"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1b8a3b74aa0c3f8c01aa5f9d479391017072d01f019c336d8e835b52dad47c68"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0a1dc3ad761d6dd38204a9ca8cd513fd77f580dd47150c159691fbf842b71a97"
+    sha256                               arm64_linux:   "092a72a761a35ab7d3c14b7a08f673579695ef6c4e883b1c745dc4fe87e34919"
+    sha256                               x86_64_linux:  "24907907f0325d589e4ce5963553fc243b59a551d350c0deb994e100f36d34b6"
   end
 
   # We need the CLT installed for libIndexStore.dylib.
