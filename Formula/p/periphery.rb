ba9b0680c0d11bd35225afb61d4fcbe8001c372c
Carlo Cabrera <github@carlo.cab>
2026-04-30 14:59:11 +0800

    periphery: ensure that CLT `libIndexStore` can be found
    Fixes #279809.

--- Formula/p/periphery.rb
+++ Formula/p/periphery.rb
@@ -4,6 +4,7 @@ class Periphery < Formula
   url "https://github.com/peripheryapp/periphery/archive/refs/tags/3.7.4.tar.gz"
   sha256 "6e3eb93904d4ea3ba346526b3e7dd90d0d258d4eff91977b859b91115f028711"
   license "MIT"
+  revision 1
   head "https://github.com/peripheryapp/periphery.git", branch: "master"
 
   bottle do
@@ -14,15 +15,24 @@ class Periphery < Formula
     sha256                               x86_64_linux:  "703779b1a182165b076a38704fc21045826d6f091f703c9681c9229275b33e35"
   end
 
+  # We need the CLT installed for libIndexStore.dylib.
+  pour_bottle? only_if: :clt_installed
+
   depends_on xcode: ["16.4", :build]
 
   uses_from_macos "curl"
   uses_from_macos "libxml2"
   uses_from_macos "swift"
 
+  def clt_lib_directory
+    on_macos do
+      "#{MacOS::CLT::PKG_PATH}/usr/lib"
+    end
+  end
+
   def install
     args = if OS.mac?
-      ["--disable-sandbox"]
+      ["--disable-sandbox", "-Xlinker", "-rpath", "-Xlinker", clt_lib_directory]
     else
       swift_cellar_libexec_lib = Formula["swift"].libexec/"lib"
 
@@ -41,5 +51,9 @@ class Periphery < Formula
     manifest = shell_output "swift package --disable-sandbox describe --type json"
     File.write "manifest.json", manifest
     system bin/"periphery", "scan", "--strict", "--skip-build", "--json-package-manifest-path", "manifest.json"
+
+    return unless OS.mac?
+
+    assert_includes (bin/"periphery").rpaths, clt_lib_directory
   end
 end
