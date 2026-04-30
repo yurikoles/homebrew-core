fedacefa87aeea1d769627513351c790d2de2afa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 23:10:49 +0000

    prowler: update 5.25.1 bottle.

--- Formula/p/prowler.rb
+++ Formula/p/prowler.rb
@@ -9,12 +9,12 @@ class Prowler < Formula
   head "https://github.com/prowler-cloud/prowler.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any,                 arm64_tahoe:   "e8b7820facb6452fe428a525258f4826e20dceed61a220ef26719905871d2a58"
-    sha256 cellar: :any,                 arm64_sequoia: "c861869e3c2c3c156160a9ad4b7860cb9440c9bf769470dc8ce76b57c4e5f81d"
-    sha256 cellar: :any,                 arm64_sonoma:  "0bc03a27b6971003304a257b795fae4878cd247a6ed1d2656217b77bc2b56f95"
-    sha256 cellar: :any,                 sonoma:        "bd3731d93383b3aa7cf4a3fa033a9b00da8777df5e7e9201616d967c0946f74c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "007ad6aa6dfe6979592d56511b4888645a83f71704b757cf9036b25e3ab9fb69"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "556400a169a22ccd3c348d21e12a2750297bdf367a17dab0a8491c8d59aa5fe6"
+    sha256 cellar: :any,                 arm64_tahoe:   "442d45ae7dfb195b9ad3b97e608a19e1295e1085cdf99f80c8417f7ce28f6b17"
+    sha256 cellar: :any,                 arm64_sequoia: "d2f2b6700fe046044de8e188762a22a04ac4d7bea9f4fa57cffa2102d017048f"
+    sha256 cellar: :any,                 arm64_sonoma:  "650f90e755cba0f5f25e3a15a3f0204edb430b320c3cdb6cd4a5055090c238af"
+    sha256 cellar: :any,                 sonoma:        "0e913158e4a6735820ae90fedf087fd1cb8ba5eddf7c88b1a6b6223204878d35"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2bbe47f433bf5f4f3de37d07553490a1812b7663ea1e8a0cbb88fc6b2d3790fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c25d8f0633144377eca722c4d96b603180711afd8c8a9b67526a9bdb75b4ccf3"
   end
 
   depends_on "cmake" => :build
