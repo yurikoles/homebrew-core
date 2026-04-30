00a9e0cd9ba1d87116c4da537ddfde382d614b1c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 12:33:28 +0000

    wuppiefuzz 1.5.0

--- Formula/w/wuppiefuzz.rb
+++ Formula/w/wuppiefuzz.rb
@@ -1,8 +1,8 @@
 class Wuppiefuzz < Formula
   desc "Coverage-guided REST API fuzzer developed on top of LibAFL"
   homepage "https://github.com/TNO-S3/WuppieFuzz"
-  url "https://github.com/TNO-S3/WuppieFuzz/releases/download/v1.4.3/source.tar.gz"
-  sha256 "283450c7b4d9723a0c3e67e537cddb1d4d6d77e3d93e5630dafc8f071ce79cfd"
+  url "https://github.com/TNO-S3/WuppieFuzz/releases/download/v1.5.0/source.tar.gz"
+  sha256 "f13052b7066be9c0b1047abc7c7a1ce9c5ed1139d52228fe2609b87853daa946"
   license "Apache-2.0"
 
   bottle do
