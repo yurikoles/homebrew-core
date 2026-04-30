624475d0e5696371503cd1903c963daf23324ece
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 21:40:45 +0000

    container: update 0.12.3 bottle.

--- Formula/c/container.rb
+++ Formula/c/container.rb
@@ -7,8 +7,8 @@ class Container < Formula
   head "https://github.com/apple/container.git", branch: "main"
 
   bottle do
-    sha256 arm64_tahoe:   "90ba11911fc7b02c7e6922a24edcd082030855ba1498e9cb0d94fd798526a5a7"
-    sha256 arm64_sequoia: "7b00d4345db3084cdde8260cf30cbe82da363e3ae9c2616bfb1a330284b7e632"
+    sha256 arm64_tahoe:   "f6ee6ab7fd98797b2e2961e3a6e28bbcdbf8abe95c3d20ee3eba1ea83aa4beb8"
+    sha256 arm64_sequoia: "a3a060599d6bb02e12b3fe4571844261c12e317d12182fe2173e51f27c025243"
   end
 
   depends_on xcode: ["26.0", :build]
