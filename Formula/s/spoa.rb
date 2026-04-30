1c66d2a51972a57169797fc43da3c3571af0aca8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 06:20:00 +0000

    spoa: add 4.1.5 bottle.

--- Formula/s/spoa.rb
+++ Formula/s/spoa.rb
@@ -5,6 +5,15 @@ class Spoa < Formula
   sha256 "b5d323740b01255c55725e88db2548c666a05bc83b825c19cfac10586e21e7b3"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "ad55584bc1d671174019fc33fba25846d45695b1495a8a58e4f569df3f51e4a7"
+    sha256 cellar: :any,                 arm64_sequoia: "e78b56f6b7ccd0c550496370806409371af5aa0e247b19fb6a19766dc8588b98"
+    sha256 cellar: :any,                 arm64_sonoma:  "a233dfaffda20031385fff9037eed19790d91745b7a158c251924de0710eac83"
+    sha256 cellar: :any,                 sonoma:        "6e5926cd309fa1eeac6595bc1bedad2d27a1b9073c263829509cd9a598b286b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "13f431aa4da78f6726cf32c9d188b5bde27f7120d54db94763cb5f1f7aef29fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d45598cf9672ff7d9daa64cdb07d315980580e21fddb16100b4de4c8c2b849c9"
+  end
+
   depends_on "cmake" => :build
 
   uses_from_macos "zlib"
