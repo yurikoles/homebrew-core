38175456ebe80a06f618c36ed4496321bd6ca1fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 17:44:57 +0000

    unordered_dense: add 4.8.1 bottle.

--- Formula/u/unordered_dense.rb
+++ Formula/u/unordered_dense.rb
@@ -5,6 +5,10 @@ class UnorderedDense < Formula
   sha256 "9f7202ec6d8353932ef865d33f5872e4b7a1356e9032da7cd09c3a0c5bb2b7de"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, all: "8b691bc4407ac13bc85937d52994d364ba13647abda79829c011b734cd08f7dd"
+  end
+
   depends_on "cmake" => :build
 
   def install
