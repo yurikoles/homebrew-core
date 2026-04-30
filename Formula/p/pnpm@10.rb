65aaf861f4363e4eaa2eb424346116a8d5223cbe
Rui Chen <rui@chenrui.dev>
2026-04-30 13:29:20 -0400

    pnpm@10 10.33.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/pnpm@10.rb
@@ -0,0 +1,46 @@
+class PnpmAT10 < Formula
+  desc "Fast, disk space efficient package manager"
+  homepage "https://pnpm.io/"
+  url "https://registry.npmjs.org/pnpm/-/pnpm-10.33.2.tgz"
+  sha256 "7a7bcf13d7f6ceb3946c03978373d99be9fde1cafc3000bdfed4c4f791167610"
+  license "MIT"
+
+  livecheck do
+    url "https://registry.npmjs.org/pnpm/latest-10"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "node" => [:build, :test]
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+    bin.install_symlink bin/"pnpm" => "pnpm@10"
+    bin.install_symlink bin/"pnpx" => "pnpx@10"
+
+    generate_completions_from_executable(bin/"pnpm", "completion")
+
+    # remove non-native architecture pre-built binaries
+    (libexec/"lib/node_modules/pnpm/dist").glob("reflink.*.node").each do |f|
+      next if f.arch == Hardware::CPU.arch
+
+      rm f
+    end
+  end
+
+  def caveats
+    <<~EOS
+      pnpm requires a Node installation to function. You can install one with:
+        brew install node
+    EOS
+  end
+
+  test do
+    system bin/"pnpm", "init"
+    assert_path_exists testpath/"package.json", "package.json must exist"
+  end
+end
