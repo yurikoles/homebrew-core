183b76e27e7da5ce54bb9955ae0453de2f4ed170
Rui Chen <rui@chenrui.dev>
2026-05-01 00:38:16 +0000

    pnpm@10: add 10.33.2 bottle.

--- Formula/p/pnpm@10.rb
+++ Formula/p/pnpm@10.rb
@@ -12,6 +12,15 @@ class PnpmAT10 < Formula
     end
   end
 
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "8fc2e5fb5841db7708969c24ca29c5b8fdee1faec02e2c7012d039855f658a2d"
+    sha256 cellar: :any,                 arm64_sequoia: "c108c9383b0aeaaa7332bb015e733f185299bd7ac2fccb907eb081b35630fdd1"
+    sha256 cellar: :any,                 arm64_sonoma:  "c108c9383b0aeaaa7332bb015e733f185299bd7ac2fccb907eb081b35630fdd1"
+    sha256 cellar: :any,                 sonoma:        "c99c7471951f39b1d2332b563fd3ef258ee60e1c85aacf3d936cc16ea42ebea1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "711e4394740fd6bce7c6bea8748e6802a921a9dd9c8e9311d762e333274cf014"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "711e4394740fd6bce7c6bea8748e6802a921a9dd9c8e9311d762e333274cf014"
+  end
+
   keg_only :versioned_formula
 
   depends_on "node" => [:build, :test]
