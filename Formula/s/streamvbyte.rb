72b15404ecf9802e82093b79955c5e6c5209da2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-01 01:48:27 +0000

    streamvbyte: update 3.0.0 bottle.

--- Formula/s/streamvbyte.rb
+++ Formula/s/streamvbyte.rb
@@ -7,14 +7,12 @@ class Streamvbyte < Formula
   head "https://github.com/fast-pack/streamvbyte.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a5b3c81835da9ae317838f2d8ad1a088c99fbdb465eccd4d338c6dade886c7cd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "955cfd051b2957a90b5dcd3206023547acbc96298938bd489f7413eb2bc1721a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "51d9a35f2a353d5ceb107471e0467e5689a3050715513cae8cdf2cca75c30e47"
-    sha256 cellar: :any_skip_relocation, arm64_ventura: "08b01a057acf0496f9e2afe325fcc1b051c812258a1026441a9b6705da57f2ba"
-    sha256 cellar: :any_skip_relocation, sonoma:        "78259a695625fe8a4d8db9cce7fe270e9da2feb5f138af8891d553003abe6074"
-    sha256 cellar: :any_skip_relocation, ventura:       "505180ca0088a187f5369cd4b4c9cc00069adcad065cfa04d976ef8a83da3a86"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "333f773e6f4ccd9231d9802d1768d652c417a532fff56cfe398f0b0b2d291fef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a2086e097ff025f7f27bf7c1326f2493eed90785881afae43779a37c88b45fd8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b9b38be68f098c19220568c2efca7d24eadc1031874cc0b3fb7ef21c4f321732"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9bd3c8fd90242a4afa1353cb261f823472455a8d531fe50d92675d9c61ef419"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dbcc8adcefbbf9ee6c4f87995871b974e103a7f5041efa91621cb1643fdd3219"
+    sha256 cellar: :any_skip_relocation, sonoma:        "5078507dec276064feb608d062f9644d39a5a2fda48b2d4288b5ad40e5898abe"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "76f3550eff1da0051efa3f6154f744504d1958d913635b244cbe029430dddc43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8485dc5e02365f948be578f4639016e77eb30d78f1ebf2b298c001e3e6fede46"
   end
 
   depends_on "cmake" => :build
