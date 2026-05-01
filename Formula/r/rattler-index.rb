fe4598d32e30816b594ef9987b7fa20c6b012f96
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-01 15:06:59 +0000

    rattler-index 0.28.1

--- Formula/r/rattler-index.rb
+++ Formula/r/rattler-index.rb
@@ -1,8 +1,8 @@
 class RattlerIndex < Formula
   desc "Index conda channels using rattler"
   homepage "https://github.com/conda/rattler"
-  url "https://github.com/conda/rattler/archive/refs/tags/rattler_index-v0.28.0.tar.gz"
-  sha256 "ff0ca443b94be71a6668810dc7c96e1535e50198978d5d84c48b1c680b0c1895"
+  url "https://github.com/conda/rattler/archive/refs/tags/rattler_index-v0.28.1.tar.gz"
+  sha256 "d8ef660f4abf1f8ee8afef7565ab0088b790432b502770892dcaffddc401d9dc"
   license "BSD-3-Clause"
   head "https://github.com/conda/rattler.git", branch: "main"
 
