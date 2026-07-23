4b14944067592d44110734b4740e77f9d5a60816
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 12:10:57 +0000

    git-pkgs: update 0.18.1 bottle.

--- Formula/g/git-pkgs.rb
+++ Formula/g/git-pkgs.rb
@@ -7,12 +7,12 @@ class GitPkgs < Formula
   head "https://github.com/git-pkgs/git-pkgs.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c63502a47ed298c4aedc97b4943a9f911185498c823ab18ab37a2e6d0d8a2f58"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c63502a47ed298c4aedc97b4943a9f911185498c823ab18ab37a2e6d0d8a2f58"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c63502a47ed298c4aedc97b4943a9f911185498c823ab18ab37a2e6d0d8a2f58"
-    sha256 cellar: :any_skip_relocation, sonoma:        "afd3ec28740e7cb831deceea103ae077d5f38846f4f324f019d341384a1a4c97"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "74934a7cc1815b25c4bd7e287ce1948a773438c7555c6f65e4ffb6e71c27bc9a"
-    sha256 cellar: :any,                 x86_64_linux:  "a817ab496423b702943e934e1b4c5ada3305daf21e563d6eb74d2697c0882c90"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f8d1b1efa1d43f169986316d56be49d9d9d3ac922978a3df9a9e25407613d19f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8d1b1efa1d43f169986316d56be49d9d9d3ac922978a3df9a9e25407613d19f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f8d1b1efa1d43f169986316d56be49d9d9d3ac922978a3df9a9e25407613d19f"
+    sha256 cellar: :any_skip_relocation, sonoma:        "a15ec40609121d2311faec0e47ac9d663b3c8b2a9c6c60915430282adb8ea53e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c591500955d3affb260aa93f4cb7be35e85d599354d290b3b25e67903f30139"
+    sha256 cellar: :any,                 x86_64_linux:  "2841da21b9bfa081a5e9993ca0e058ac9efc092ea6d777446d1baa4e3d86d9a0"
   end
 
   depends_on "go" => :build
